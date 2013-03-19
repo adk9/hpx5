105853ae16e7469e3c0695a9e98c0ff309b84df1
Kevin Bohan <pbohan@indiana.edu>
2013-03-19 21:56:34 +0000

    changed _hpx_mctx_boot to call exit() in case no context linkage was set before hpx_mctx_makecontext()
    git-svn-id: https://svn.crest.iu.edu/svn/hpx/hpx4/trunk@20 1aa0cbc6-15dc-43e9-bc76-a4c543290def

--- include/hpx_mctx.h
+++ include/hpx_mctx.h
@@ -67,6 +67,7 @@ typedef struct _hpx_mctx_context_t {
 void hpx_mctx_getcontext(hpx_mctx_context_t *, hpx_mconfig_t, uint64_t);
 void hpx_mctx_setcontext(hpx_mctx_context_t *, hpx_mconfig_t, uint64_t);
 void hpx_mctx_makecontext(hpx_mctx_context_t *, hpx_mconfig_t, uint64_t, void *, int, ...);
+void hpx_mctx_swapcontext(hpx_mctx_context_t *, hpx_mctx_context_t *, hpx_mconfig_t, uint64_t);
 
 void _hpx_mctx_boot(void);
 
