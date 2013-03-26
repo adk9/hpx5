9bf3b1ca7261c0de9268c258a5764f3050a3b0a7
Kevin Bohan <pbohan@indiana.edu>
2013-03-26 20:00:20 +0000

    made syscalls work for saving/restoring signals under Mac OS X
    git-svn-id: https://svn.crest.iu.edu/svn/hpx/hpx4/trunk@27 1aa0cbc6-15dc-43e9-bc76-a4c543290def

--- tests/00_hpxtest.c
+++ tests/00_hpxtest.c
@@ -75,9 +75,9 @@ int main(int argc, char * argv[]) {
 
   /* test machine context switching */
   tcase_add_test(tc, test_libhpx_mctx_getcontext);
-  //  tcase_add_test(tc, test_libhpx_mctx_getcontext_ext);
-  //  tcase_add_test(tc, test_libhpx_mctx_getcontext_sig);
-  //  tcase_add_test(tc, test_libhpx_mctx_getcontext_ext_sig);
+  tcase_add_test(tc, test_libhpx_mctx_getcontext_ext);
+  tcase_add_test(tc, test_libhpx_mctx_getcontext_sig);
+  tcase_add_test(tc, test_libhpx_mctx_getcontext_ext_sig);
   tcase_add_test(tc, test_libhpx_mctx_setcontext);
   tcase_add_test(tc, test_libhpx_mctx_setcontext_ext);
   tcase_add_test(tc, test_libhpx_mctx_setcontext_sig);
