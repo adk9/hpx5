742b2c3f797b71259b2fff04300d5285510c2c8d
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2013-07-07 20:13:55 -0400

    move the timer functions in hpx/utils

--- examples/fibonacci.c
+++ examples/fibonacci.c
@@ -1,17 +1,11 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <hpx/timer.h>
 #include <hpx/thread.h>
 
-#ifdef __APPLE__
-  #include <mach/mach_time.h>
-#endif
-
-#ifdef __linux__
-  #include <time.h>
-#endif
-
-hpx_context_t * ctx;
+hpx_context_t *ctx;
+hpx_timer_t    timer;
 
 void fib(unsigned int n) {
   hpx_thread_t * th1;
@@ -31,27 +25,11 @@ void fib(unsigned int n) {
   hpx_thread_join(th2, NULL);
 }
 
-
 int main(int argc, char * argv[]) {
   hpx_config_t cfg;
   hpx_thread_t * th;
   unsigned int n;
   uint32_t cores;
-
-#ifdef __APPLE__
-  static mach_timebase_info_data_t tbi;
-  uint64_t begin_ts;
-  uint64_t end_ts;
-
-  if (tbi.denom == 0) {
-    (void) mach_timebase_info(&tbi);
-  }
-#endif
-
-#ifdef __linux__
-  struct timespec begin_ts;
-  struct timespec end_ts;
-#endif
 
   /* validate our arguments */
   if (argc < 2) {
@@ -75,13 +53,8 @@ int main(int argc, char * argv[]) {
   /* get a thread context */
   ctx = hpx_ctx_create(&cfg);
 
-#ifdef __APPLE__
-  begin_ts = mach_absolute_time();
-#endif
-
-#ifdef __linux__
-  clock_gettime(CLOCK_MONOTONIC, &begin_ts);
-#endif
+  /* get start time */
+  hpx_get_time(timer);
 
   /* create a fibonacci thread */
   th = hpx_thread_create(ctx, fib, (void *) n);
@@ -89,23 +62,10 @@ int main(int argc, char * argv[]) {
   /* wait for the thread to finish */
   hpx_thread_join(th, NULL);
 
-#ifdef __APPLE__
-  end_ts = mach_absolute_time();
-
-  printf("seconds: %.7f\ncores:   %d\nthreads: %ld\n", (((end_ts - begin_ts) * tbi.numer / tbi.denom) / 1000000000.0),
+  printf("seconds: %.7f\ncores:   %d\nthreads: %ld\n", hpx_elapsed_us(timer)/1e3,
 	 hpx_config_get_cores(&cfg), (unsigned long) pow(2, n) + 1);
-#endif
-
-#ifdef __linux__
-  clock_gettime(CLOCK_MONOTONIC, &end_ts);
-  unsigned long elapsed = ((end_ts.tv_sec * 1000000000) + end_ts.tv_nsec) - ((begin_ts.tv_sec * 1000000000) + begin_ts.tv_nsec);
-
-  printf("seconds: %.7f\ncores:   %d\nthreads: %ld\n", (elapsed / 1000000000.0),
-	 hpx_config_get_cores(&cfg), (unsigned long) pow(2, n) + 1);
-#endif
 
   /* cleanup */
   hpx_ctx_destroy(ctx);
-
   return 0;
 }
---
+++ src/util/timer.c
@@ -0,0 +1,59 @@
+
+/*
+ ====================================================================
+  High Performance ParalleX Library (libhpx)
+  
+  Copyright (c) 2013, Trustees of Indiana University 
+  All rights reserved.
+
+  This software may be modified and distributed under the terms of
+  the BSD license.  See the COPYING file for details.
+
+  This software was created at the Indiana University Center for
+  Research in Extreme Scale Technologies (CREST).
+ ====================================================================
+*/
+
+#include <assert.h>
+
+#include "hpx/timer.h"
+
+/*
+ --------------------------------------------------------------------
+  hpx_time_init
+
+  Initialize the time subsystem. This function should be called
+  before using any other timer functions.
+  -------------------------------------------------------------------
+*/
+
+void hpx_time_init(void) {
+#ifdef __APPLE__
+    if (tbi.denom == 0) {
+        (void) mach_timebase_info(&tbi);
+    }
+#endif
+}
+
+void hpx_get_time(hpx_timer_t time) {
+  assert(time);
+#ifdef __APPLE__
+  *time = mach_absolute_time();
+#endif
+#ifdef __linux__
+  clock_gettime(CLOCK_MONOTONIC, time);
+#endif
+}
+
+double hpx_elapsed_us(hpx_timer_t start_time) {
+  hpx_timer_t end_time;
+  hpx_get_time(end_time);
+  assert(start_time);
+#ifdef __APPLE__
+  return (((end_time - start_time) * tbi.numer / tbi.denom) / 1e6);
+#endif
+#ifdef __linux__
+  unsigned long elapsed = ((end_time->tv_sec * 1e9) + end_time->tv_nsec) - ((start_time->tv_sec * 1e9) + start_time->tv_nsec);
+  return (elapsed / 1e6);
+#endif
+}
