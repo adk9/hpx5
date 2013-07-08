279c9cbc948269f73a1acbd65de51a91069aea5e
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2013-07-08 16:58:34 -0400

    push out some stubs

---
+++ include/hpx/parcel.h
@@ -0,0 +1,22 @@
+/*
+ ====================================================================
+  High Performance ParalleX Library (libhpx)
+  
+  Copyright (c) 2013, Trustees of Indiana University 
+  All rights reserved.
+
+  This software may be modified and distributed under the terms of
+  the BSD license.  See the COPYING file for details.
+
+  This software was created at the Indiana University Center for
+  Research in Extreme Scale Technologies (CREST).
+ ====================================================================
+*/
+
+/*
+ --------------------------------------------------------------------
+  Parcel Registration
+  -------------------------------------------------------------------
+*/
+
+hpx_parcel_t * parcel_register(hpx_context_t *, void *, void *);
