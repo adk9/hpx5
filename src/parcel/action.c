c0196204a06fe119e81bd0400f870b587ca5a188
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2013-07-09 16:54:47 -0400

    pushing some unstaged changes

--- src/parcel/action.c
+++ src/parcel/action.c
@@ -18,8 +18,29 @@
 
 #include <stdlib.h>
 
+
 #include "hpx/action.h"
 
+  /************************************************************************/
+  /* ADK: There are a few ways to handle action registration--The         */
+  /* simplest is under the naive assumption that we are executing in a    */
+  /* homogeneous, SPMD environment and parcels simply carry function      */
+  /* pointers around. The second is to have all interested localities     */
+  /* register the required functions and then simply pass tags            */
+  /* around. Finally, a simpler, yet practical alternative, is to have a  */
+  /* local registration scheme for exported functions. Eventually, we     */
+  /* would want to have a distributed namespace for parcels that provides */
+  /* all three options.                                                   */
+  /************************************************************************/
+
+/** 
+ * Register an HPX action.
+ * 
+ * @param name Action Name
+ * @param func The HPX function that is represented by this action
+ * 
+ * @return HPX error code
+ */
 int hpx_action_register(char *name, hpx_func_t func) {
 }
 
