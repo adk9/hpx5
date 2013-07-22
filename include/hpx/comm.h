da41ac86f699eb2f4b6f7a5ece79287922e6ef9e
Benjamin Martin <benjmart@indiana.edu>
2013-07-22 17:51:11 -0400

    Some really rough code (won't compile) to reconcile comm.h and network.h

--- include/hpx/comm.h
+++ include/hpx/comm.h
@@ -13,6 +13,8 @@
  ====================================================================
 */
 
+#include <mpi.h>   /* TODO: deal with case where there is no mpi */
+
 #pragma once
 #ifndef LIBHPX_COMM_H_
 #define LIBHPX_COMM_H_
@@ -22,21 +24,37 @@
  */
 
 typedef struct comm_operations_t {
-    /* Initialize the communication layer */
-    int (*init)(void);
-    /* Helper to send a parcel structure */
-    int (*send_parcel)(hpx_locality_t *, hpx_parcel_t *);
-    /* Send a raw payload */
-    int (*send)(int peer, void *payload, size_t len);
-    /* RMA put */
-    int (*put)(int peer, void *dst, void *src, size_t len);
-    /* RMA get */
-    int (*get)(void *dst, int peer, void *src, size_t len);
-    /* The communication progress function */
-    void (*progress)(void *data);
-    /* Shutdown and clean up the communication layer */
-    void (*finalize)(void);
+  /* Initialize the communication layer */
+  int (*init)(void);
+  /* Helper to send a parcel structure */
+  int (*send_parcel)(hpx_locality_t *, hpx_parcel_t *);
+  /* Send a raw payload */
+  int (*send)(int dest, void *buffer, size_t len, comm_request_t);
+  /* Receive raw data */
+  int (*recv)(void *buffer, comm_request_t);
+  /* test for completion of send or receive */
+  int (*sendrecv_test)(comm_request_t *request, int *flag, comm_status_t *status);
+  /* RDMA put */
+  int (*put)(void *buffer, size_t len, comm_request_t *request);
+  /* RDMA get */
+  int (*get)(void *buffer, size_t len);
+  /* test for completion of put or get */
+  int (*putget_test)(comm_request_t *request, int *flag, comm_status_t *status);
+  /* Shutdown and clean up the communication layer */
+  void (*finalize)(void);
 } comm_operations_t;
+
+typedef struct comm_request_t {
+  /* TODO: deal with case where there is no mpi */
+  MPI_Request mpi;
+  uint32_t photon;
+} comm_request_t;
+
+typedef struct comm_status_t {
+  /* TODO: deal with case where there is no mpi */
+  MPI_Status mpi;
+  MPI_Status photon; /* not a mistake - Photon uses MPI status */
+}
 
 /**
  * Default communication operations
--- src/comm/comm.c
+++ src/comm/comm.c
@@ -16,40 +16,267 @@
  ====================================================================
 */
 
+#include <limits.h>
 #include <stdlib.h>
 
 #include "hpx/action.h"
 #include "hpx/comm.h"
 #include "hpx/parcel.h"
+#include "hpx/runtime.h" /* for hpx_locality_t */
+
+#include <mpi.h>
+
+int _comm_eage_threshold = 256; /* is big better or worse? smaller means less likelihood of memory problems but higher latency */
 
 /* Default communication operations */
 comm_operations_t default_operations = {
     .init            = _comm_init,
     .send_parcel     = _comm_send_parcel,
     .send            = _comm_send,
+    .recv            = _comm_recv,
+    .sendrecv_test   = _comm_sendrecv_test,
     .put             = _comm_put,
     .get             = _comm_get,
-    .progress        = _comm_progress,
+    .putget_test     = _comm_putget_test,
     .finalize        = _comm_finalize,
+    .eager_threshold = _comm_edge_threshold,
 };
 
-int _comm_init(void) {
+/*
+ * Stub versions
+ */
+
+int _comm_init(void) {}
+
+void _comm_finalize(void) {}
+
+int _comm_send_parcel(hpx_locality_t *, hpx_parcel_t *) {}
+
+int _comm_send(int dest, void *buffer, size_t len, comm_request_t) {}
+
+int _comm_recv(void *buffer, comm_request_t) {}
+
+int _comm_sendrecv_test(comm_request_t *request, int *flag, comm_status_t *status) {}
+
+int _comm_put(void *buffer, size_t len, comm_request_t *request) {}
+
+int _comm_get(void *buffer, size_t len) {}
+
+int _comm_putget_test(comm_request_t *request, int *flag, comm_status_t *status) {}
+
+
+/*
+ * MPI variants
+ */
+
+/* If you're using Photon, call _comm_init_photon instead (or else)! */
+int _comm_init_mpi(void) {
+  int retval;
+  int temp;
+  int thread_support_provided;
+
+  retval = -1;
+
+  /* TODO: see if we really need thread multiple */
+  temp = MPI_Init_thread(0, NULL, MPI_THREAD_MULTIPLE, &thread_support_provided); /* TODO: should be argc and argv if possible */
+  if (temp == MPI_SUCCESS)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+  
+  return retval;
 }
 
-int _comm_send_parcel(hpx_locality_t *, hpx_parcel_t *) {
+/* If you're using Photon, call _comm_finalize_photon instead (or else)! */
+int _comm_finalize_mpi(void) {
+  int retval;
+  int temp;
+  retval = -1;
+
+  temp = MPI_Finalize();
+
+  if (temp == MPI_SUCCESS)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+
+  return retval;
 }
 
-int _comm_send(int peer, void *payload, size_t len) {
+int _comm_send_parcel_mpi(hpx_locality_t *, hpx_parcel_t *) {
+  /* pseudocode:
+     if size > eager_threshold:
+       send notice to other process of intent to put via rdma
+       put data via rdma
+     else:
+       send parcel using _comm_send_mpi
+  */
 }
 
-int _comm_put(int peer, void *dst, void *src, size_t len) {
+/* Send data via MPI. Presumably this will be an "eager" send. Don't use "data" until it's done! */
+int _comm_send_mpi(int dest, void *data, size_t len, comm_request_t *request) {
+  int temp;
+  int rank;
+
+  /*
+  if (len > INT_MAX) {
+    __hpx_errno = HPX_ERROR;
+    retval = -1;
+  }
+  */ /* not necessary because of eager_threshold */
+  if (len > _comm_eager_threshold) { /* need to use _comm_put_* for that */
+    __hpx_errno = HPX_ERROR;
+    retval = -1;    
+  }
+
+  MPI_rank(&rank); /* TODO: cache this, obviously */
+  temp = MPI_Isend(data, (int)len, MPI_BYTE, dest, -1, rank, MPI_COMM_WORLD, request->mpi);
+
+  if (temp == MPI_SUCCESS)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+
+  return retval;  
 }
 
-int _comm_get(void *dst, int peer, void *src, size_t len) {
+/* this is non-blocking recv - user must test/wait on the request */
+int _comm_recv_mpi(void* buffer, comm_request_t *request) {
+  int retval;
+  int temp;
+  retval = -1;
+
+  temp = MPI_Irecv(buffer, _comm_eager_threshold, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, request.mpi);
+
+  if (temp == MPI_SUCCESS)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+
+  return retval;  
 }
 
-void _comm_progress(void *data) {
+/* status may be NULL */
+int _comm_sendrecv_test_mpi(comm_request_t *request, int *flag, comm_status_t *status) {
+  int retval;
+  int temp;
+  retval = -1;
+
+  if (status == NULL)
+    temp = MPI_Test(request, &flag, MPI_STATUS_IGNORE);
+  else
+    temp = MPI_Test(request, &flag, status.mpi);
+
+  if (temp == MPI_SUCCESS)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+
+  return retval;  
 }
 
-void _comm_finalize(void) {
+/*
+ * Photon functions
+ */
+
+/* If using Photon, call this instead of _comm_init_mpi */
+int _comm_init_photon(void) {
+  int retval;
+  int temp;
+  int thread_support_provided;
+  int rank;
+  int size;
+
+  retval = -1;
+
+  /* TODO: see if we really need thread multiple */
+  temp = MPI_Init_thread(0, NULL, MPI_THREAD_MULTIPLE, &thread_support_provided); /* TODO: should be argc and argv if possible */
+  if (temp == MPI_SUCCESS)
+    retval = 0;
+  else {
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+    goto error;
+  }
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* TODO: cache */
+  MPI_Comm_size(MPI_COMM_WORLD, &size); /* TODO: cache */
+  temp =  photon_init(size, rank, MPI_COMM_WORLD);
+  if (temp == 0)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+
+ error:
+  return retval;
+}
+
+/* If you're using Photon, call this instead of _comm_finalize_mpi */
+int _comm_finalize_photon(void) {
+  int retval;
+  int temp;
+  retval = -1;
+
+  temp = MPI_Finalize();
+
+  if (temp != MPI_SUCCESS) {
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+    goto error;
+  }
+
+  temp = photon_finalize();
+  if (temp == 0)
+    retval = 0;
+
+ error:
+  return retval;
+}
+
+
+/* have to test/wait on the request */
+int _comm_put_photon(void* buffer, size_t len, comm_request_t *request) {
+  int rank;
+  MPI_rank(&rank); /* TODO: cache this */
+
+  if (len > UINT32_MAX)
+    __hpx_errno = HPX_ERROR;
+    retval = -1;    
+  }
+
+  photon_post_recv_buffer_rdma(rank, buffer, (uint32_t)len, rank, request.photon);
+  return 0;
+}
+
+int _comm_get_photon(void* buffer, size_t len) {
+  int rank;
+  MPI_rank(&rank); /* TODO: cache this */
+
+  if (len > UINT32_MAX)
+    __hpx_errno = HPX_ERROR;
+    retval = -1;    
+  }
+
+  photon_wait_recv_buffer_rdma(rank, rank);
+  photon_post_os_put(rank, buffer, (uint32_t)len, rank, 0, request.photon);
+  photon_send_FIN(rank);
+  return 0;
+}
+
+int _comm_putget_test_photon(comm_request_t *request, int *flag, comm_status_t *status) {
+  int retval;
+  int temp;
+  retval = -1;
+
+  int type; /* I'm not actually sure what this does with photon. 0 is event, 1 is ledger but I don't know why I care */
+
+  if (status == NULL)
+    temp = photon_test(request, flag, &type, MPI_STATUS_IGNORE);
+  else
+    temp = photon_test(request, flag, &type, status.photon);
+
+  if (temp == 0)
+    retval = 0;
+  else
+    __hpx_errno = HPX_ERROR; /* TODO: replace with more specific error */
+
+  return retval;  
 }
