a3a267578bcbb47ca0d3a69e6c9e163ad6f7ec28
Luke Dalessandro <ldalessa@indiana.edu>
2013-10-04 22:19:26 -0400

    Added a nop instruction, as well as adding configure options to allow for Wall, Werror, pedantic, and lto flags. Modified the tatas lock to spin with nop.

---
+++ src/sync/nop.h
@@ -0,0 +1,24 @@
+/*
+  ====================================================================
+  High Performance ParalleX Library (libhpx)
+
+  Copyright (c) 2013, Trustees of Indiana University
+  All rights reserved.
+
+  This software may be modified and distributed under the terms of
+  the BSD license.  See the COPYING file for details.
+
+  This software was created at the Indiana University Center for
+  Research in Extreme Scale Technologies (CREST).
+  ====================================================================
+*/
+
+#pragma once
+#ifndef LIBHPX_SYNC_NOP_H_
+#define LIBHPX_SYNC_NOP_H_
+
+/* This file defines an interface to nop. */
+
+void hpx_sync_nop();
+
+#endif /* LIBHPX_SYNC_NOP_H_ */
