3c60221bde78555a0ec2096817bb101b2c37212c
Luke Dalessandro <ldalessa@indiana.edu>
2013-11-03 23:09:40 -0500

    Cleaned up the network header.
    1) Made the network barrier an implemenation-specific barrier.
    2) Made the functionality for computing the correct size for a message be implementation-specific.
    3) Removed unused, uncommented type.
    4) Added implementations for the two new virtual functions to each network class.

--- include/libhpx/network.h
+++ include/libhpx/network.h
@@ -1,5 +1,5 @@
 /*
- ====================================================================
+  ====================================================================
   High Performance ParalleX Library (libhpx)
   
   Network Functions
@@ -13,24 +13,21 @@
 
   This software was created at the Indiana University Center for
   Research in Extreme Scale Technologies (CREST).
- ====================================================================
+  ====================================================================
 */
 
 #pragma once
 #ifndef LIBHPX_NETWORK_H_
 #define LIBHPX_NETWORK_H_
 
-
-#include <stddef.h>
-
-#ifdef HAVE_CONFIG_H 
-#include <config.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>                             /* HAVE_* */
 #endif
-
+#include <stddef.h>                             /* size_t */
+#include <stdint.h>                             /* uint32_t */
 #if HAVE_MPI
 #include <mpi.h>
 #endif
-
 #if HAVE_PHOTON
 #include <photon.h>
 #endif
@@ -40,22 +37,23 @@
 #define NETWORK_ANY_SOURCE -1
 #define NETWORK_ANY_LENGTH -1
 
-#define FOURBYTE_ALIGN(n) ((n + sizeof(uint32_t) - 1) & ~(sizeof(uint32_t) - 1))
-
 /**
- * Some basic underlying network types
+ * The network request type.
  */
-
-typedef struct network_request {
+struct network_request {
 #if HAVE_MPI
   MPI_Request mpi;
 #endif
 #if HAVE_PHOTON
   uint32_t photon;
 #endif
-} network_request_t;
+};
+typedef struct network_request network_request_t;
 
-typedef struct network_status_t {
+/**
+ *
+ */
+struct network_status {
   int source;
   int count;
 #if HAVE_MPI
@@ -64,76 +62,170 @@ typedef struct network_status_t {
 #if HAVE_PHOTON
   struct photon_status_t photon;
 #endif
-} network_status_t;
+};
+typedef struct network_request network_status_t;
 
 /**
  * Network Operations interface
  *
  * This defines the abstract interface for a network implementation.
  */
-typedef struct network_ops {
-  /* Initialize the network layer */
+struct network_ops {
+  /**
+   * Initialize the network layer.
+   *
+   * @returns HPX_SUCCESS or an error condition
+   */
   int (*init)(void);
-  /* Shutdown and clean up the network layer */
-  int (*finalize)(void);
-  /* The network progress function */
-  void (*progress)(void *data);
-  int (*probe)(int src, int* flag, network_status_t* status);
-  /* Send a raw payload */
-  int (*send)(int dest, void *buffer, size_t len, network_request_t *request);
-  /* Receive a raw payload */
-  int (*recv)(int src, void *buffer, size_t len, network_request_t *request);
-  /* test for completion of send or receive */
-  int (*sendrecv_test)(network_request_t *request, int *flag, network_status_t *status);
-  /* RMA put */
-  int (*put)(int dest, void *buffer, size_t len, network_request_t *request);
-  /* RMA get */
-  int (*get)(int dest, void *buffer, size_t len, network_request_t *request); 
-  /* test for completion of communication */
-  int (*putget_test)(network_request_t *request, int *flag, network_status_t *status);
-  /* pin memory for put/get */
-  int (*pin)(void* buffer, size_t len);
-  /* unpin memory for put/get */
-  int (*unpin)(void* buffer, size_t len);
-  /* get the physical address of the current locality */
-  int (*phys_addr)(hpx_locality_t *l);
-} network_ops_t;
 
-/**
- * The concrete implementations of the network operations.
- * @{
- */
+  /**
+   * Teardown the network layer.
+   *
+   * @returns HPX_SUCCESS or an error condition
+   */
+  int (*finalize)(void);
+  
+  /**
+   * The network progress function.
+   *
+   * @param[in] data - 
+   */
+  void (*progress)(void *data);
+
+  /**
+   * Probe the network layer for events.
+   *
+   * @param[in]    src -
+   * @param[out] flags -
+   * @param[in] status -
+   *
+   * @returns HPX_SUCCESS or an error condition
+   */
+  int (*probe)(int src, int* flag, network_status_t* status);
+
+  /**
+   * Send a network message.
+   *
+   * @param[in] dest    - the destination id
+   * @param[in] buffer  - the bytes to send
+   * @param[in] len     - the number of bytes
+   * @param[in] request - a request identifier
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*send)(int dest, void *buffer, size_t len, network_request_t *request);
+
+  /**
+   * Receive a raw payload.
+   *
+   * @param[in] src     - LD: not sure
+   * @param[in] buffer  - the buffer to receive into
+   * @param[in] len     - the length to receive
+   * @param[in] request - a request identifier
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*recv)(int src, void *buffer, size_t len, network_request_t *request);
+
+  /**
+   * Test for completion of send or receive.
+   *
+   * @param[in] request -
+   * @param[out]   flag -
+   * @param[out] status -
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*sendrecv_test)(network_request_t *request, int *flag, network_status_t *status);
+
+  /**
+   * An asynchronous RMA put operation.
+   *
+   * @param[in]    dest - LD: not sure
+   * @param[in]  buffer - the buffer to send
+   * @param[in]     len - the length to send
+   * @param[in] request - a request identifier
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*put)(int dest, void *buffer, size_t len, network_request_t *request);
+
+  /**
+   * An aysnchronous RMA get operation.
+   *
+   * @param[in]    dest - LD: not sure
+   * @param[in]  buffer - the buffer to write
+   * @param[in]     len - the number of bytes
+   * @param[in] request - a request identifier
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*get)(int dest, void *buffer, size_t len, network_request_t *request); 
+
+  /**
+   * Test for completion of communication.
+   *
+   * @param[in] request - the request to check
+   * @param[out]   flag - LD: not sure
+   * @param[out] status - LD: not sure
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*putget_test)(network_request_t *request, int *flag, network_status_t *status);
+
+  /**
+   * Pin memory for put() or get().
+   *
+   * @param[in] buffer - buffer address
+   * @param[in]    len - the extent to pin
+   *
+   * @return HPX_SUCCESS or an error code
+   */
+  int (*pin)(void* buffer, size_t len);
+
+  /**
+   * Unpin memory after a put() or get().
+   *
+   * @param[in] buffer - the buffer address
+   * @param[in]    len - the extent
+   */
+  int (*unpin)(void* buffer, size_t len);
+
+  /**
+   * Get the physical address of the current locality.
+   *
+   * @param[out] locality - the locality
+   *
+   * @returns HPX_SUCCESS or an error code
+   */
+  int (*phys_addr)(hpx_locality_t *locality);
+
+  /**
+   * Adjust the number of bytes for a network operation.
+   *
+   * Some networks have strict requirements on the size of messages. For
+   * instance, the message length for ugni appears to be four bytes. This
+   * encapsulates the network-specific functionality.
+   *
+   * @param[in] n - number of bytes we want to send
+   *
+   * @returns the number of bytes that the network requires that we send.
+   */
+  size_t (*get_network_bytes)(size_t n);
+
+  /**
+   * Network-specific barrier implementation.
+   */
+  void (*barrier)(void)
+};
+typedef struct network_ops network_ops_t;
+
+/** The concrete implementations of the network operations. @{ */
 extern network_ops_t default_net_ops;
 extern network_ops_t mpi_ops;
 extern network_ops_t photon_ops;
 extern network_ops_t sim_ops;
-/**
- * @}
- */
-
-/**
- * Network Transport
- */
-typedef struct network_trans_t {
-  char           name[128];
-  network_ops_t *ops;
-  int           *flags;
-  int            active;
-} network_trans_t;
-
-typedef struct network_mgr_t {
-  /* Default transport. We do not want to walk a list when
-   * multi-railing is turned off.  */
-  network_trans_t *defaults;
-  /* Number of transports in list, since we don't walk to traverse
-     the list to know how many */
-  int count;
-  /* List of configured transports  */
-  network_trans_t *trans;
-} network_mgr_t;
-
-/* Generic network barrier */
-void hpx_network_barrier(void);
+/** @} */
 
 #endif /* LIBHPX_NETWORK_H_ */
 
