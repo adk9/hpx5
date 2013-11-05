27b3fa459040bd47facfa050d6f7be3b48071a5e
Luke Dalessandro <ldalessa@indiana.edu>
2013-11-04 20:45:41 -0500

    Broke some hpx_future functionlity into its own header/source.

--- include/hpx/future.h
+++ include/hpx/future.h
@@ -28,22 +28,35 @@
  */
 
 #include <stddef.h>                             /* size_t */
-#include "platform.h"                           /* MACROS */
-#include "types.h"                              /* struct hpx_future */
+#include "system/attributes.h"                  /* HPX MACROS */
+
+/** Forward declarations @{ */
+struct hpx_future;
+/** @} */
 
 /**
+ * Allocate a future.
  *
+ * @param[in] bytes - the number of bytes we need for the future's value
+ *
+ * @returns the future, or NULL if there was an error
  */
-struct hpx_future *hpx_future_create(void);
+struct hpx_future *hpx_future_create(size_t bytes);
+
+/**
+ * Free a future.
+ *
+ * @param[in] future - the future to free, may be NULLo
+ */
 void hpx_future_destroy(struct hpx_future *future);
 
 /**
  * Set the future, which will wake up any threads that are waiting for it. Does
  * not set a value on the future.
  *
- * @param[in] f - the future to set
+ * @param[in] future - the future to set
  */
-void hpx_future_set(struct hpx_future * const f)
+void hpx_future_set(struct hpx_future *future)
   HPX_ATTRIBUTE(HPX_NON_NULL(1));
 
 /**
@@ -52,12 +65,11 @@ void hpx_future_set(struct hpx_future * const f)
  *
  * If @p bytes > 0 then @p value != NULL.
  *
- * @param[in] f     - the future to set
- * @param[in] bytes - the number of bytes pointed to by value
- * @param[in] value - a pointer to the data
+ * @param[in] future - the future to set
+ * @param[in]  bytes - the number of bytes pointed to by value
+ * @param[in]  value - a pointer to the data
  */
-void hpx_future_setv(struct hpx_future * const f, size_t bytes,
-                     const void * const value)
+void hpx_future_setv(struct hpx_future *future, size_t bytes, const void *value)
   HPX_ATTRIBUTE(HPX_NON_NULL(1));
 
 #endif /* HPX_FUTURE_H_ */
