555f6e5857d6eb31cb0b7d8bf0b6a693e3a3e7bd
Luke Dalessandro <ldalessa@indiana.edu>
2013-11-14 13:36:04 -0500

    Modified pingpong from examples/hpx to deal with new parcel interface.

--- examples/hpx/pingpong.c
+++ examples/hpx/pingpong.c
@@ -3,12 +3,6 @@
 
 #include "hpx.h"
 
-struct pingpong_args {
-  int ping_id;
-  int pong_id;
-  char msg[128];
-};
-
 #define BUFFER_SIZE 128
 
 int other_rank;
@@ -22,76 +16,79 @@ int opt_iter_limit = 1000;
 int opt_text_ping = 0;
 int opt_screen_out = 0;
 
-static hpx_action_t ping_action;
-static hpx_action_t pong_action;
-static hpx_action_t done_action;
+static hpx_action_t ping_action = HPX_ACTION_NULL;
+static hpx_action_t pong_action = HPX_ACTION_NULL;
+static hpx_action_t done_action = HPX_ACTION_NULL;
 
-static void done(void* args) {
+static void
+done(void* args)
+{
   hpx_lco_future_set_state(&done_fut);
 }
 
-static void ping(void* args) {
-  /* int success; LD:never checked */
-  hpx_parcel_t* p;
-  struct pingpong_args* in_args = (struct pingpong_args*)args;
-  int ping_id = in_args->pong_id + 1;
-  struct pingpong_args* out_args;
+typedef struct {
+  int ping_id;
+  int pong_id;
+  char msg[128];
+} pingpong_args_t;
 
-  //  if (global_count >= opt_iter_limit) {
+static void
+ping(pingpong_args_t* args)
+{
+  int ping_id = args->pong_id + 1;
+
   if (ping_id >= opt_iter_limit) {
-    p = hpx_alloc(sizeof(hpx_parcel_t));
-    /* success = LD: never checked */ hpx_new_parcel(done_action, (void*)NULL, 0, p);
-    /* success = LD: never checked */ hpx_send_parcel(other_loc, p);
+    hpx_parcel_t *p = hpx_parcel_acquire(0);
+    hpx_parcel_set_action(p, done_action);
+    hpx_parcel_send(other_loc, p, NULL, NULL, NULL);
+    hpx_parcel_release(p);
     hpx_action_invoke(done_action, NULL, NULL);
   }
   else {
-    out_args = hpx_alloc(sizeof(struct pingpong_args));
-    if (out_args == NULL) {
-      printf("Dieing horribly - no memory!\n");
-      exit(-1);
+    hpx_parcel_t *p = hpx_parcel_acquire(sizeof(*args));
+    if (!p) {
+      fprintf(stderr, "Failed to acquire parcel in 'ping' action, aborting!\n");
+      hpx_abort();
     }
-    out_args->ping_id = ping_id;
-    out_args->msg[0] = '\0';
+    /* set pong as target action */
+    hpx_parcel_set_action(p, pong_action);
+    /* fill in pong arguments */
+    pingpong_args_t *pargs = (pingpong_args_t*)hpx_parcel_get_data(p);
+    pargs->ping_id = ping_id;
+    pargs->msg[0]  = '\0';
     if (opt_text_ping)
-      snprintf(out_args->msg, 128, "Message %d from proc 0", ping_id);
+      snprintf(pargs->msg, 128, "Message %d from proc 0", ping_id);
     if (opt_screen_out)
-      printf("Ping acting; global_count=%d, message=%s\n", global_count, out_args->msg);
-
-    p = hpx_alloc(sizeof(hpx_parcel_t));
-    /* success = LD: never checked */ hpx_new_parcel(pong_action, (void*)out_args, sizeof(struct pingpong_args), p);
-    /* success = LD: never checked */ hpx_send_parcel(other_loc, p);
+      printf("Ping acting; global_count=%d, message=%s\n", global_count, pargs->msg);    
+    hpx_parcel_send(other_loc, p, NULL, NULL, NULL);
+    hpx_parcel_release(p);
   }
   
-  hpx_free(in_args);
   global_count++;
 }
 
-void pong(void* args) {
-  /* int success; LD:never checked */
-  int str_length;
-  char copy_buffer[BUFFER_SIZE];
-  hpx_parcel_t* p;
-  struct pingpong_args* out_args;
-  struct pingpong_args* in_args = (struct pingpong_args*)args;
-  int ping_id = in_args->ping_id;
-  int pong_id = ping_id;
-
-  //  if (global_count >= opt_iter_limit - 1) {
-  if (ping_id >= opt_iter_limit) {
+static void
+pong(pingpong_args_t* args)
+{
+  if (args->ping_id >= opt_iter_limit) {
   }
   else {
-    out_args = hpx_alloc(sizeof(struct pingpong_args));
-    if (out_args == NULL) {
-      printf("Dieing horribly!\n");
-      exit(-1);
+    int pong_id = args->ping_id + 1;
+    hpx_parcel_t *p = hpx_parcel_acquire(sizeof(*args));
+    if (!p) {
+      fprintf(stderr, "Could not allocate parcel in 'pong' action, aborting!\n");
+      hpx_abort();
     }
+    hpx_parcel_set_action(p, ping_action);
+    pingpong_args_t* out_args = (pingpong_args_t*)hpx_parcel_get_data(p);
     out_args->pong_id = pong_id;
     out_args->msg[0] = '\0';
-
     if (opt_text_ping) {
+      int str_length;
+      char copy_buffer[BUFFER_SIZE];
       snprintf(copy_buffer, 50, "At %d, received from proc 0 message: '", pong_id);
       str_length = strlen(copy_buffer);
-      strcpy(&copy_buffer[str_length], in_args->msg);
+      strcpy(&copy_buffer[str_length], args->msg);
       str_length = strlen(copy_buffer);
       strcpy(&copy_buffer[str_length], "'");
       strcpy(out_args->msg, copy_buffer);
@@ -99,25 +96,21 @@ void pong(void* args) {
     
     if (opt_screen_out)
       printf("Pong acting; global_count=%d, message=%s\n", global_count, out_args->msg);
-    
-    p = hpx_alloc(sizeof(hpx_parcel_t));
-    /* success = LD: never checked */ hpx_new_parcel(ping_action, (void*)out_args, sizeof(struct pingpong_args), p);
-    /* success = LD: never checked */ hpx_send_parcel(other_loc, p);
+    hpx_parcel_send(other_loc, p, NULL, NULL, NULL);
+    hpx_parcel_release(p);
   }
 
   global_count++;
 }
 
-void pingpong(void* _args) {
-  unsigned int my_rank;
-  struct pingpong_args* args;
-
-  my_rank = my_loc->rank;
-
+static void
+pingpong(void* _args)
+{
+  unsigned int my_rank = my_loc->rank;
   hpx_lco_future_init(&done_fut);
 
   if (my_rank == 0) {
-    args = hpx_alloc(sizeof(struct pingpong_args));
+    pingpong_args_t *args = hpx_alloc(sizeof(*args));
     args->pong_id = -1;
     //    p = hpx_alloc(sizeof(hpx_parcel_t));
     //    success = hpx_new_parcel("_ping_action", (void*)args, sizeof(struct pingpong_args), p);
@@ -126,15 +119,14 @@ void pingpong(void* _args) {
   }
   else if (my_rank == other_rank) {
   }
-  else
-    {}
-
+  else {
+  }
   hpx_thread_wait(&done_fut);
-
   hpx_locality_destroy(other_loc);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   int success;
   hpx_future_t* fut;
   
@@ -169,9 +161,9 @@ int main(int argc, char** argv) {
     {}
   
   /* register action for parcel (must be done by all ranks) */
-  ping_action = hpx_action_register("_ping_action", ping);
-  pong_action = hpx_action_register("_pong_action", pong);
-  done_action = hpx_action_register("_done_action", done);
+  ping_action = hpx_action_register("_ping_action", (hpx_func_t)ping);
+  pong_action = hpx_action_register("_pong_action", (hpx_func_t)pong);
+  done_action = hpx_action_register("_done_action", (hpx_func_t)done);
   hpx_action_registration_complete();
 
   hpx_timer_t ts;
