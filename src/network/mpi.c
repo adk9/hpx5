5709431c296a2a909633949643002e5acc148563
Benjamin Martin <benjmart@indiana.edu>
2013-12-19 11:34:49 -0500

    Small changes, including fix for unnecessary pinning

--- src/network/mpi.c
+++ src/network/mpi.c
@@ -293,7 +293,7 @@ recv(int source, void* buffer, size_t len, network_request_t *request)
   retval = HPX_ERROR;
   if (source == NETWORK_ANY_SOURCE) {
     mpi_src = MPI_ANY_SOURCE;
-    tag = 0;
+    tag = -1;
   }
   else {
     mpi_src = source;
