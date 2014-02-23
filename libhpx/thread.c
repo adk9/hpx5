2f1aa88006cbee6d8baa9eaf2ce96fb853002f85
Luke Dalessandro <ldalessa@indiana.edu>
2014-02-22 20:39:35 -0500

    Just some commenting related to the transfer code.

--- libhpx/thread.c
+++ libhpx/thread.c
@@ -46,18 +46,18 @@
 #error No stack frame for your architecture
 #else
 typedef struct {
-  uint32_t     mxcsr;
-  uint16_t     fpucw;
-  uint16_t   padding;                           // has to match transfer.S
-  void          *rdi;                           // passes initial parcel
-  void          *r15;
-  void          *r14;
-  void          *r13;
-  void          *r12;
-  void          *rbx;
-  void          *rbp;
-  thread_entry_t rip;
-  void *alignment[1];
+  uint32_t     mxcsr;                           // 8
+  uint16_t     fpucw;                           // 8.5
+  uint16_t   padding;                           // 8.74 has to match transfer.S
+  void          *rdi;                           // 7 passes initial parcel
+  void          *r15;                           // 6
+  void          *r14;                           // 5
+  void          *r13;                           // 4
+  void          *r12;                           // 3
+  void          *rbx;                           // 2
+  void          *rbp;                           // 1
+  thread_entry_t rip;                           // 0
+  void *alignment[1];                           // offset for stack alignment
 } HPX_PACKED _frame_t;
 #endif
 
