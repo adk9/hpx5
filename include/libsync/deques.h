42d7753dc4967fefa505ea3d553e82c7f85e8f72
Luke Dalessandro <ldalessa@indiana.edu>
2014-03-19 21:56:06 -0400

    Modified the interface to be slightly less verbose.

--- include/libsync/deques.h
+++ include/libsync/deques.h
@@ -24,26 +24,26 @@
 
 typedef struct ws_deque {
   void (*delete)(struct ws_deque*) HPX_NON_NULL(1);
-  void (*push_left)(struct ws_deque*, void *) HPX_NON_NULL(1);
-  void *(*pop_left)(struct ws_deque*) HPX_NON_NULL(1);
-  void *(*pop_right)(struct ws_deque*) HPX_NON_NULL(1);
+  void (*push)(struct ws_deque*, void *) HPX_NON_NULL(1);
+  void *(*pop)(struct ws_deque*) HPX_NON_NULL(1);
+  void *(*steal)(struct ws_deque*) HPX_NON_NULL(1);
 } ws_deque_t;
 
 static inline HPX_NON_NULL(1) void sync_ws_deque_delete(ws_deque_t *d) {
   d->delete(d);
 }
 
-static inline HPX_NON_NULL(1) void sync_ws_deque_push_left(ws_deque_t *d,
-                                                           void *val) {
-  d->push_left(d, val);
+static inline HPX_NON_NULL(1) void sync_ws_deque_push(ws_deque_t *d, void *val)
+{
+  d->push(d, val);
 }
 
-static inline HPX_NON_NULL(1) void *sync_ws_deque_pop_left(ws_deque_t *d) {
-  return d->pop_left(d);
+static inline HPX_NON_NULL(1) void *sync_ws_deque_pop(ws_deque_t *d) {
+  return d->pop(d);
 }
 
-static inline HPX_NON_NULL(1) void *sync_ws_deque_pop_right(ws_deque_t *d) {
-  return d->pop_right(d);
+static inline HPX_NON_NULL(1) void *sync_ws_deque_steal(ws_deque_t *d) {
+  return d->steal(d);
 }
 
 typedef struct chase_lev_ws_deque {
@@ -66,14 +66,14 @@ HPX_INTERNAL void sync_chase_lev_ws_deque_fini(chase_lev_ws_deque_t *d)
 HPX_INTERNAL void sync_chase_lev_ws_deque_delete(chase_lev_ws_deque_t *d)
   HPX_NON_NULL(1);
 
-HPX_INTERNAL void sync_chase_lev_ws_deque_push_left(chase_lev_ws_deque_t *d,
-                                                    void *val)
+HPX_INTERNAL void sync_chase_lev_ws_deque_push(chase_lev_ws_deque_t *d,
+                                               void *val)
   HPX_NON_NULL(1);
 
-HPX_INTERNAL void *sync_chase_lev_ws_deque_pop_left(chase_lev_ws_deque_t *d)
+HPX_INTERNAL void *sync_chase_lev_ws_deque_pop(chase_lev_ws_deque_t *d)
   HPX_NON_NULL(1);
 
-HPX_INTERNAL void *sync_chase_lev_ws_deque_pop_right(chase_lev_ws_deque_t *d)
+HPX_INTERNAL void *sync_chase_lev_ws_deque_steal(chase_lev_ws_deque_t *d)
   HPX_NON_NULL(1);
 
 #endif // LIBHPX_DEQUES_H
