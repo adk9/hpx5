5bfd9d14ead0df82120af27328717b7a812f9913
Luke Dalessandro <ldalessa@indiana.edu>
2014-04-10 17:12:33 -0400

    Added an hpx_gas_t enumeration for configuration; extended the agas skeleton.

--- libhpx/gas/agas.c
+++ libhpx/gas/agas.c
@@ -17,7 +17,43 @@
 /// ----------------------------------------------------------------------------
 /// Agas
 /// ----------------------------------------------------------------------------
+#include "libhpx/gas.h"
+#include "addr.h"
 
 static void **_btt = NULL;                      // the block translation table
 
 
+static void _delete(gas_t *gas) {
+}
+
+static hpx_addr_t _alloc(size_t bytes, gas_t *gas) {
+  return HPX_NULL;
+}
+
+static hpx_addr_t _global_alloc(size_t n, uint32_t butes, gas_t *gas) {
+  return HPX_NULL;
+}
+
+static int _where(const hpx_addr_t addr, gas_t *gas) {
+  return addr_block_id(addr);
+}
+
+static bool _try_pin(const hpx_addr_t addr, gas_t *gas) {
+  return false;
+}
+
+static void _unpin(const hpx_addr_t addr, gas_t *gas) {
+}
+
+static gas_t agas = {
+  .delete       = _delete,
+  .alloc        = _alloc,
+  .global_alloc = _global_alloc,
+  .where        = _where,
+  .try_pin      = _try_pin,
+  .unpin        = _unpin
+};
+
+gas_t *gas_agas_new(const struct boot *boot) {
+  return &agas;
+}
