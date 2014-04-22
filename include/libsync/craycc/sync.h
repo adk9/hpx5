6309302b46dc4254bb0a2083bf86ae86a7e5d2aa
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2014-04-22 15:29:11 -0400

    Add atomics for the cray compiler.

--- include/libsync/craycc/sync.h
+++ include/libsync/craycc/sync.h
@@ -26,6 +26,48 @@
   ====================================================================
 */
 
-#error No synchronization support yet for craycc builtins.
+#include <intrinsics.h>
+
+/* no memory model */
+#define SYNC_RELAXED 0
+#define SYNC_CONSUME 0
+#define SYNC_ACQ_REL 0
+#define SYNC_SEQ_CST 0
+#define SYNC_ACQUIRE 0
+#define SYNC_RELEASE 0
+
+
+#define sync_load(val, addr, mm) do {           \
+    __builtin_ia32_lfence();                    \
+    val = *addr;                                \
+  } while (0)
+
+
+#define sync_store(addr, val, mm) do {          \
+    *addr = val;                                \
+    __builtin_ia32_sfence();                    \
+  } while (0)
+
+#define sync_swap(addr, val, mm)                \
+  __sync_val_compare_and_swap(addr, *addr, val)
+
+#define sync_cas(addr, from, to, onsuccess, onfailure) \
+  (__sync_val_compare_and_swap(addr, from, to) == from)
+
+#define sync_cas_val(addr, from, to, onsuccess, onfailure)  \
+  __sync_val_compare_and_swap(addr, from, to)
+
+#define sync_fadd(addr, val, mm) __sync_fetch_and_add(addr, val)
+
+#define sync_addf(addr, val, mm) __sync_add_and_fetch(addr, val)
+
+#define sync_fence(mm) __builtin_ia32_mfence()
+
+#define SYNC_ATOMIC(decl) volatile decl
+
+/* ../generic.h implements all of the strongly-typed versions in
+ * terms of the above generic versions.
+ */
+#include "../generic.h"
 
 #endif /* HPX_SYNC_CRAYCC_SYNC_H_ */
