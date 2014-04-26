a858c46488f7d2a46707535b5684d8f5c564847a
Luke Dalessandro <ldalessa@iu.edu>
2014-04-26 15:27:03 -0500

    Set scheduler worker affinities explicitly.
    This is necessary because mvapich2 seems to inhibit the normal placement that Linux does. There may be a way to avoid this issue, but explicitly setting affinities is fine.

--- libhpx/scheduler/worker.c
+++ libhpx/scheduler/worker.c
@@ -443,8 +443,8 @@ void *worker_run(scheduler_t *sched) {
   // initialize my worker structure
   self.thread    = pthread_self();
   self.id        = sync_fadd(&sched->next_id, 1, SYNC_ACQ_REL);
-  self.core_id   = -1; // let linux do this for now
-  // self.core_id   = self.id % here->ranks;       // round robin
+  /* self.core_id   = -1; // let linux do this for now */
+  self.core_id   = self.id % sched->cores;       // round robin
   self.seed      = self.id;
   self.backoff   = 0;
 
