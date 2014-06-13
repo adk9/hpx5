6ba7e4a820bbb189b5146348ad36f5a154cf683e
Luke Dalessandro <ldalessa@indiana.edu>
2014-06-13 09:37:56 -0400

    Added an interface for a synchronized lockable pointer.

---
+++ include/libsync/lockable_ptr.h
@@ -0,0 +1,37 @@
+/*
+  ====================================================================
+  High Performance ParalleX Library (libhpx)
+
+  Copyright (c) 2013, Trustees of Indiana University
+  All rights reserved.
+
+  This software may be modified and distributed under the terms of
+  the BSD license.  See the COPYING file for details.
+
+  This software was created at the Indiana University Center for
+  Research in Extreme Scale Technologies (CREST).
+  ====================================================================
+*/
+#ifndef HPX_SYNC_LOCKABLE_PTR_H
+#define HPX_SYNC_LOCKABLE_PTR_H
+
+#include "hpx/attributes.h"
+#include "sync.h"
+
+typedef const void * SYNC_ATOMIC() lockable_ptr_t;
+#define SYNC_LOCKABLE_PTR(T) const T * SYNC_ATOMIC()
+
+HPX_INTERNAL lockable_ptr_t *sync_lockable_ptr_lock(lockable_ptr_t *p);
+HPX_INTERNAL void sync_lockable_ptr_unlock(lockable_ptr_t *p);
+HPX_INTERNAL lockable_ptr_t *sync_lockable_ptr_read_ptr(lockable_ptr_t *p);
+
+#define SYNC_LOCKABLE_PTR_LOCK(ptr) \
+  (__typeof__(ptr))sync_lockable_ptr_lock((lockable_ptr_t*)ptr)
+
+#define SYNC_LOCKABLE_PTR_UNLOCK(ptr) \
+  sync_lockable_ptr_unlock((lockable_ptr_t*)ptr)
+
+#define SYNC_LOCKABLE_PTR_READ_PTR(ptr) \
+  (__typeof__(ptr))sync_lockable_ptr_read_ptr(ptr)
+
+#endif // HPX_SYNC_LOCKABLE_PTR_H
