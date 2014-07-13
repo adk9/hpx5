e837a8f8a5c3aabf57ab91dee694383d18bf632d
Luke Dalessandro <ldalessa@indiana.edu>
2014-07-13 13:37:16 -0400

    Updated example0 to use the common infrastructure.

---
+++ apps/lulesh/parcels/tutorial/example0/main.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+int
+main_action(const main_args_t *args)
+{
+  // output the arguments we're running with
+  printf("Number of domains: %d maxCycles: %d cores: %d\n",
+         args->nDoms, args->maxCycles, args->cores);
+  fflush(stdout);
+
+  // and shutdown (arg is error code, if necessary)
+  hpx_shutdown(0);
+}
