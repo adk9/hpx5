6756aa28b89cd2e5fff5b9844a7de3d7c1c0c225
Luke Dalessandro <ldalessa@indiana.edu>
2014-07-13 13:36:21 -0400

    Extracted common code from the examples and dumped it into a common directory.

---
+++ apps/lulesh/parcels/tutorial/common/common.mk
@@ -0,0 +1,25 @@
+CC = mpicc
+CPPFLAGS = -I../common
+CFLAGS = -g
+LIBS = -lhpx
+
+VPATH = ../common
+SRC = common.c parse.c $(shell ls *.c)
+OBJ = $(SRC:.c=.o)
+
+.PHONY: all clean help
+
+all: $(TARGET)
+
+clean:
+	$(RM) $(OBJ) $(TARGET) *~
+
+help:
+	@echo "Usage: ./$(TARGET) [nDoms] [maxcycles] [nCores]"
+
+$(TARGET): $(OBJ)
+	$(CC) -o $(TARGET) $(OBJ) $(LIBS) 
+
+%.o: %.c
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
+
