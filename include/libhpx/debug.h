2441f9876b6b6ee0ff6c1af0ba828b86144e0da0
Luke Dalessandro <ldalessa@indiana.edu>
2014-07-22 12:26:30 -0400

    Added a dbg_check to deal with error codes.

--- include/libhpx/debug.h
+++ include/libhpx/debug.h
@@ -28,9 +28,13 @@ HPX_INTERNAL int dbg_error1(unsigned line, const char *f, const char *fmt, ...) 
 #ifdef ENABLE_DEBUG
 #define dbg_log(...) dbg_log1(__LINE__, __func__, __VA_ARGS__)
 #define dbg_error(...) dbg_error1(__LINE__, __func__, __VA_ARGS__)
+
+#define dbg_check(e, ...) do { if (e != HPX_SUCCESS) dbg_error(__VA_ARGS__); } while (0)
+
 #else
 #define dbg_log(...)
 #define dbg_error(...) dbg_error1(__LINE__, __func__, __VA_ARGS__)
+#define dbg_check(e, ...) assert(e == HPX_SUCCESS)
 #endif
 
 HPX_INTERNAL void dbg_wait(void);
