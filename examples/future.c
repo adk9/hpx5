5231ecd6c1f73ab4117631b2348d94f47f1aec43
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2014-09-04 18:15:46 -0400

    Minor (stylistic) changes.

--- examples/future.c
+++ examples/future.c
@@ -1,23 +1,15 @@
-/*
- ====================================================================
-  High Performance ParalleX Library (libhpx)
-
-  Pingong example
-  examples/hpx/pingpong.c
-
-  Copyright (c) 2013, Trustees of Indiana University
-  All rights reserved.
-
-  This software may be modified and distributed under the terms of
-  the BSD license.  See the COPYING file for details.
-
-  This software was created at the Indiana University Center for
-  Research in Extreme Scale Technologies (CREST).
- ====================================================================
-*/
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
--- examples/memget.c
+++ examples/memget.c
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #define BENCHMARK "OSU HPX MEMGET Test"
 
 #include <unistd.h>
