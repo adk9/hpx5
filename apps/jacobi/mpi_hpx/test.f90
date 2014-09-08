86a45cf64a7038d58063f95ccfe45a027da1341c
Matt Anderson <andersmw@indiana.edu>
2014-09-08 14:09:03 -0400

    adding testcase

--- apps/jacobi/mpi_hpx/test.f90
+++ apps/jacobi/mpi_hpx/test.f90
@@ -1,4 +1,19 @@
             subroutine jacobi
+            implicit none
+            include 'mpif.h'
 
+            integer ngrid,ierr,np,myid
             print*,' HELLO WORLD '
+
+            call mpi_comm_size(mpi_comm_world,np,ierr)
+            call mpi_comm_rank(mpi_comm_world,myid,ierr) 
+
+            if (myid .eq. 0 ) then
+              ngrid = 59
+            endif
+
+            call mpi_bcast(ngrid,1,mpi_integer,0,mpi_comm_world,ierr)
+            print*,' TEST bcast ',ngrid
+
+
             end subroutine
