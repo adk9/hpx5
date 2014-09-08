490671e40f2248a16051f4702e1a2ba374f1a561
Benjamin Martin <benjmart@indiana.edu>
2014-09-08 13:13:47 -0400

    Fixed jacobi mpi layer

--- apps/jacobi/mpi_hpx/sendrecv.c
+++ apps/jacobi/mpi_hpx/sendrecv.c
@@ -5,27 +5,27 @@
 #include "mpi_wrapper.h"
 #include "mpi_system.h"
 
-int mpi_sendrecv(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
-                int dest, int sendtag,
-                void *recvbuf, int recvcount, MPI_Datatype recvtype,
-                int source, int recvtag,
-                MPI_Comm comm, MPI_Status *status)
+int mpi_sendrecv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
+		 int dest, int sendtag,
+		 void *recvbuf, int recvcount, MPI_Datatype recvtype,
+		 int source, int recvtag,
+		 MPI_Comm comm, MPI_Status *status)
 {
   int ierr;
-  MPI_Request   send_request,recv_request;
-  ierr=mpi_isend(sendbuf,sendcount,sendtype,
-                   dest,sendtag,MPI_COMM_WORLD_,&send_request);
-  ierr=mpi_irecv(recvbuf,recvcount,recvtype,
-                   source,recvtag,MPI_COMM_WORLD_,&recv_request);
-  ierr=mpi_wait(&send_request,&status);
-  ierr=mpi_wait(&recv_request,&status);
+  MPI_Request send_request,recv_request;
+  ierr = mpi_isend(sendbuf, sendcount, sendtype,
+		   dest, sendtag, MPI_COMM_WORLD_, &send_request);
+  ierr = mpi_irecv(recvbuf, recvcount, recvtype,
+		   source, recvtag, MPI_COMM_WORLD_, &recv_request);
+  ierr = mpi_wait(&send_request, status);
+  ierr = mpi_wait(&recv_request, status);
 }
 
-void mpi_sendrecv_(const void *sendbuf, int *fsendcount, MPI_Datatype *fsendtype,
-                int *fdest, int *fsendtag,
-                void *recvbuf, int *frecvcount, MPI_Datatype *frecvtype,
-                int *fsource, int *frecvtag,
-                MPI_Comm *fcomm, MPI_Status *status,int *ierr)
+void mpi_sendrecv_(void *sendbuf, int *fsendcount, MPI_Datatype *fsendtype,
+		   int *fdest, int *fsendtag,
+		   void *recvbuf, int *frecvcount, MPI_Datatype *frecvtype,
+		   int *fsource, int *frecvtag,
+		   MPI_Comm *fcomm, MPI_Status *status, int *ierr)
 {
   int sendcount = *fsendcount;
   MPI_Datatype sendtype = *fsendtype;
@@ -36,7 +36,7 @@ void mpi_sendrecv_(const void *sendbuf, int *fsendcount, MPI_Datatype *fsendtype
   int source = *fsource;
   int recvtag = *frecvtag;
   MPI_Comm comm = *fcomm;
-  *ierr = mpi_sendrecv(sendbuf,sendcount,sendtype,dest,sendtag
-                       recvbuf,recvcount,recvtype,source,recvtag,comm,status);
+  *ierr = mpi_sendrecv(sendbuf, sendcount, sendtype, dest, sendtag,
+                       recvbuf, recvcount, recvtype, source, recvtag, comm, status);
   return;
 }
