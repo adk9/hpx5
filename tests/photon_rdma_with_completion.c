042d735c8e2fabf03f3b35ad01320b96e384f3ac
Jayashree Candadai <jayaajay@cutter.crest.iu.edu>
2014-09-22 11:33:39 -0400

    test fixes

--- tests/photon_rdma_with_completion.c
+++ tests/photon_rdma_with_completion.c
@@ -93,6 +93,8 @@ START_TEST(test_rdma_with_completion)
   }
   fprintf(detailed_log, "\n");
 
+  ck_assert_msg(strcmp(send, recv) == 0, "Photon rdma with completion test failed");
+
   photon_unregister_buffer(send, PHOTON_SEND_SIZE);
   photon_unregister_buffer(recv, PHOTON_SEND_SIZE);
   free(send);
