d19c774db89d3940d436895fcb230d9283e5663f
Luke Dalessandro <ldalessa@indiana.edu>
2014-09-24 09:59:28 -0400

    Updated the gas_mem{get,pu} interfaces, added a memcpy, and implemented them all in terms of two-sided parcel operations.

---
+++ libhpx/gas/parcel_memcpy.c
@@ -0,0 +1,77 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+/// @file libhpx/gas/parcel_memcpy.c
+/// @brief Implements hpx_gas_memcpy() using two-sided parcels
+///
+
+#include <string.h>
+#include <hpx/attributes.h>
+#include <hpx/hpx.h>
+#include "libhpx/debug.h"
+#include "libhpx/parcel.h"
+
+static hpx_action_t _memcpy_request = 0;
+static hpx_action_t _memcpy_reply = 0;
+
+// Just copy the data into the target block.
+static int _memcpy_reply_action(void *data) {
+  hpx_addr_t target = hpx_thread_current_target();
+  char *local;
+  if (!hpx_gas_try_pin(target, (void**)&local))
+    return HPX_RESEND;
+
+  memcpy(local, data, hpx_thread_current_args_size());
+  hpx_gas_unpin(target);
+  return HPX_SUCCESS;
+}
+
+typedef struct {
+  size_t     size;
+  hpx_addr_t   to;
+  hpx_addr_t sync;
+} _memcpy_request_args_t;
+
+static int _memcpy_request_action(_memcpy_request_args_t *args) {
+  hpx_addr_t target = hpx_thread_current_target();
+  char *local;
+  if (!hpx_gas_try_pin(target, (void**)&local))
+    return HPX_RESEND;
+
+  int e = hpx_call(args->to, _memcpy_reply, local, args->size, args->sync);
+  dbg_check(e, "could not initiate a memcpy reply.\n");
+  hpx_gas_unpin(target);
+  return e;
+}
+
+static HPX_CONSTRUCTOR void _init_actions(void) {
+  _memcpy_request = HPX_REGISTER_ACTION(_memcpy_request_action);
+  _memcpy_reply = HPX_REGISTER_ACTION(_memcpy_reply_action);
+}
+
+int hpx_gas_memcpy(hpx_addr_t to, hpx_addr_t from, size_t size,
+                   hpx_addr_t sync) {
+  _memcpy_request_args_t args = {
+    .size = size,
+    .to = to,
+    .sync = sync
+  };
+
+  int e = hpx_call(from, _memcpy_request, &args, sizeof(args), HPX_NULL);
+  dbg_check(e, "Failed to initiate a memcpy request.\n");
+  return e;
+}
+
---
+++ libhpx/gas/parcel_memget.c
@@ -0,0 +1,90 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+/// @file libhpx/gas/parcel_memget.c
+/// @brief Implements hpx_gas_memget() using two-sided parcels
+///
+
+#include <string.h>
+#include <hpx/attributes.h>
+#include <hpx/hpx.h>
+#include "libhpx/debug.h"
+#include "libhpx/parcel.h"
+
+static hpx_action_t _memget_request = 0;
+static hpx_action_t _memget_reply = 0;
+
+typedef struct {
+  void    *to;
+  char from[];
+} _memget_reply_args_t;
+
+static int _memget_reply_action(_memget_reply_args_t *args) {
+  memcpy(args->to, &args->from,
+         hpx_thread_current_args_size() - sizeof(args->to));
+  return HPX_SUCCESS;
+}
+
+
+typedef struct {
+  size_t      size;
+  void         *to;
+  hpx_addr_t lsync;
+  hpx_addr_t reply;
+} _memget_request_args_t;
+
+
+static int _memget_request_action(_memget_request_args_t *args) {
+  hpx_addr_t target = hpx_thread_current_target();
+  char *local;
+  if (!hpx_gas_try_pin(target, (void**)&local))
+    return HPX_RESEND;
+
+  hpx_parcel_t *p = hpx_parcel_acquire(NULL, sizeof(_memget_reply_args_t) +
+                                       args->size);
+  assert(p);
+
+  p->action = _memget_reply;
+  p->target = args->reply;
+  p->c_action = hpx_lco_set_action;
+  p->c_target = args->lsync;
+
+  _memget_reply_args_t *repargs = hpx_parcel_get_data(p);
+  repargs->to = args->to;
+  memcpy(&repargs->from, local, args->size);
+  hpx_gas_unpin(target);
+  hpx_parcel_send_sync(p);
+  return HPX_SUCCESS;
+}
+
+static HPX_CONSTRUCTOR void _init_actions(void) {
+  _memget_request = HPX_REGISTER_ACTION(_memget_request_action);
+  _memget_reply = HPX_REGISTER_ACTION(_memget_reply_action);
+}
+
+int hpx_gas_memget(void *to, hpx_addr_t from, size_t size, hpx_addr_t lsync) {
+  _memget_request_args_t args = {
+    .size = size,
+    .to = to,
+    .lsync = lsync,
+    .reply = HPX_HERE
+  };
+
+  int e = hpx_call(from, _memget_request, &args, sizeof(args), HPX_NULL);
+  dbg_check(e, "Failed to initiate a memget request.\n");
+  return e;
+}
+
