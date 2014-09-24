0003b7b4bfaa0871dfb0f38a6efda6625024132a
Luke Dalessandro <ldalessa@indiana.edu>
2014-09-24 14:54:34 -0400

    Simplified memget example.

--- tests/unit/13_TestMemput.c
+++ tests/unit/13_TestMemput.c
@@ -3,7 +3,7 @@
 // @Project       High Performance ParallelX Library (libhpx)
 //----------------------------------------------------------------------------
 // @Subject       Library Unit Test Harness - Memput
-// 
+//
 // @Compiler      GCC
 // @OS            Linux
 // @Description   Future based memput test
@@ -14,13 +14,13 @@
 //                This software may be modified and distributed under the terms
 //                of the BSD license.  See the COPYING file for details.
 //
-//                This software was created at the Indiana University Center 
+//                This software was created at the Indiana University Center
 //                for Research in Extreme Scale Technologies (CREST).
 //----------------------------------------------------------------------------
 // @Date          09/23/2014
 // @Author        Jayashree Candadai <jayaajay [at] indiana.edu>
 // @Version       0.1
-// Commands to Run: make, mpirun hpxtest 
+// Commands to Run: make, mpirun hpxtest
 //****************************************************************************
 
 //****************************************************************************
@@ -40,15 +40,13 @@ int t13_memput_verify_action(void *args) {
   if (!hpx_gas_try_pin(target, (void**)&local))
     return HPX_RESEND;
 
-  uint64_t result = false;
+  bool result = false;
   const size_t BLOCK_ELEMS = sizeof(block) / sizeof(block[0]);
-  for (int i = 0; i < BLOCK_ELEMS; ++i){
-    if (local[i] != block[i]) {
-      result = true;
-    }
-  }                 
+  for (int i = 0; i < BLOCK_ELEMS; ++i)
+    result |= (local[i] != block[i]);
+
   hpx_gas_unpin(target);
-  hpx_thread_continue(sizeof(uint64_t), &result);
+  HPX_THREAD_CONTINUE(result);
 }
 
 //****************************************************************************
@@ -60,7 +58,7 @@ START_TEST (test_libhpx_memput)
   int rank = HPX_LOCALITY_ID;
   int size = HPX_LOCALITIES;
   int peerid = (rank+1)%size;
- 
+
   hpx_addr_t data = hpx_gas_global_alloc(size, sizeof(block));
   hpx_addr_t remote = hpx_addr_add(data, peerid * sizeof(block));
 
@@ -74,15 +72,12 @@ START_TEST (test_libhpx_memput)
   hpx_lco_delete(remoteComplete, HPX_NULL);
   printf(" Elapsed: %g\n", hpx_time_elapsed_ms(t1));
 
-  uint64_t output;
-  hpx_addr_t done = hpx_lco_future_new(0);
-  hpx_call(remote, t13_memput_verify, NULL, 0, done);
-  hpx_lco_wait(done);
-  
-  hpx_lco_get(done, sizeof(output), &output);
+  bool output = false;
+  int e = hpx_call_sync(remote, t13_memput_verify, NULL, 0,
+                        &output, sizeof(output));
+  ck_assert_msg(e == HPX_SUCCESS, "hpx_call_sync failed with %d", e);
   ck_assert_msg(output == false, "gas_memput failed");
-  hpx_lco_delete(done, HPX_NULL);
-} 
+}
 END_TEST
 
 //****************************************************************************
