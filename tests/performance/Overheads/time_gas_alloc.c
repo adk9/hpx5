102742e1ee4f65ff7ae39d841a97c4452b6b8e20
Jayashree Candadai <jayaajay@cutter.crest.iu.edu>
2014-10-02 16:40:00 -0400

    perf tests

---
+++ tests/performance/Overheads/time_gas_alloc.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include "hpx/hpx.h"
+#include "common.h"
+
+#define MAX_BYTES        1073741824 //1gb
+#define SKIP_LARGE       10
+#define LOOP_LARGE       100
+
+int skip = 1000;
+int loop = 10000;
+
+#define BENCHMARK "HPX COST OF GAS ALLOCATIONS (ms)"
+
+#define HEADER "# " BENCHMARK "\n"
+#define FIELD_WIDTH 10
+
+/// This file tests cost of GAS operations
+static void usage(FILE *stream) {
+  fprintf(stream, "Usage:  [options]\n"
+          "\t-c, number of cores to run on\n"
+          "\t-t, number of scheduler threads\n"
+          "\t-D, all localities wait for debugger\n"
+          "\t-d, wait for debugger at specific locality\n"
+          "\t-l, logging level\n"
+          "\t-h, this help display\n");
+}
+
+static hpx_action_t _main    = 0;
+
+static int _main_action(void *args) {
+  hpx_addr_t local, global, calloc_global;
+  hpx_time_t t;
+
+  fprintf(test_log, HEADER);
+  fprintf(test_log, "%s\t\t%*s%*s%*s%*s%*s%*s\n", "# Size ", FIELD_WIDTH, " LOCAL_ALLOC ", FIELD_WIDTH, " FREE ", FIELD_WIDTH, "  GLOBAL_ALLOC ", FIELD_WIDTH, " FREE ", FIELD_WIDTH, " GLOBAL_CALLOC ", FIELD_WIDTH, " FREE ");
+
+  for (size_t size = 1; size <= MAX_BYTES; size*=2) {
+    t = hpx_time_now();
+    local = hpx_gas_alloc(size);
+    fprintf(test_log, "%-*lu%*g\t", 10,  size, FIELD_WIDTH, hpx_time_elapsed_ms(t));
+   
+    t = hpx_time_now();
+    hpx_gas_free(local, HPX_NULL);
+    fprintf(test_log, "%*g", FIELD_WIDTH, hpx_time_elapsed_ms(t));
+
+    t = hpx_time_now();
+    global = hpx_gas_global_alloc(hpx_get_num_ranks(), size);
+    fprintf(test_log, "%*g", FIELD_WIDTH, hpx_time_elapsed_ms(t));   
+
+    t = hpx_time_now();
+    hpx_gas_free(global, HPX_NULL);
+    fprintf(test_log, "%*g\t", FIELD_WIDTH, hpx_time_elapsed_ms(t));
+
+    t = hpx_time_now();
+    calloc_global = hpx_gas_global_calloc(hpx_get_num_ranks(), size);
+    fprintf(test_log, "%*g\t", FIELD_WIDTH, hpx_time_elapsed_ms(t));
+
+    t = hpx_time_now();
+    hpx_gas_free(calloc_global, HPX_NULL);
+    fprintf(test_log, "%*g", FIELD_WIDTH, hpx_time_elapsed_ms(t));
+    fprintf(test_log, "\n");
+  }
+  hpx_shutdown(HPX_SUCCESS);
+}
+
+int
+main(int argc, char *argv[])
+{
+  hpx_config_t cfg = HPX_CONFIG_DEFAULTS;
+
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "c:t:d:Dl:h")) != -1) {
+    switch (opt) {
+     case 'c':
+      cfg.cores = atoi(optarg);
+      break;
+     case 't':
+      cfg.threads = atoi(optarg);
+      break;
+     case 'D':
+      cfg.wait = HPX_WAIT;
+      cfg.wait_at = HPX_LOCALITY_ALL;
+      break;
+     case 'd':
+      cfg.wait = HPX_WAIT;
+      cfg.wait_at = atoi(optarg);
+      break;
+     case 'l':
+      cfg.log_level = atoi(optarg);
+      break;
+     case 'h':
+      usage(stdout);
+      return 0;
+     case '?':
+     default:
+      usage(stderr);
+      return -1;
+    }
+  }
+
+  if (hpx_init(&cfg)) {
+    fprintf(stderr, "HPX: failed to initialize.\n");
+    return 1;
+  }
+
+  test_log = fopen("test.log", "a+");
+  fprintf(test_log, "Starting the cost of GAS Allocation benchmark\n");
+
+  // Register the main action
+  _main    = HPX_REGISTER_ACTION(_main_action);
+
+  // run the main action
+  return hpx_run(_main, NULL, 0);
+}
---
+++ tests/performance/PutGetSM/run_tests.sh
@@ -0,0 +1,2 @@
+mpirun perfmain
+mpirun guppie -t 2 10 12
