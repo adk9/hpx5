6b3d107778e59b17bb7e0d9953a6702d0e641382
Luke Dalessandro <ldalessa@indiana.edu>
2014-10-03 07:09:29 -0400

    Added tracking of the number of pending sends and recvs, and the hooks for controlling when we initiate new transport operations.

--- libhpx/transport/progress.c
+++ libhpx/transport/progress.c
@@ -33,256 +33,282 @@
 
 request_t *request_init(request_t *request, hpx_parcel_t *p) {
   request->parcel = p;
-  return request;
-}
+   return request;
+ }
 
-request_t *request_new(hpx_parcel_t *p, int request) {
-  request_t *r = malloc(sizeof(*r) + request);
-  if (!r) {
-    dbg_error("could not allocate request.\n");
-    return NULL;
-  }
-  return request_init(r, p);
-}
+ request_t *request_new(hpx_parcel_t *p, int request) {
+   request_t *r = malloc(sizeof(*r) + request);
+   if (!r) {
+     dbg_error("could not allocate request.\n");
+     return NULL;
+   }
+   return request_init(r, p);
+ }
 
-void request_delete(request_t *r) {
-  if (!r)
-    return;
-  free(r);
-}
-
-
-/// Requests associate parcels to transport send and receive operations, and
-/// list nodes as well. This constructor creates new requests, bound to the
-/// passed parcel.
-///
-/// We try to abstract the transport request interface so that we're not
-/// exposing any transport-specific types in headers. The transport request
-/// (i.e., the request identifier that we can probe with later), is abstracted
-/// as a byte array.
-///
-/// This allocator mallocs enough space for the current transport's request
-/// size.
-static HPX_MALLOC request_t *_new_request(progress_t *progress, hpx_parcel_t *p) {
-  request_t *r = progress->free;
-  if (r) {
-    progress->free = progress->free->next;
-    request_init(r, p);
-  }
-  else {
-    int bytes = transport_request_size(here->transport);
-    r = request_new(p, bytes);
-  }
-  return r;
-}
+ void request_delete(request_t *r) {
+   if (!r)
+     return;
+   free(r);
+ }
 
 
-/// Frees a previously allocated request.
-///
-/// This uses a freelist algorithm for request nodes.
-///
-/// @param progress - the progress object
-/// @param request  - the request
-static void _delete_request(progress_t *progress, request_t *request) {
-  request->next = progress->free;
-  progress->free = request->next;
-}
+ /// Requests associate parcels to transport send and receive operations, and
+ /// list nodes as well. This constructor creates new requests, bound to the
+ /// passed parcel.
+ ///
+ /// We try to abstract the transport request interface so that we're not
+ /// exposing any transport-specific types in headers. The transport request
+ /// (i.e., the request identifier that we can probe with later), is abstracted
+ /// as a byte array.
+ ///
+ /// This allocator mallocs enough space for the current transport's request
+ /// size.
+ static HPX_MALLOC request_t *_new_request(progress_t *progress, hpx_parcel_t *p) {
+   request_t *r = progress->free;
+   if (r) {
+     progress->free = progress->free->next;
+     request_init(r, p);
+   }
+   else {
+     int bytes = transport_request_size(here->transport);
+     r = request_new(p, bytes);
+   }
+   return r;
+ }
 
 
-/// Finish a generic request.
-static void _finish_request(progress_t *progress, request_t *r) {
-  _delete_request(progress, r);
-}
+ /// Frees a previously allocated request.
+ ///
+ /// This uses a freelist algorithm for request nodes.
+ ///
+ /// @param progress - the progress object
+ /// @param request  - the request
+ static void _delete_request(progress_t *progress, request_t *request) {
+   request->next = progress->free;
+   progress->free = request->next;
+ }
 
 
-/// Finish a send request.
-///
-/// This finishes a send by freeing the request's parcel, and then calling the
-/// generic finish handler.
-///
-/// @param progress - the progress object
-/// @param        r - the request to finish
-static void _finish_send(progress_t *progress, request_t *r) {
-  assert(r);
-  hpx_parcel_release(r->parcel);
-  _finish_request(progress, r);
-}
+ /// Finish a generic request.
+ static void _finish_request(progress_t *progress, request_t *r) {
+   _delete_request(progress, r);
+ }
 
 
-/// Finish a receive request.
-///
-/// This finishes a receive by pushing the request's parcel into the receive
-/// queue, and then calling the generic finish handler.
-///
-/// @param progress - The progress object.
-/// @param        r - The request to finish.
-static void _finish_recv(progress_t *progress, request_t *r) {
-  scheduler_spawn(r->parcel);
-  _finish_request(progress, r);
-}
+ /// Finish a send request.
+ ///
+ /// This finishes a send by freeing the request's parcel, and then calling the
+ /// generic finish handler.
+ ///
+ /// @param progress - the progress object
+ /// @param        r - the request to finish
+ static void _finish_send(progress_t *progress, request_t *r) {
+   assert(r);
+   hpx_parcel_release(r->parcel);
+   _finish_request(progress, r);
+ }
 
 
-/// Called during network progress to initiate a send with the transport.
-///
-/// Try and pop a network request off of the send queue, allocate a request node
-/// for it, and initiate a byte-send with the transport.
-///
-/// @param      progress - The progress object
-///
-/// @returns true if we initiated a send
-static bool _try_start_send(progress_t *progress) {
-  uint32_t dest;
-  int size;
-  hpx_parcel_t *p = network_tx_dequeue(here->network);
-  if (!p)
-    return false;
-
-  request_t *r = _new_request(progress, p);
-  if (!r) {
-    dbg_error("could not allocate a network request.\n");
-    goto unwind0;
-  }
-
-  dest = btt_owner(here->btt, p->target);
-  size = sizeof(*p) + p->size;
-  if (transport_send(here->transport, dest, p, size, &r->request)) {
-    dbg_error("transport failed send.\n");
-    goto unwind1;
-  }
-
-  r->next = progress->pending_sends;
-  progress->pending_sends = r;
-  return true;
-
- unwind1:
-  _delete_request(progress, r);
- unwind0:
-  hpx_parcel_release(p);
-  return false;
-}
+ /// Finish a receive request.
+ ///
+ /// This finishes a receive by pushing the request's parcel into the receive
+ /// queue, and then calling the generic finish handler.
+ ///
+ /// @param progress - The progress object.
+ /// @param        r - The request to finish.
+ static void _finish_recv(progress_t *progress, request_t *r) {
+   scheduler_spawn(r->parcel);
+   _finish_request(progress, r);
+ }
 
 
-/// Called during network progress to initiate a recv with the transport.
-static bool _try_start_recv(progress_t *progress) {
-  int src = TRANSPORT_ANY_SOURCE;
-  int size = transport_probe(here->transport, &src);
-  if (size < 1)
-    return false;
+ /// Called during network progress to initiate a send with the transport.
+ ///
+ /// Try and pop a network request off of the send queue, allocate a request node
+ /// for it, and initiate a byte-send with the transport.
+ ///
+ /// @param      progress - The progress object
+ ///
+ /// @returns 1 if we initiated a send
+ static int _try_start_send(progress_t *progress) {
+   uint32_t dest;
+   int size;
+   hpx_parcel_t *p = network_tx_dequeue(here->network);
+   if (!p)
+     return 0;
 
-  // make sure things happened like we expected
-  assert(src >= 0);
-  assert(src < here->ranks);
-  assert(src != TRANSPORT_ANY_SOURCE);
-  assert(size > 0);
+   request_t *r = _new_request(progress, p);
+   if (!r) {
+     dbg_error("could not allocate a network request.\n");
+     goto unwind0;
+   }
 
-  // allocate a parcel to provide the buffer to receive into
-  hpx_parcel_t *p = hpx_parcel_acquire(NULL, size - sizeof(*p));
-  if (!p) {
-    dbg_error("could not acquire a parcel of size %d during receive.\n", size);
-    return false;
-  }
+   dest = btt_owner(here->btt, p->target);
+   size = sizeof(*p) + p->size;
+   if (transport_send(here->transport, dest, p, size, &r->request)) {
+     dbg_error("transport failed send.\n");
+     goto unwind1;
+   }
 
-  // remember the source
-  p->src = src;
+   r->next = progress->pending_sends;
+   progress->pending_sends = r;
+   return 1;
 
-  // allocate a request to track this transport operation
-  request_t *r = _new_request(progress, p);
-  if (!r) {
-    dbg_error("could not allocate a network request.\n");
-    goto unwind0;
-  }
-
-  // get the buffer inside the parcel that we're supposed to receive to, and
-  // perform the receive
-  if (transport_recv(here->transport, src, p, size, &r->request)) {
-    dbg_error("could not receive from transport.\n");
-    goto unwind1;
-  }
-
-  // remember that this receive is pending so that we can poll it later
-  r->next = progress->pending_recvs;
-  progress->pending_recvs = r;
-  return true;
-
- unwind1:
-  _delete_request(progress, r);
- unwind0:
-  hpx_parcel_release(p);
-  return false;
-}
+  unwind1:
+   _delete_request(progress, r);
+  unwind0:
+   hpx_parcel_release(p);
+   return 0;
+ }
 
 
-/// Test a list of results.
-///
-/// @param   progress The progress object used for testing.
-/// @param     finish A callback to finish the request.
-/// @param       curr The current request to test.
-/// @param          n The current number of completed requests.
-///
-/// @returns The total number of completed requests.
-static int _test(progress_t *p, request_t **i,
-                 void (*finish)(progress_t*, request_t*)) {
-  transport_class_t *t = here->transport;
-  int n = 0;
-  while (*i != NULL) {
-    request_t *j = *i;
-    int complete = 0;
-    int e = transport_test(t, &j->request, &complete);
-    dbg_check(e, "transport test failed");
+ /// Called during network progress to initiate a recv with the transport.
+ static int _try_start_recv(progress_t *progress) {
+   int src = TRANSPORT_ANY_SOURCE;
+   int size = transport_probe(here->transport, &src);
+   if (size < 1)
+     return 0;
 
-    if (!complete) {
-      i = &j->next;
-    }
-    else {
-      *i = j->next;
-      finish(p, j);
-      ++n;
-    }
-  }
-  return n;
-}
+   // make sure things happened like we expected
+   assert(src >= 0);
+   assert(src < here->ranks);
+   assert(src != TRANSPORT_ANY_SOURCE);
+   assert(size > 0);
+
+   // allocate a parcel to provide the buffer to receive into
+   hpx_parcel_t *p = hpx_parcel_acquire(NULL, size - sizeof(*p));
+   if (!p) {
+     dbg_error("could not acquire a parcel of size %d during receive.\n", size);
+     return 0;
+   }
+
+   // remember the source
+   p->src = src;
+
+   // allocate a request to track this transport operation
+   request_t *r = _new_request(progress, p);
+   if (!r) {
+     dbg_error("could not allocate a network request.\n");
+     goto unwind0;
+   }
+
+   // get the buffer inside the parcel that we're supposed to receive to, and
+   // perform the receive
+   if (transport_recv(here->transport, src, p, size, &r->request)) {
+     dbg_error("could not receive from transport.\n");
+     goto unwind1;
+   }
+
+   // remember that this receive is pending so that we can poll it later
+   r->next = progress->pending_recvs;
+   progress->pending_recvs = r;
+   return 1;
+
+  unwind1:
+   _delete_request(progress, r);
+  unwind0:
+   hpx_parcel_release(p);
+   return 0;
+ }
 
 
-/// This call tries to "flush" the transport progress queues. It ensures that
-/// all of the pending sends are finished. This is particularly useful during
-/// shutdown where we need the "shutdown-action" parcels to go out before
-/// shutting down the scheduler.
-void network_progress_flush(progress_t *p) {
-  bool send = true;
-  while (send)
-    send = _try_start_send(p);
+ /// Test a list of results.
+ ///
+ /// @param   progress The progress object used for testing.
+ /// @param     finish A callback to finish the request.
+ /// @param       curr The current request to test.
+ /// @param          n The current number of completed requests.
+ ///
+ /// @returns The total number of completed requests.
+ static int _test(progress_t *p, request_t **i,
+                  void (*finish)(progress_t*, request_t*)) {
+   transport_class_t *t = here->transport;
+   int n = 0;
+   while (*i != NULL) {
+     request_t *j = *i;
+     int complete = 0;
+     int e = transport_test(t, &j->request, &complete);
+     dbg_check(e, "transport test failed");
 
-  // flush the pending sends
-  while (p->pending_sends)
-    _test(p, &p->pending_sends, _finish_send);
+     if (!complete) {
+       i = &j->next;
+     }
+     else {
+       *i = j->next;
+       finish(p, j);
+       ++n;
+     }
+   }
+   return n;
+ }
 
-  // if we have any pending receives, we wait for those to finish as well
-  while (p->pending_recvs)
-    _test(p, &p->pending_recvs, _finish_recv);
-}
 
-void network_progress_poll(progress_t *p) {
-  int sends = _test(p, &p->pending_sends, _finish_send);
-  if (sends)
-    dbg_log_trans("progress: finished %d sends.\n", sends);
+ /// This call tries to "flush" the transport progress queues. It ensures that
+ /// all of the pending sends are finished. This is particularly useful during
+ /// shutdown where we need the "shutdown-action" parcels to go out before
+ /// shutting down the scheduler.
+ void network_progress_flush(progress_t *p) {
+   bool send = true;
+   while (send)
+     send = _try_start_send(p);
 
-  bool send;
-  while ((send = _try_start_send(p)))
-    dbg_log_trans("progress: started a send.\n");
+   // flush the pending sends
+   while (p->pending_sends)
+     _test(p, &p->pending_sends, _finish_send);
 
-  int recvs = _test(p, &p->pending_recvs, _finish_recv);
-  if (recvs)
-    dbg_log_trans("progress: finished %d receives.\n", recvs);
+   // if we have any pending receives, we wait for those to finish as well
+   while (p->pending_recvs)
+     _test(p, &p->pending_recvs, _finish_recv);
+ }
 
-  bool recv;
-  if ((recv = _try_start_recv(p)))
-    dbg_log_trans("progress: started a recv.\n");
+ void network_progress_poll(progress_t *p) {
+   int sends = 0;
+   do {
+     sends = _test(p, &p->pending_sends, _finish_send);
+     assert(sends <= p->npsends);
+     p->npsends -= sends;
+     DEBUG_IF (sends) {
+       dbg_log_trans("progress: finished %d sends.\n", sends);
+     }
+   } while (network_progress_drain_sends(p));
+
+   int send = 1;
+   while (send && network_progress_can_recv(p)) {
+     send = _try_start_send(p);
+     p->npsends += send;
+     DEBUG_IF(send) {
+       dbg_log_trans("progress: started a send.\n");
+     }
+   }
+
+   int recvs = 0;
+   do {
+     recvs = _test(p, &p->pending_recvs, _finish_recv);
+     assert(recvs <= p->nprecvs);
+     p->nprecvs -= recvs;
+     DEBUG_IF (recvs) {
+       dbg_log_trans("progress: finished %d receives.\n", recvs);
+     }
+   } while (network_progress_drain_recvs(p));
+
+   int recv = 1;
+   while (recv && network_progress_can_recv(p)) {
+     recv = _try_start_recv(p);
+     p->nprecvs += recv;
+     DEBUG_IF (recv) {
+       dbg_log_trans("progress: started a recv.\n");
+     }
+   }
 }
 
 progress_t *network_progress_new() {
   progress_t *p = malloc(sizeof(*p));
   assert(p);
+  p->psend_limit   = UINT32_MAX;
+  p->npsends       = 0;
   p->pending_sends = NULL;
+  p->precv_limit   = UINT32_MAX;
+  p->nprecvs       = 0;
   p->pending_recvs = NULL;
   p->free          = NULL;
   return p;
--- libhpx/transport/progress.h
+++ libhpx/transport/progress.h
@@ -27,25 +27,44 @@ struct request {
 typedef struct progress progress_t;
 struct progress {
   request_t         *free;              // request freelist
+  uint32_t             psend_limit;       //
+  uint32_t             npsends;           //
   request_t         *pending_sends;     // outstanding send requests
+  uint32_t             precv_limit;       //
+  uint32_t             nprecvs;           //
   request_t         *pending_recvs;     // outstanding recv requests
 };
 
 
-HPX_INTERNAL progress_t *network_progress_new(void)
-  HPX_MALLOC;
+progress_t *network_progress_new(void)
+  HPX_INTERNAL HPX_MALLOC;
 
 
-HPX_INTERNAL void network_progress_poll(progress_t *p)
-  HPX_NON_NULL(1);
+void network_progress_poll(progress_t *p)
+  HPX_INTERNAL HPX_NON_NULL(1);
 
 
-HPX_INTERNAL void network_progress_flush(progress_t *p)
-  HPX_NON_NULL(1);
+void network_progress_flush(progress_t *p)
+  HPX_INTERNAL HPX_NON_NULL(1);
 
 
-HPX_INTERNAL void network_progress_delete(progress_t *p)
-  HPX_NON_NULL(1);
+void network_progress_delete(progress_t *p)
+  HPX_INTERNAL HPX_NON_NULL(1);
 
+static inline int network_progress_can_send(progress_t *p) {
+  return (p->npsends < p->psend_limit);
+}
+
+static inline int network_progress_can_recv(progress_t *p) {
+  return (p->nprecvs < p->precv_limit);
+}
+
+static inline int network_progress_drain_sends(progress_t *p) {
+  return !network_progress_can_send(p);
+}
+
+static inline int network_progress_drain_recvs(progress_t *p) {
+  return !network_progress_can_recv(p);
+}
 
 #endif // LIBHPX_TRANSPORT_PROGRESS_H
