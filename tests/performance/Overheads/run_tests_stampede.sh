561dc4fc082e5233d132c09cce3e36a716963fd2
Jayashree Candadai <jayaajay@cutter.crest.iu.edu>
2014-10-10 17:24:19 -0400

    script parameter changes for stampede

--- tests/performance/Overheads/run_tests_stampede.sh
+++ tests/performance/Overheads/run_tests_stampede.sh
@@ -1,20 +1,21 @@
 ibrun perfmain
-ibrun -np 2 time_gas_alloc
-ibrun -np 2 time_gas_addr_trans
+ibrun -np 1 time_gas_alloc
+ibrun -np 1 time_gas_addr_trans
 ibrun -np 2 time_lco_chan
-ibrun -np 2 time_lco_sema
-#ibrun time_lco_and
-ibrun -np 2 time_lco_future
-ibrun -np 2 time_lco_allgather 10
-ibrun -np 2 time_lco_allgather 100
-ibrun -np 2 time_lco_allgather 1000
-ibrun -np 2 time_lco_allgather 10000
-ibrun -np 2 time_lco_allreduce 10
-ibrun -np 2 time_lco_allreduce 100
-ibrun -np 2 time_lco_allreduce 1000
-ibrun -np 2 time_lco_allreduce 10000
-ibrun -np 2 time_lco_alltoall 10
-ibrun -np 2 time_lco_alltoall 100
-ibrun -np 2 time_lco_alltoall 1000
+ibrun -np 1 time_lco_sema
+ibrun -np 1 time_lco_and
+ibrun -np 1 time_lco_future
+ibrun -np 1 time_lco_allgather 10
+ibrun -np 1 time_lco_allgather 100
+ibrun -np 1 time_lco_allgather 1000
+ibrun -np 1 time_lco_allgather 10000
+ibrun -np 1 time_lco_allreduce 10
+ibrun -np 1 time_lco_allreduce 100
+ibrun -np 1 time_lco_allreduce 1000
+ibrun -np 1 time_lco_allreduce 10000
+ibrun -np 1 time_lco_alltoall 10
+ibrun -np 1 time_lco_alltoall 100
+ibrun -np 1 time_lco_alltoall 1000
 ibrun -np 2 time_lco_netfutures
 ibrun -np 2 time_lco_netfutures_msgSize -c 1
+~                                              
