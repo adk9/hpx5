45424ec34e40e2c79bf24a6aa97f06c6c77a13ac
Jayashree Candadai <jayaajay@indiana.edu>
2014-10-10 12:47:52 -0400

    modified chmod for one of the script

--- tests/performance/Overheads/run_tests_br2.sh
+++ tests/performance/Overheads/run_tests_br2.sh
@@ -7,13 +7,10 @@ aprun  -n 2 -N 1 -d 32 ./time_lco_future -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 
 aprun  -n 2 -N 1 -d 32 ./time_lco_allgather -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 10
 aprun  -n 2 -N 1 -d 32 ./time_lco_allgather -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 100
 aprun  -n 2 -N 1 -d 32 ./time_lco_allgather -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 1000
-aprun  -n 2 -N 1 -d 32 ./time_lco_allgather -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 10000
 aprun  -n 2 -N 1 -d 32 ./time_lco_allreduce -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 10
 aprun  -n 2 -N 1 -d 32 ./time_lco_allreduce -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 100
 aprun  -n 2 -N 1 -d 32 ./time_lco_allreduce -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 1000
-aprun  -n 2 -N 1 -d 32 ./time_lco_allreduce -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50)) 10000
 aprun  -n 2 -N 1 -d 32 ./time_lco_alltoall -c 16 -T 0 -s 4097 -p $((1024 * 1024* 1024 * 50)) 10
 aprun  -n 2 -N 1 -d 32 ./time_lco_alltoall -c 16 -T 0 -s 4097 -p $((1024 * 1024* 1024 * 50)) 100
 aprun  -n 2 -N 1 -d 32 ./time_lco_alltoall -c 16 -T 0 -s 4097 -p $((1024 * 1024* 1024 * 50)) 1000
-aprun  -n 2 -N 1 -d 32 ./time_lco_alltoall -c 16 -T 0 -s 4097 -p $((1024 * 1024* 1024 * 50)) 10000
-aprun -n 2 -N 1 -d 32 ./time_lco_netfutures -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50))
+#aprun -n 2 -N 1 -d 32 ./time_lco_netfutures -c 16 -T 0 -s 4097 -p $((1024 * 1024 * 1024 * 50))
