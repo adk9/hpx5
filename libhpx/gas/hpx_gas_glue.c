f7c383cf56f97ba143adff0c0dc80a25a89b67a9
Luke Dalessandro <ldalessa@indiana.edu>
2014-10-15 10:17:54 -0400

    Cleaned up the hpx/addr.h header to allow 64bit displacements and expose gva subtraction.

--- libhpx/gas/hpx_gas_glue.c
+++ libhpx/gas/hpx_gas_glue.c
@@ -30,9 +30,14 @@ bool hpx_addr_eq(const hpx_addr_t lhs, const hpx_addr_t rhs) {
   return (lhs == rhs);
 }
 
-hpx_addr_t hpx_addr_add(const hpx_addr_t addr, int bytes, uint32_t block_size) {
-  assert(here && here->gas);
-  return here->gas->add(addr, bytes, block_size);
+hpx_addr_t hpx_addr_add(hpx_addr_t addr, int64_t bytes, uint32_t bsize) {
+  assert(here && here->gas && here->gas->add);
+  return here->gas->add(addr, bytes, bsize);
+}
+
+int64_t hpx_addr_sub(hpx_addr_t lhs, hpx_addr_t rhs, uint32_t bsize) {
+  assert(here && here->gas && here->gas->sub);
+  return here->gas->sub(lhs, rhs, bsize);
 }
 
 bool hpx_gas_try_pin(const hpx_addr_t addr, void **local) {
