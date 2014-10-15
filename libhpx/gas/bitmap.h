e7330dfa3d54e0f2a19353a45111ec914d02cf22
Luke Dalessandro <ldalessa@indiana.edu>
2014-10-15 16:06:21 -0400

    Cleaned up and commented the bitmap implementation. Fixed some corner case allocation issues that might have manifested in artificail resource constraints.

--- libhpx/gas/bitmap.h
+++ libhpx/gas/bitmap.h
@@ -28,44 +28,20 @@
 /// functionality.
 ///
 /// In our case, we want jemalloc to use chunks that we provide from our
-/// symmetric heap.
+/// global heap.
 ///
 /// @note Our implementation is currently single-lock based. If this becomes a
 ///       synchronization bottleneck we can move to a non-blocking or more
 ///       fine-grained approach.
 ///
-
-typedef struct {
-  tatas_lock_t lock;                            // single lock for now
-  uint32_t      min;
-  uint32_t   nwords;
-  uintptr_t  bits[];
-} bitmap_t;
-
-/// Determine the size of a bitmap structure required to allocate @p n blocks.
-///
-/// @param n The number of blocks we need to manage.
-///
-/// @return The size of the bitmap_t required to manage @p n blocks.
-size_t bitmap_sizeof(const uint32_t n)
-  HPX_INTERNAL;
-
-/// Initialize a bitmap.
-///
-/// This isn't synchronized and shouldn't be used from possibly-concurrent
-/// threads.
-///
-/// @param   bitmap The bitmap to initialize.
-/// @param        n The number of blocks we need to manage.
-void bitmap_init(bitmap_t *bitmap, const uint32_t n)
-  HPX_INTERNAL HPX_NON_NULL(1);
+typedef struct bitmap bitmap_t;
 
 /// Allocate and initialize a bitmap.
 ///
-/// @param        n The number of blocks we need to manage.
+/// @param        nbits The number of bits we need to manage.
 ///
 /// @returns The new bitmap or NULL if there was an error.
-bitmap_t *bitmap_new(const uint32_t n)
+bitmap_t *bitmap_new(uint32_t nbits)
   HPX_INTERNAL;
 
 /// Delete a bitmap that was previously allocated with bitmap_new().
@@ -74,34 +50,33 @@ bitmap_t *bitmap_new(const uint32_t n)
 void bitmap_delete(bitmap_t *bitmap)
   HPX_INTERNAL;
 
-/// Allocate @p n contiguous blocks from the bitmap, aligned to a @p align
+/// Allocate @p nbits contiguous bits from the bitmap, aligned to a @p align
 /// boundary.
 ///
-/// @param[in]   bitmap The bitmap to allocate from.
-/// @param[in]        n The number of continuous blocks to allocate.
+/// @param[in]      map The bitmap to allocate from.
+/// @param[in]    nbits The number of continuous bits to allocate.
 /// @param[in]    align The alignment required.
 /// @param[out]       i The offset of the start of the allocation.
 ///
 /// @returns LIBHPX_OK, LIBHPX_ENOMEM
-int bitmap_reserve(bitmap_t *bitmap, const uint32_t n, const uint32_t align,
-                   uint32_t *i)
+int bitmap_reserve(bitmap_t *map, uint32_t nbits, uint32_t align, uint32_t *i)
   HPX_INTERNAL HPX_NON_NULL(1, 4);
 
-/// Free @p n contiguous blocks of memory, starting at offset @p i.
+/// Free @p nbits contiguous bits of memory, starting at offset @p i.
 ///
-/// @param   bitmap The bitmap to free from.
-/// @param     from The offset to start freeing from.
-/// @param        n The number of blocks to free.
-void bitmap_release(bitmap_t *bitmap, const uint32_t from, const uint32_t n)
+/// @param          map The bitmap to free from.
+/// @param            i The offset to start freeing from.
+/// @param        nbits The number of bits to free.
+void bitmap_release(bitmap_t *map, uint32_t i, uint32_t nbits)
   HPX_INTERNAL HPX_NON_NULL(1);
 
-/// Determine if a particular block has been allocated.
+/// Determine if a particular bit has been allocated.
 ///
-/// @param   bitmap The bitmap to check.
-/// @param    block The block offset to check.
+/// @param          map The bitmap to check.
+/// @param            i The bit offset to check.
 ///
-/// @returns true if the block is set, false otherwise.
-bool bitmap_is_set(bitmap_t *bitmap, const uint32_t block)
+/// @returns true if the bit is set, false otherwise.
+bool bitmap_is_set(bitmap_t *map, uint32_t bit)
   HPX_INTERNAL HPX_NON_NULL(1);
 
 #endif
