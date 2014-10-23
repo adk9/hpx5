adk9/hpx5

commit 44515e1fa26c26099dfb8784f3787777ee6a650b

Added a configuration option to use malloc for PGAS heap allocation.
One of the main reasons for this is to enable easy use of huge pages
on systems where malloc can be backed by huge pages automatically.
Specifically, this applies to cray machines where the appropriat huge
pages module may be loaded during compilation and during running.

Since huge pages can be now enabled with the module on Cray, I took
out the Cray hack. I updated SSSP example to use malloc by default.

NOTE: Currently Photon is experiencing problems with huge pages.

NOTE: Is there any reason not to always use malloc?

Signed-off-by: Marcin Zalewski <marcin.zalewski@gmail.com>

libhpx/gas/pgas/heap.c
@@ -149,18 +149,21 @@ static bitmap_t *_new_bitmap(size_t nchunks) {
 }
 
 
-static void *_map_heap(const size_t bytes) {
-  const int prot = PROT_READ | PROT_WRITE;
-  const int flags = MAP_ANON | MAP_PRIVATE | MAP_NORESERVE;
-#ifdef CRAY_HUGE_HACK
-  void *heap = get_huge_pages((bytes + gethugepagesize() - 1) / gethugepagesize() * gethugepagesize(), GHP_DEFAULT);
-#else
-  void *heap = mmap(NULL, bytes, prot, flags, -1, 0);
-#endif
+static void *_map_heap(const size_t bytes, hpx_pgas_alloc_t alloc) {
+  void *heap;
+  if(alloc == HPX_PGAS_ALLOC_MALLOC) 
+    heap = malloc(bytes);
+  else {
+    const int prot = PROT_READ | PROT_WRITE;
+    const int flags = MAP_ANON | MAP_PRIVATE | MAP_NORESERVE;
+    heap = mmap(NULL, bytes, prot, flags, -1, 0);
+  }
   if (!heap) {
     dbg_error("failed to mmap %lu bytes for the shared heap\n", bytes);
   }
-  else {
+  else if(alloc = HPX_PGAS_ALLOC_MALLOC) {
+    dbg_log_gas("malloced %lu bytes for the shared heap\n", bytes);
+  } else {
     dbg_log_gas("mmaped %lu bytes for the shared heap\n", bytes);
   }
   return heap;
@@ -186,7 +189,7 @@ int heap_init(heap_t *heap, const size_t size, bool init_cyclic) {
   // use one extra chunk to deal with alignment
   heap->raw_nchunks = heap->nchunks + 1;
   heap->raw_nbytes = heap->raw_nchunks * heap->bytes_per_chunk;
-  heap->raw_base = _map_heap(heap->raw_nbytes);
+  heap->raw_base = _map_heap(heap->raw_nbytes, heap->alloc);
 
   // adjust stored base based on alignment requirements
   const size_t r = ((uintptr_t)heap->raw_base % heap->bytes_per_chunk);
@@ -224,13 +227,13 @@ void heap_fini(heap_t *heap) {
     if (heap->transport)
       heap->transport->unpin(heap->transport, heap->base, heap->nbytes);
 
-#ifdef CRAY_HUGE_HACK
-    free_huge_pages(heap->raw_base);
-#else
-    int e = munmap(heap->raw_base, heap->raw_nbytes);
-    if (e)
-      dbg_error("pgas: failed to munmap the heap.\n");
-#endif
+    if(heap->alloc == HPX_PGAS_ALLOC_MALLOC)
+      free(heap->raw_base);
+    else {
+      int e = munmap(heap->raw_base, heap->raw_nbytes);
+      if (e)
+	dbg_error("pgas: failed to munmap the heap.\n");
+    }
   }
 }
 

apps/libPXGL/examples/sssp.c
@@ -29,6 +29,7 @@ static void _usage(FILE *stream) {
           "\t-D, all localities wait for debugger\n"
           "\t-d, wait for debugger at specific locality\n"
           "\t-l, set logging level\n"
+	  "\t-b, select allocator for PGAS heap (see hpx config)\n"
           "\t-s, set stack size\n"
           "\t-p, set per-PE global heap size\n"
 	  "\t-r, set send/receive request limit\n"
@@ -311,50 +312,57 @@ int main(int argc, char *const argv[argc]) {
   uint64_t time_limit = 1000;
   int realloc_adj_list = 1;
 
+  // By default use malloc
+  cfg.alloc = HPX_PGAS_ALLOC_MALLOC;
+
   int opt = 0;
   while ((opt = getopt(argc, argv, "c:t:T:d:Dl:s:p:b:r:q:ah")) != -1) {
     switch (opt) {
-     case 'c':
+    case 'c':
       cfg.cores = atoi(optarg);
       break;
-     case 't':
+    case 't':
       cfg.threads = atoi(optarg);
       break;
-     case 'T':
+    case 'T':
       cfg.transport = atoi(optarg);
       assert(0 <= cfg.transport && cfg.transport < HPX_TRANSPORT_MAX);
       break;
-     case 'D':
+    case 'D':
       cfg.wait = HPX_WAIT;
       cfg.wait_at = HPX_LOCALITY_ALL;
       break;
-     case 'd':
+    case 'd':
       cfg.wait = HPX_WAIT;
       cfg.wait_at = atoi(optarg);
       break;
-     case 'l':
+    case 'l':
       cfg.log_level = atoi(optarg);
       break;
-     case 's':
+    case 'b':
+      cfg.transport = atoi(optarg);
+      assert(0 <= cfg.alloc && cfg.alloc < HPX_PGAS_ALLOC_MAX);
+      break;
+    case 's':
       cfg.stack_bytes = strtoul(optarg, NULL, 0);
       break;
-     case 'p':
+    case 'p':
       cfg.heap_bytes = strtoul(optarg, NULL, 0);
       break;
-     case 'r':
+    case 'r':
       cfg.req_limit = strtoul(optarg, NULL, 0);
       break;
-     case 'q':
+    case 'q':
       time_limit = strtoul(optarg, NULL, 0);
       break;
-     case 'a':
+    case 'a':
       realloc_adj_list = 0;
       break;
-     case 'h':
+    case 'h':
       _usage(stdout);
       return 0;
-     case '?':
-     default:
+    case '?':
+    default:
       _usage(stderr);
       return -1;
     }

include/hpx/config.h
@@ -35,6 +35,14 @@ static const char* const HPX_GAS_TO_STRING[] = {
   "HPX_GAS_AGAS_SWITCH"
 };
 
+//! Configuration options for heap allocator in PGAS
+typedef enum {
+  HPX_PGAS_ALLOC_DEFAULT = 0, //!< Let HPX choose the allocator
+  HPX_PGAS_ALLOC_MMAP,        //!< Use mmap to get heap space
+  HPX_PGAS_ALLOC_MALLOC,      //!< Use malloc to get heap space
+  HPX_PGAS_ALLOC_MAX
+} hpx_pgas_alloc_t;
+
 //! Configuration options for the network transports HPX can use.
 typedef enum {
   HPX_TRANSPORT_DEFAULT = 0, //!< Let HPX choose what transport to use.
@@ -91,6 +99,7 @@ typedef struct {
   int           stack_bytes;          //!< minimum stack size in bytes.
   size_t         heap_bytes;          //!< shared heap size in bytes
   hpx_gas_t             gas;          //!< Type of GAS.
+  hpx_pgas_alloc_t    alloc;          //!< PGAS heap allocator
   hpx_boot_t           boot;          //!< bootstrap method to use.
   hpx_transport_t transport;          //!< transport to use.
   hpx_wait_t           wait;          //!< when to wait for a debugger.
@@ -106,14 +115,15 @@ typedef struct {
     .threads     = 0,                           \
     .backoff_max = 1024,                        \
     .stack_bytes = 32768,                       \
-    .heap_bytes  = 1lu << 30, /* 1GB */           \
+    .heap_bytes  = 1lu << 30, /* 1GB */         \
     .gas         = HPX_GAS_PGAS,                \
+    .alloc       = HPX_PGAS_ALLOC_DEFAULT,      \
     .boot        = HPX_BOOT_DEFAULT,            \
     .transport   = HPX_TRANSPORT_DEFAULT,       \
     .wait        = HPX_WAIT_NONE,               \
     .wait_at     = HPX_LOCALITY_NONE,           \
     .log_level   = HPX_LOG_DEFAULT,             \
-    .statistics  = true,            \
+    .statistics  = true,                        \
     .req_limit   = 0                            \
   }
 

include/libhpx/gas.h
@@ -59,12 +59,12 @@ gas_class_t *gas_smp_new(size_t heap_size, struct boot_class *boot,
                          struct transport_class *transport)
   HPX_INTERNAL HPX_NON_NULL(2,3);
 
-gas_class_t *gas_pgas_new(size_t heap_size, struct boot_class *boot,
+gas_class_t *gas_pgas_new(size_t heap_size, hpx_pgas_alloc_t alloc, struct boot_class *boot,
                          struct transport_class *transport)
-  HPX_INTERNAL HPX_NON_NULL(2,3);
+  HPX_INTERNAL HPX_NON_NULL(3,4);
 
 gas_class_t *gas_new(size_t heap_size, struct boot_class *boot,
-                     struct transport_class *transport, hpx_gas_t type)
+                     struct transport_class *transport, hpx_gas_t type, hpx_pgas_alloc_t alloc)
   HPX_INTERNAL HPX_NON_NULL(2,3);
 
 inline static void gas_delete(gas_class_t *gas) {

libhpx/gas/gas.c
@@ -18,7 +18,8 @@
 #include "libhpx/debug.h"
 
 gas_class_t *gas_new(size_t heap_size, struct boot_class *boot,
-                     struct transport_class *transport, hpx_gas_t type)
+                     struct transport_class *transport, hpx_gas_t type,
+		     hpx_pgas_alloc_t alloc)
 {
   gas_class_t *gas = NULL;
 
@@ -27,7 +28,7 @@ gas_class_t *gas_new(size_t heap_size, struct boot_class *boot,
   }
 
   if (type == HPX_GAS_PGAS) {
-    gas = gas_pgas_new(heap_size, boot, transport);
+    gas = gas_pgas_new(heap_size, alloc, boot, transport);
     if (!gas) {
       dbg_log_gas("PGAS failed to initialize\n");
     }

libhpx/gas/pgas/heap.h
@@ -66,6 +66,7 @@ typedef struct heap {
   char                    *raw_base;
   struct transport_class *transport;
   unsigned             cyclic_arena;
+  hpx_pgas_alloc_t            alloc;
 } heap_t;
 
 /// Initialize a heap to manage the specified number of bytes.

libhpx/gas/pgas/pgas.c
@@ -373,7 +373,7 @@ static gas_class_t _pgas_vtable = {
   .owner_of      = pgas_gva_to_rank
 };
 
-gas_class_t *gas_pgas_new(size_t heap_size, boot_class_t *boot,
+gas_class_t *gas_pgas_new(size_t heap_size, hpx_pgas_alloc_t alloc, boot_class_t *boot,
                           struct transport_class *transport) {
   if (here->ranks == 1) {
     dbg_log_gas("PGAS requires at least two ranks\n");
@@ -388,6 +388,7 @@ gas_class_t *gas_pgas_new(size_t heap_size, boot_class_t *boot,
     dbg_error("pgas: could not allocate global heap\n");
     return NULL;
   }
+  global_heap->alloc = alloc;
 
   int e = heap_init(global_heap, heap_size, (here->rank == 0));
   if (e) {

libhpx/hpx.c
@@ -121,7 +121,7 @@ int hpx_init(const hpx_config_t *cfg) {
     return _cleanup(here, dbg_error("init: failed to create transport.\n"));
   dbg_log("initialized the %s transport.\n", transport_id(here->transport));
 
-  here->gas = gas_new(cfg->heap_bytes, here->boot, here->transport, cfg->gas);
+  here->gas = gas_new(cfg->heap_bytes, here->boot, here->transport, cfg->gas, cfg->alloc);
   if (here->gas == NULL)
     return _cleanup(here, dbg_error("init: failed to create the global address "
                                     "space.\n"));

commit 2c4fb8e38be206e1a1ae233a74ad9c2c4e474ff5

Fixed warning and bug introduced in 44515e1fa26c26099dfb8784f3787777ee6a650b.

libhpx/gas/pgas/heap.c
@@ -151,7 +151,7 @@ static bitmap_t *_new_bitmap(size_t nchunks) {
 
 static void *_map_heap(const size_t bytes, hpx_pgas_alloc_t alloc) {
   void *heap;
-  if(alloc == HPX_PGAS_ALLOC_MALLOC) 
+  if(alloc == HPX_PGAS_ALLOC_MALLOC)
     heap = malloc(bytes);
   else {
     const int prot = PROT_READ | PROT_WRITE;
@@ -161,7 +161,7 @@ static void *_map_heap(const size_t bytes, hpx_pgas_alloc_t alloc) {
   if (!heap) {
     dbg_error("failed to mmap %lu bytes for the shared heap\n", bytes);
   }
-  else if(alloc = HPX_PGAS_ALLOC_MALLOC) {
+  else if (alloc == HPX_PGAS_ALLOC_MALLOC) {
     dbg_log_gas("malloced %lu bytes for the shared heap\n", bytes);
   } else {
     dbg_log_gas("mmaped %lu bytes for the shared heap\n", bytes);
@@ -232,7 +232,7 @@ void heap_fini(heap_t *heap) {
     else {
       int e = munmap(heap->raw_base, heap->raw_nbytes);
       if (e)
-	dbg_error("pgas: failed to munmap the heap.\n");
+    dbg_error("pgas: failed to munmap the heap.\n");
     }
   }
 }