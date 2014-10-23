adk9/hpx5

commit b2bf84cf469dc49790fa95df7f8fac415748ae1b

Stop using the implicit freelisting version of the two-lock queue.

libhpx/scheduler/worker.c
@@ -209,16 +209,20 @@ static hpx_parcel_t *_network(void) {
 /// Send a mail message to another worker.
 static void _send_mail(uint32_t id, hpx_parcel_t *p) {
   worker_t *w = here->sched->workers[id];
-  sync_two_lock_queue_enqueue(&(w->inbox), p);
+  two_lock_queue_node_t *node = malloc(sizeof(*node));
+  node->value = p;
+  sync_two_lock_queue_enqueue_node(&(w->inbox), node);
 }
 
 
 /// Process my mail queue.
 static void _handle_mail(void) {
-  hpx_parcel_t *p = NULL;
-  while ((p = sync_two_lock_queue_dequeue(&self.inbox)) != NULL) {
+  two_lock_queue_node_t *node = NULL;
+  while ((node = sync_two_lock_queue_dequeue_node(&self.inbox)) != NULL) {
     profile_ctr(++self.stats.mail);
+    hpx_parcel_t *p = node->value;
     sync_chase_lev_ws_deque_push(&self.work, p);
+    free(p);
   }
 }
 

include/libsync/queues.h
@@ -63,13 +63,24 @@ typedef struct {
 two_lock_queue_t *sync_two_lock_queue_new(void) HPX_MALLOC;
 void sync_two_lock_queue_delete(two_lock_queue_t *q) HPX_NON_NULL(1);
 
-void  sync_two_lock_queue_init(two_lock_queue_t *q, two_lock_queue_node_t *init)
+void sync_two_lock_queue_init(two_lock_queue_t *q, two_lock_queue_node_t *init)
   HPX_NON_NULL(1);
-void  sync_two_lock_queue_fini(two_lock_queue_t *q) HPX_NON_NULL(1);
-void  sync_two_lock_queue_enqueue(two_lock_queue_t *q, void *val) HPX_NON_NULL(1);
-void *sync_two_lock_queue_dequeue(two_lock_queue_t *q) HPX_NON_NULL(1);
-void  sync_two_lock_queue_enqueue_node(two_lock_queue_t *q, two_lock_queue_node_t *node) HPX_NON_NULL(2);
-two_lock_queue_node_t *sync_two_lock_queue_dequeue_node(two_lock_queue_t *q) HPX_NON_NULL(1);
+
+void sync_two_lock_queue_fini(two_lock_queue_t *q)
+  HPX_NON_NULL(1);
+
+void sync_two_lock_queue_enqueue(two_lock_queue_t *q, void *val)
+  HPX_NON_NULL(1);
+
+void *sync_two_lock_queue_dequeue(two_lock_queue_t *q)
+  HPX_NON_NULL(1);
+
+void sync_two_lock_queue_enqueue_node(two_lock_queue_t *q,
+                                      two_lock_queue_node_t *node)
+  HPX_NON_NULL(2);
+
+two_lock_queue_node_t *sync_two_lock_queue_dequeue_node(two_lock_queue_t *q)
+  HPX_NON_NULL(1);
 
 typedef struct {
   queue_t vtable;

commit 158b83013c95bf26a0218b79abb37679aba17bc7

Fix to bug introduced in b2bf84cf469dc49790fa95df7f8fac415748ae1b.

libhpx/scheduler/worker.c
@@ -211,6 +211,7 @@ static void _send_mail(uint32_t id, hpx_parcel_t *p) {
   worker_t *w = here->sched->workers[id];
   two_lock_queue_node_t *node = malloc(sizeof(*node));
   node->value = p;
+  node->next = NULL;
   sync_two_lock_queue_enqueue_node(&(w->inbox), node);
 }
 
@@ -220,9 +221,8 @@ static void _handle_mail(void) {
   two_lock_queue_node_t *node = NULL;
   while ((node = sync_two_lock_queue_dequeue_node(&self.inbox)) != NULL) {
     profile_ctr(++self.stats.mail);
-    hpx_parcel_t *p = node->value;
-    sync_chase_lev_ws_deque_push(&self.work, p);
-    free(p);
+    sync_chase_lev_ws_deque_push(&self.work, node->value);
+    free(node);
   }
 }
 