89e41646ad7419832093c19fd3f3bef6d7bcd999
Luke Dalessandro <ldalessa@indiana.edu>
2014-10-28 07:01:12 -0400

    Added asm macro for .section.

--- libhpx/scheduler/arch/x86_64/asm_macros.h
+++ libhpx/scheduler/arch/x86_64/asm_macros.h
@@ -14,27 +14,39 @@
 #define LIBHPX_ASM_MACROS_H
 
 #if defined(__APPLE__)
-#define GLOBAL(S) .globl _##S
-#define LABEL(S) _##S:
-#define INTERNAL(S) .private_extern _##S
-#define SIZE(S)
+# define GLOBAL(S) .globl _##S
+# define LABEL(S) _##S:
+# define INTERNAL(S) .private_extern _##S
+# define SIZE(S)
+# define TYPE(S, T) .type _##S, T
+# define STARTPROC .cfi_startproc
+# define ENDPROC .cfi_endproc
+# define SECTION(S) .section .S
+# define CFA_DEF_OFFSET(N) .cfi_def_cfa_offset N
+# define CFA_ADJ_OFFSET(N) .cfi_adjust_cfa_offset N
+# define CFA_REGISTER(R) .cfi_def_cfa_register R
+# define CFA_DEF(R,N) .cfi_def_cfa R,N
+# define CFI_RESTORE(R) .cfi_restore R
+# define CFI_OFFSET(R,N) .cfi_offset R,N
+# define CFI_REL_OFFSET(R,N) .cfi_rel_offset R,N
 #elif defined(__linux__)
-#define GLOBAL(S) .globl S
-#define LABEL(S) S:
-#define INTERNAL(S) .internal S
-#define SIZE(S) .size S, .-S
-#define TYPE(S, T) .type S, T
-#define STARTPROC .cfi_startproc
-#define ENDPROC .cfi_endproc
-#define CFA_DEF_OFFSET(N) .cfi_def_cfa_offset N
-#define CFA_ADJ_OFFSET(N) .cfi_adjust_cfa_offset N
-#define CFA_REGISTER(R) .cfi_def_cfa_register R
-#define CFA_DEF(R,N) .cfi_def_cfa R,N
-#define CFI_RESTORE(R) .cfi_restore R
-#define CFI_OFFSET(R,N) .cfi_offset R,N
-#define CFI_REL_OFFSET(R,N) .cfi_rel_offset R,N
+# define GLOBAL(S) .globl S
+# define LABEL(S) S:
+# define INTERNAL(S) .internal S
+# define SIZE(S) .size S, .-S
+# define TYPE(S, T) .type S, T
+# define STARTPROC .cfi_startproc
+# define ENDPROC .cfi_endproc
+# define SECTION(S) .section .S
+# define CFA_DEF_OFFSET(N) .cfi_def_cfa_offset N
+# define CFA_ADJ_OFFSET(N) .cfi_adjust_cfa_offset N
+# define CFA_REGISTER(R) .cfi_def_cfa_register R
+# define CFA_DEF(R,N) .cfi_def_cfa R,N
+# define CFI_RESTORE(R) .cfi_restore R
+# define CFI_OFFSET(R,N) .cfi_offset R,N
+# define CFI_REL_OFFSET(R,N) .cfi_rel_offset R,N
 #else
-#error No ASM support for your platform.
+# error No ASM support for your platform.
 #endif
 
 #endif // LIBHPX_ASM_MACROS_H
