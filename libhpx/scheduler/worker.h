9095576da3668ab1602382b37579cb54c9f813d3
Luke Dalessandro <ldalessa@indiana.edu>
2014-11-18 08:54:29 -0500

    Expose the "struct worker" type in the worker.h header.
    Former-commit-id: 52f07f3a339b9d26b315e8920874f9660b2a2188

--- libhpx/scheduler/worker.h
+++ libhpx/scheduler/worker.h
@@ -13,10 +13,40 @@
 #ifndef LIBHPX_WORKER_H
 #define LIBHPX_WORKER_H
 
-#include "hpx/attributes.h"
+#include <pthread.h>
+#include <hpx/hpx.h>
+#include <hpx/attributes.h>
+#include <libsync/deques.h>
+#include <libsync/queues.h>
+#include <libhpx/stats.h>
 
+/// Forward declarations.
+/// @{
 struct scheduler;
-struct worker;
+/// @}
+
+
+/// Class representing a worker thread's state.
+///
+/// Worker threads are "object-oriented" insofar as that goes, but each native
+/// thread has exactly one, thread-local worker structure, so the interface
+/// doesn't take a "this" pointer and instead grabs the "self" structure using
+/// __thread local storage.
+///
+/// @{
+struct worker {
+  pthread_t          thread;                    // this worker's native thread
+  int                    id;                    // this workers's id
+  int               core_id;                    // useful for "smart" stealing
+  unsigned int         seed;                    // my random seed
+  int                UNUSED;
+  void                  *sp;                    // this worker's native stack
+  hpx_parcel_t     *current;                    // current thread
+  chase_lev_ws_deque_t work;                    // my work
+  two_lock_queue_t    inbox;                    // mail sent to me
+  volatile int     shutdown;                    // cooperative shutdown flag
+  scheduler_stats_t   stats;                    // scheduler statistics
+};
 
 
 /// The main entry function for a scheduler worker thread.
