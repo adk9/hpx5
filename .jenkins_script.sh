bd3a1b01a42acf83435e6ac7564bcb4f9f2d772d
Ryan Newton <rrnewton@gmail.com>
2014-11-17 22:26:27 -0500

    add HPXMODE and HPXDEBUG to vary the CI testing.
    Former-commit-id: 440784541922e3d12f3bf6a8633c6bbfca716c5c

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -3,6 +3,26 @@
 DIR=$1
 shift
 
+function add_mpi() {
+    # This is currently cutter-specific and needs to be generalized.
+    module load openmpi/1.6.3
+    export C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/openmpi/1.6.3/include/
+}
+
+case "$HPXMODE" in
+    photon)
+	CFGFLAGS=" --with-mpi --enable-photon "
+	add_mpi	
+	;;
+    mpi)
+	CFGFLAGS=" --with-mpi "
+	add_mpi	
+	;;
+    *)
+	CFGFLAGS=" "
+	;;
+esac
+
 echo "Building HPX in $DIR"
 cd $DIR
 set -xe
@@ -11,8 +31,11 @@ rm -rf ./build/
 ./bootstrap
 mkdir build
 cd build
-../configure --with-mpi --enable-photon --with-check --enable-testsuite --enable-apps
+../configure $CFGFLAGS --with-check --enable-testsuite --enable-apps $HPXDEBUG
 make
 
-# TODO: activate tests:
+# Finally, run all the unit tests:
 make check
+
+# TODO: Run the apps and check their output...
+
