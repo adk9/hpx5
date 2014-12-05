effdbe3c2b43ac8b27851e9b4d5b1b1c621a934e
jayaajay <jayaajay@indiana.edu>
2014-12-05 18:25:12 -0500

    HPX Tutorial
    Former-commit-id: e2026ba49b29b2dc1f9be41da0f7880fc1122fe5

---
+++ tutorial/threads/threadSpawn2.c
@@ -0,0 +1,48 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+
+//****************************************************************************
+// Example code - Thread Creation using hpx_par_call_sync and Termination
+//****************************************************************************
+#include <stdio.h>
+#include <hpx/hpx.h>
+
+#define NUM_THREADS                5
+static  hpx_action_t _main       = 0;
+static  hpx_action_t _printHello = 0;
+
+static int _printHello_action(int *args) {
+  printf("Hello World! It's me, thread #%d!\n", hpx_thread_get_tls_id());
+  hpx_thread_exit(HPX_SUCCESS);
+}
+
+//****************************************************************************
+// @Action which spawns the threads
+//****************************************************************************
+static int _main_action(int *args) {
+  hpx_par_call_sync(_printHello, 0, NUM_THREADS, 8, 1000, 0, NULL, 0, 0);
+  hpx_shutdown(HPX_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+  int e = hpx_init(&argc, &argv);
+  if (e) {
+    fprintf(stderr, "HPX: failed to initialize.\n");
+    return e;
+  }
+   
+  HPX_REGISTER_ACTION(&_main, _main_action);
+  HPX_REGISTER_ACTION(&_printHello, _printHello_action);
+
+  return hpx_run(&_main, NULL, 0);
+}
