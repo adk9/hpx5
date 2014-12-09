e94b56d3a6b92fcb33685937077148b51a2ee3fe
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2014-12-09 13:11:38 -0500

    Updated the interface, and some re-organization.
    Former-commit-id: 4f0a4892767c378a27f346a6c392f59c8ce4a39f

--- libhpx/scheduler/worker.c
+++ libhpx/scheduler/worker.c
@@ -412,6 +412,44 @@ void scheduler_spawn(hpx_parcel_t *p) {
 }
 
 
+/// Spawn a user-level task.
+void scheduler_spawn_task(hpx_parcel_t *p) {
+  int status = action_invoke(p);
+  switch (status) {
+    default:
+      dbg_error("action: produced unhandled error %i.\n", (int)status);
+      hpx_shutdown(status);
+    case HPX_ERROR:
+      dbg_error("action: produced error.\n");
+      hpx_abort();
+    case HPX_RESEND:
+      hpx_parcel_send(p, HPX_NULL);
+    case HPX_SUCCESS:
+    case HPX_LCO_ERROR:
+      process_recover_credit(p);
+      hpx_action_t c_act = hpx_parcel_get_cont_action(p);
+      hpx_addr_t c_target = hpx_parcel_get_cont_target(p);
+      if ((c_target != HPX_NULL) && c_act != HPX_ACTION_NULL) {
+        if (p->pid != HPX_NULL) {
+          --p->credit;
+        }
+
+        if (c_act == hpx_lco_set_action) {
+          hpx_call_with_continuation(c_target, c_act, NULL, 0, HPX_NULL,
+                                     HPX_ACTION_NULL);
+        } else {
+          locality_cont_args_t cargs = { .action = c_act,
+                                         .status = status };
+          hpx_call_with_continuation(c_target, locality_call_continuation, &cargs,
+                                     sizeof(cargs),
+                                     HPX_NULL, HPX_ACTION_NULL);
+        }
+      }
+      break;
+  }
+}
+
+
 /// This is the continuation that we use to yield a thread.
 ///
 /// 1) We can't put a yielding thread onto our workqueue with the normal push
