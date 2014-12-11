88d9e5ec2ad882d44fe3dedaf657682c1c893c52
Luke Dalessandro <ldalessa@indiana.edu>
2014-12-11 14:50:37 -0500

    Correctly pass the configuration to the network constructor.
    Former-commit-id: 20f1fb050215000a00250434438a01b0d4f2ac16

--- libhpx/hpx.c
+++ libhpx/hpx.c
@@ -184,8 +184,7 @@ int hpx_init(int *argc, char ***argv) {
   }
 
   // parcel network
-  here->network = network_new(LIBHPX_NETWORK_DEFAULT, here->boot, here->gas,
-                              workers);
+  here->network = network_new(here->config, here->boot, here->gas, workers);
   if (!here->network) {
     status = dbg_error("failed to create network.\n");
     goto unwind1;
