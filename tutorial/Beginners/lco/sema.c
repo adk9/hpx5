bed24191f5f22068d6269c3a2716d18649ebae3d
jayaajay <jayaajay@indiana.edu>
2014-12-16 11:44:26 -0500

    tutorial for 2D heat conduction
    Former-commit-id: d551302d705d7529b58b695aa2b7b6a8f03ec901

---
+++ tutorial/Beginners/lco/sema.c
@@ -0,0 +1,66 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+
+#include <stdio.h>
+#include <hpx/hpx.h>
+
+#define NITER         1000000
+#define NUM_THREADS   2
+static  hpx_action_t _main = 0;
+static  hpx_action_t _add  = 0;
+
+int count = 0;
+hpx_addr_t sem;
+
+static int _add_action(int *args) {
+  int i, tmp;
+  for (i = 0; i < NITER; ++i) {
+    hpx_lco_sema_p(sem);
+    tmp = count;
+    tmp = tmp + 1;
+    count = tmp;
+    hpx_lco_sema_v(sem);
+  }
+  return HPX_SUCCESS;
+}
+
+static int _main_action(void *args) {
+  sem = hpx_lco_sema_new(1);
+  hpx_addr_t and = hpx_lco_and_new(NUM_THREADS);
+  for (int i = 0; i < NUM_THREADS; i++) 
+    hpx_call(HPX_HERE, _add, &i, sizeof(i), and);
+
+  hpx_lco_wait(and);
+  hpx_lco_delete(and, HPX_NULL);
+
+  if (count < 2 * NITER) {
+    printf("BOOM! count is [%d], should be %d\n", count, 2*NITER);
+  } else {
+    printf("OK! count is [%d]\n", count);
+  }
+
+  hpx_shutdown(HPX_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+  int e = hpx_init(&argc, &argv);
+  if (e) {
+    fprintf(stderr, "HPX: failed to initialize.\n");
+    return e;
+  }
+   
+  HPX_REGISTER_ACTION(&_main, _main_action);
+  HPX_REGISTER_ACTION(&_add, _add_action);
+
+  return hpx_run(&_main, NULL, 0);
+}
---
+++ tutorial/Beginners/threads/threadContCleanup.c
@@ -0,0 +1,61 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+
+//****************************************************************************
+// Example code - Thread continue cleanup tutorial -- Finishes the current
+// thread's execution, sending value to the thread's contination address.
+// This version gives the application to cleanup for instance, to free the 
+// value. After dealing with the continued data, it will run cleanup(env)
+//****************************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <hpx/hpx.h>
+
+static  hpx_action_t _main       = 0;
+static  hpx_action_t _cleanup    = 0;
+
+const int DATA_SIZE              = sizeof(uint64_t);
+const int SET_CONT_VALUE         = 1234;
+
+static int _cleanup_action(void *args) {
+  uint64_t *value = (uint64_t*) malloc(sizeof(uint64_t));
+  *value = SET_CONT_VALUE;
+  hpx_thread_continue_cleanup(DATA_SIZE, value, free, value);
+}
+
+//****************************************************************************
+// @Action which spawns the thread
+//****************************************************************************
+static int _main_action(int *args) {
+  int rank = hpx_get_my_rank();
+  uint64_t *block = malloc(DATA_SIZE);
+
+  hpx_call_sync(HPX_HERE, _cleanup, &rank, sizeof(rank), block, DATA_SIZE);
+  printf("Value in block is %"PRIu64"\n", *block);
+
+  hpx_shutdown(HPX_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+  int e = hpx_init(&argc, &argv);
+  if (e) {
+    fprintf(stderr, "HPX: failed to initialize.\n");
+    return e;
+  }
+   
+  HPX_REGISTER_ACTION(&_main, _main_action);
+  HPX_REGISTER_ACTION(&_cleanup, _cleanup_action);
+
+  return hpx_run(&_main, NULL, 0);
+}
