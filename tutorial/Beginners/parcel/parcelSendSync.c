8ae04061430f5dbbea952670d9cf2d7f829dcd1f
Ben Martin <benjmart@indiana.edu>
2014-12-17 16:25:01 -0500

    Fix for parcelSendSync.c in tutorials.
    Former-commit-id: 6cdf69b6767af6796ba4ede99ee7cc193ebbbeaf

--- tutorial/Beginners/parcel/parcelSendSync.c
+++ tutorial/Beginners/parcel/parcelSendSync.c
@@ -12,6 +12,7 @@
 // =============================================================================
 
 #include <stdio.h>
+#include <string.h>
 #include <inttypes.h>
 #include <hpx/hpx.h>
 
@@ -20,7 +21,8 @@ static hpx_action_t _sendData = 0;
 
 typedef struct parcel_data {
   int index;
-  char *message;
+  hpx_addr_t done;
+  char message[256];
 } parcel_data_t;
 
 hpx_addr_t _partner(void) {
@@ -32,23 +34,28 @@ hpx_addr_t _partner(void) {
 static int _sendData_action(const parcel_data_t *args)
 {
   printf("Received #%d: Message = %s\n", args->index, args->message);
+  hpx_lco_set(args->done, 0, NULL, HPX_NULL, HPX_NULL);
   return HPX_SUCCESS;
 }
 
 static int _main_action(void *args) {
+  hpx_addr_t done = hpx_lco_future_new(0);
   parcel_data_t parcelData = {
     .index = hpx_get_my_rank(),
-    .message = "Hello World!"
+    .done = done,
   };
+  strncpy(parcelData.message, "Hello World!", 256);
 
   hpx_addr_t to = _partner();
 
   hpx_parcel_t *p = hpx_parcel_acquire(&parcelData, sizeof(parcelData));
   hpx_parcel_set_action(p, _sendData);
-  hpx_parcel_set_data(p, &parcelData, sizeof(parcelData));
   hpx_parcel_set_target(p, to);
 
   hpx_parcel_send_sync(p);
+
+  hpx_lco_wait(done);
+  hpx_lco_delete(done, HPX_NULL);
 
   hpx_shutdown(HPX_SUCCESS);
 }
