4bb5d5cba5e44d760128fe5af2c6ed08c8c0f823
Marcin Zalewski <zalewski@indiana.edu>
2015-01-07 01:44:52 -0500

    Changed compare and swap to not cause a warning.
    Signed-off-by: Marcin Zalewski <zalewski@indiana.edu>
    
    Former-commit-id: 056fe8c5bc67d39d9c49ac9cbe65eb77dc209275

--- include/libsync/gcc/atomic.h
+++ include/libsync/gcc/atomic.h
@@ -48,7 +48,7 @@
 #define sync_cas(addr, from, to, onsuccess, onfailure)              \
     __atomic_compare_exchange_n(addr, &from, to, false, onsuccess, onfailure)
 #define sync_cas_val(addr, from, to, onsuccess, onfailure)          \
-    (__atomic_compare_exchange_n(addr, &from, to, false, onsuccess, onfailure), from)
+  __atomic_compare_exchange_n(addr, &from, to, false, onsuccess, onfailure) ? from : from
 
 #define sync_fadd(addr, val, mm) __atomic_fetch_add(addr, val, mm)
 #define sync_addf(addr, val, mm) __atomic_add_fetch(addr, val, mm)
