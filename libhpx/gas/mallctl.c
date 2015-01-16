14940b41e63929ed4c24c4974f7f2ca74e9cc6a9
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-01-15 22:41:18 -0500

    We don't need the hpxje prefix anymore.
    Former-commit-id: 550e0f03b4a270245ea01922356f64e4b41af8aa

--- libhpx/gas/mallctl.c
+++ libhpx/gas/mallctl.c
@@ -33,12 +33,10 @@ int mallctl_get_lg_dirty_mult(void) {
 /// This is a hack to force jemalloc to avoid purging pages. The
 /// opt.lg_dirty_mult is a read-only config value, but we can force it to be
 /// what we want because we're using an embedded jemalloc.
-extern ssize_t hpxje_opt_lg_dirty_mult;
+extern ssize_t je_opt_lg_dirty_mult;
 
 bool mallctl_disable_dirty_page_purge(void) {
-#if !defined(__ARMEL__) 
-  hpxje_opt_lg_dirty_mult = -1;
-#endif
+  je_opt_lg_dirty_mult = -1;
   return (mallctl_get_lg_dirty_mult() == -1);
 }
 
