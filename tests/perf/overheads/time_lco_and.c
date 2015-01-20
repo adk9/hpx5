2ed54b62daf0385b714ee2989384de9fac4b43a6
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-01-20 15:27:00 -0500

    Reduce some of the input sizes to perf/overhead so that make check finishes faster.
    Former-commit-id: 24a0115c243cf3c6849168a81dfec8f0c2fec1de

--- tests/perf/overheads/time_lco_and.c
+++ tests/perf/overheads/time_lco_and.c
@@ -11,11 +11,11 @@
 #define HEADER_FIELD_WIDTH 5
 
 static int num[] = {
-  1000000,
-  2000000,
-  3000000,
-  4000000,
-  5000000
+  10000,
+  20000,
+  30000,
+  40000,
+  50000
 };
 
 static void _usage(FILE *stream) {
