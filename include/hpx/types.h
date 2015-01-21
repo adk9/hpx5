969bbfe740c375b37693dc12096d33dab8ffb18b
Luke Dalessandro <ldalessa@indiana.edu>
2015-01-21 10:45:53 -0500

    Added a rudimentary hpx_strerror() function for debugging.
    Former-commit-id: 7e77562d6929bf479e76a3f6e662d980a574424e

--- include/hpx/types.h
+++ include/hpx/types.h
@@ -29,6 +29,9 @@ typedef short hpx_status_t;
 #define  HPX_LCO_TIMEOUT     ((hpx_status_t)4)
 #define  HPX_LCO_RESET       ((hpx_status_t)5)
 #define  HPX_USER            ((hpx_status_t)127)
+
+const char *hpx_strerror(hpx_status_t);
+
 /// @}
 
 /// HPX datatypes
