a35d55dcf12740fa219cf6f606cf3e0cbcb110cf
Luke Dalessandro <ldalessa@indiana.edu>
2015-01-29 11:23:33 -0500

    Add more iterations for this test.
    Former-commit-id: 2ce0e3b31ebc8af3ab36f8a1a7c9d4f43fbc2dc2

--- tests/perf/overheads/time_lco_sema.c
+++ tests/perf/overheads/time_lco_sema.c
@@ -13,11 +13,11 @@
 #define HEADER_FIELD_WIDTH 5
 
 static int num[] = {
-  10,
-  20,
-  30,
-  40,
-  50
+  100000,
+  200000,
+  300000,
+  400000,
+  500000
 };
 
 static int _thread1_handler(uint32_t iter, hpx_addr_t sem1, hpx_addr_t sem2) {
