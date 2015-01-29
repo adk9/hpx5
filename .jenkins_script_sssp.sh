31cb554932edfb2376ae1e31a8deb0e4c4236abe
Jayashree Candadai <jayaajay@indiana.edu>
2015-01-29 15:52:29 -0500

    jenkins script for SSSP regression test
    Former-commit-id: 5511015ac639440163fece8f7848b6db23edbddd

---
+++ .jenkins_script_sssp.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+DIR=$1
+shift
+
+function add_mpi() {
+    # This is currently cutter-specific and needs to be generalized.
+    module load openmpi/1.8.1
+    export C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/openmpi/1.8.1/include/
+}
+
+function add_photon() {
+    # This is currently cutter-specific and needs to be generalized.
+    export HPX_USE_IB_DEV=mlx4_0
+    export HPX_USE_IB_PORT=1
+    export HPX_USE_CMA=0
+    export HPX_USE_ETH_DEV=roce0
+
+    export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+    export LIBRARY_PATH=/usr/lib64:$LIBRARY_PATH
+}
+
+set -xe
+case "$HPXMODE" in
+    photon)
+	CFGFLAGS=" --with-mpi=ompi --enable-photon "
+	add_mpi
+        add_photon
+	;;
+    mpi)
+	CFGFLAGS=" --with-mpi=ompi "
+	add_mpi	
+	;;
+    *)
+	CFGFLAGS=" "
+	;;
+esac
+
+echo "Building HPX in $DIR"
+cd $DIR
+
+rm -rf ./build/
+./bootstrap
+mkdir build
+cd build
+../configure $CFGFLAGS --enable-pedantic --enable-wall --enable-apps --enable-jemalloc --enable-debug --enable-logging CFLAGS="-O3 -ggdb" $HPXDEBUG
+make
+
+# Run the apps and check their output...
+set -xe
+case "$HPXMODE" in
+    photon)
+        cd $DIR/apps/libPXGL/examples        
+        # Delta-Stepping
+        mpirun -n 10 --map-by node:PE=16 --tag-output sssp -q 60 -c -z 40000 --hpx-cores=16 --hpx-heap=$((1024 * 1024 * 1024 * 25)) --hpx-sendlimit=32 --hpx-recvlimit=10000 /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.22.0.gr /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.22.0.ss
+        # Chaotic
+        mpirun -n 10 --map-by node:PE=16 --tag-output sssp -q 60 -c --hpx-cores=16 --hpx-heap=$((1024 * 1024 * 1024 * 15)) --hpx-sendlimit=32 --hpx-recvlimit=10000 /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.gr /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.ss < /dev/null 2>&1 | less
+        # Distributed control
+        mpirun -n 10 --map-by node:PE=16 --tag-output sssp -q 60 -d --hpx-cores=16 --hpx-heap=$((1024 * 1024 * 1024 * 15)) --hpx-sendlimit=32 --hpx-recvlimit=10000 /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.gr /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.ss
+        ;;
+    mpi)
+        cd $DIR/apps/libPXGL/examples
+        # Delta-Stepping
+	mpirun -n 10 --map-by node:PE=16 --tag-output sssp -q 60 -c -z 40000 --hpx-cores=16 --hpx-heap=$((1024 * 1024 * 1024 * 25)) --hpx-sendlimit=32 --hpx-recvlimit=10000 /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.22.0.gr /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.22.0.ss
+        # Chaotic
+        mpirun -n 10 --map-by node:PE=16 --tag-output sssp -q 60 -c --hpx-cores=16 --hpx-heap=$((1024 * 1024 * 1024 * 15)) --hpx-sendlimit=32 --hpx-recvlimit=10000 /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.gr /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.ss < /dev/null 2>&1 | less
+        # Distributed control
+        mpirun -n 10 --map-by node:PE=16 --tag-output sssp -q 60 -d --hpx-cores=16 --hpx-heap=$((1024 * 1024 * 1024 * 15)) --hpx-sendlimit=32 --hpx-recvlimit=10000 /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.gr /u/jsfiroz/DIMACS/ch9-1.1/inputs/Random4-n/Random4-n.20.0.ss
+        ;;
+esac
+
+exit 0
