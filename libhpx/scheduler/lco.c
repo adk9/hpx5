e0290b4fc2db223ef4847ddc59c0555520b055df
Luke Dalessandro <ldalessa@indiana.edu>
2015-01-28 21:48:07 -0500

    Style nits.
    Former-commit-id: 69f30a93d662cd91bfa1f111f2a431c7873a98d7

--- libhpx/scheduler/lco.c
+++ libhpx/scheduler/lco.c
@@ -207,14 +207,13 @@ void hpx_lco_error(hpx_addr_t target, hpx_status_t code, hpx_addr_t rsync) {
 }
 
 void hpx_lco_set(hpx_addr_t target, int size, const void *value,
-                 hpx_addr_t lsync, hpx_addr_t rsync)
-{
+                 hpx_addr_t lsync, hpx_addr_t rsync) {
   if (target == HPX_NULL) {
     return;
   }
 
   lco_t *lco = NULL;
-  if ((size > HPX_LCO_SET_ASYNC)  && hpx_gas_try_pin(target, (void**)&lco)) {
+  if ((size > HPX_LCO_SET_ASYNC) && hpx_gas_try_pin(target, (void**)&lco)) {
     _set(lco, size, value);
     hpx_gas_unpin(target);
     hpx_lco_set(lsync, 0, NULL, HPX_NULL, HPX_NULL);
