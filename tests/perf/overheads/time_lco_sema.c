b2057cbfa2e537ee19f3ea981939991d741df9b5
Luke Dalessandro <ldalessa@indiana.edu>
2015-01-29 00:00:12 -0500

    Simplified test case code.
    Former-commit-id: 2390d2eaced7e7fe39ebbea575a4b0503e3e7446

--- tests/perf/overheads/time_lco_sema.c
+++ tests/perf/overheads/time_lco_sema.c
@@ -12,11 +12,11 @@
 #define HEADER_FIELD_WIDTH 5
 
 static int num[] = {
-  1000000,
-  2000000,
-  3000000,
-  4000000,
-  5000000
+  100000,
+  200000,
+  300000,
+  400000,
+  500000
 };
 
 static int _thread1_handler(uint32_t iter, hpx_addr_t sem1, hpx_addr_t sem2) {
@@ -25,9 +25,7 @@ static int _thread1_handler(uint32_t iter, hpx_addr_t sem1, hpx_addr_t sem2) {
     hpx_lco_sema_p(sem1);
     hpx_lco_sema_v(sem2);
   }
-  fprintf(stdout, "Thread 1: %d%*g\n", iter, FIELD_WIDTH,
-              hpx_time_elapsed_ms(t));
-
+  printf("Thread 1: %d%*g\n", iter, FIELD_WIDTH, hpx_time_elapsed_ms(t));
   return HPX_SUCCESS;
 }
 
@@ -40,9 +38,7 @@ static int _thread2_handler(uint32_t iter, hpx_addr_t sem1, hpx_addr_t sem2) {
     hpx_lco_sema_p(sem2);
     hpx_lco_sema_v(sem1);
   }
-  fprintf(stdout, "Thread 2: %d%*g\n", iter, FIELD_WIDTH,
-          hpx_time_elapsed_ms(t));
-
+  printf("Thread 2: %d%*g\n", iter, FIELD_WIDTH, hpx_time_elapsed_ms(t));
   return HPX_SUCCESS;
 }
 
@@ -50,72 +46,44 @@ static HPX_ACTION_DEF(DEFAULT, _thread2_handler, _thread2, HPX_UINT32, HPX_ADDR,
                       HPX_ADDR);
 
 static HPX_ACTION(_main, void) {
-  fprintf(stdout, HEADER);
-  fprintf(stdout, "Semaphore non contention performance\n");
-  fprintf(stdout, "%s%*s%*s\n", "# Iters " , FIELD_WIDTH, "Init time ",
-          FIELD_WIDTH, " latency (ms)");
+  printf(HEADER);
+  printf("Semaphore non contention performance\n");
+  printf("%s%*s%*s\n", "# Iters " , FIELD_WIDTH, "Init time ", FIELD_WIDTH,
+         " latency (ms)");
 
   hpx_time_t t;
   for (int i = 0, e = sizeof(num)/sizeof(num[0]); i < e; ++i) {
     const int n = num[i];
-    fprintf(stdout, "%d", n);
+    printf("%d", n);
     t = hpx_time_now();
     hpx_addr_t mutex = hpx_lco_sema_new(n);
-    fprintf(stdout, "%*g", FIELD_WIDTH, hpx_time_elapsed_ms(t));
+    printf("%*g", FIELD_WIDTH, hpx_time_elapsed_ms(t));
     t = hpx_time_now();
     for (int j = 0, e = n; j < e; ++j) {
       hpx_lco_sema_p(mutex);
       hpx_lco_sema_v(mutex);
     }
     hpx_lco_delete(mutex, HPX_NULL);
-    fprintf(stdout, "%*g\n", FIELD_WIDTH,  hpx_time_elapsed_ms(t));
+    printf("%*g\n", FIELD_WIDTH,  hpx_time_elapsed_ms(t));
   }
-  return HPX_SUCCESS;
 
-  fprintf(stdout, "\nSemaphore contention performance\n");
-  fprintf(stdout, "%s%s%*s\n", "# Thread ID ", "Iters " , FIELD_WIDTH, "latency (ms)");
+  printf("\nSemaphore contention performance\n");
+  printf("%s%s%*s\n", "# Thread ID ", "Iters " , FIELD_WIDTH, "latency (ms)");
   // Semaphore contention test
   for (int i = 0, e = sizeof(num)/sizeof(num[0]); i < e; ++i) {
-    uint32_t value = num[i];
-    hpx_addr_t peers[] = {
-      HPX_HERE,
-      HPX_HERE
-    };
+    int value = num[i];
 
-    int sizes[] = {
-      sizeof(uint32_t),
-      sizeof(uint32_t)
-    };
+    hpx_addr_t and = hpx_lco_and_new(2);
+    hpx_addr_t s1 = hpx_lco_sema_new(value);
+    hpx_addr_t s2 = hpx_lco_sema_new(value);
 
-    uint32_t array[] = {
-      0,
-      0
-    };
+    hpx_call(HPX_HERE, _thread1, and, &value, &s1, &s2);
+    hpx_call(HPX_HERE, _thread2, and, &value, &s1, &s2);
+    hpx_lco_wait(and);
 
-    void *addrs[] = {
-      &array[0],
-      &array[1]
-    };
-
-    hpx_addr_t futures[] = {
-      hpx_lco_future_new(sizeof(uint32_t)),
-      hpx_lco_future_new(sizeof(uint32_t))
-    };
-
-    hpx_addr_t sems[2] = {
-      hpx_lco_sema_new(value),
-      hpx_lco_sema_new(value)
-    };
-
-    hpx_call(peers[0], _thread1, futures[0], &value, &sems[0], &sems[1]);
-    hpx_call(peers[1], _thread2, futures[1], &value, &sems[0], &sems[1]);
-
-    hpx_lco_get_all(2, futures, sizes, addrs, NULL);
-
-    hpx_lco_delete(sems[1], HPX_NULL);
-    hpx_lco_delete(sems[0], HPX_NULL);
-    hpx_lco_delete(futures[1], HPX_NULL);
-    hpx_lco_delete(futures[0], HPX_NULL);
+    hpx_lco_delete(s2, HPX_NULL);
+    hpx_lco_delete(s1, HPX_NULL);
+    hpx_lco_delete(and, HPX_NULL);
   }
 
   hpx_shutdown(HPX_SUCCESS);
