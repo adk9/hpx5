4610f029b113a96ecf67a6f03ea6feecc59a164d
Ben Martin <benjmart@indiana.edu>
2015-02-05 10:55:16 -0500

    Renamed some instrumentation routines and files.
    Former-commit-id: d272cb0923780eed9ef2accbe8aea79ce9d4df29

--- libhpx/instrumentation/instrumentation.c
+++ libhpx/instrumentation/instrumentation.c
@@ -21,19 +21,19 @@
 
 #include <config.h>
 
-#include "libhpx/logging.h"
+#include "libhpx/instrumentation.h"
 #include "logtable.h"
 
-#ifdef HPX_LOG_ENABLED
-static logtable_t logtables[HPX_LOG_NUM_EVENTS];
-static size_t logging_max_log_size = 40*1024*1024-1;
-static bool hpx_logging_enabled = false;
-static bool log_class_enabled[HPX_LOG_NUM_CLASSES];
-static char log_dir_name[256];
+#ifdef HPX_INST_ENABLED
+static logtable_t logtables[HPX_INST_NUM_EVENTS];
+static size_t inst_max_log_size = 40*1024*1024-1;
+static bool hpx_inst_enabled = false;
+static bool inst_class_enabled[HPX_INST_NUM_CLASSES];
+static char inst_dir_name[256];
 static hpx_time_t time_start;
-static bool logging_active = false; // not whether we WANT to log, but whether 
-                                    // we are ABLE to (i.e. after initialization
-                                    // and before shutdown)
+static bool inst_active = false; // not whether we WANT to log, but whether 
+                                 // we are ABLE to (i.e. after initialization
+                                 // and before shutdown)
 
 static bool get_env_var(char *env_var_name) {
   char* env_var_text;
@@ -58,7 +58,7 @@ time_diff(uint64_t *out_s, uint64_t *out_ns, hpx_time_t *start, hpx_time_t *end)
 }
 
 static int 
-log_create(hpx_logging_class_type_t class, hpx_logging_event_type_t event, 
+log_create(hpx_inst_class_type_t class, hpx_inst_event_type_t event, 
                size_t max_size) {
   char filename[256];
   snprintf(filename, 256, "log.%d.%d.%d.log", 
@@ -71,29 +71,29 @@ log_create(hpx_logging_class_type_t class, hpx_logging_event_type_t event,
 }
 #endif
 
-int hpx_logging_init() {
-#ifdef HPX_LOG_ENABLED
+int hpx_inst_init() {
+#ifdef HPX_INST_ENABLED
   int success = -1;
 
-  hpx_logging_enabled = get_env_var("HPX_LOGGING_ENABLE");
-  if (!hpx_logging_enabled)
+  hpx_inst_enabled = get_env_var("HPX_INST_ENABLE");
+  if (!hpx_inst_enabled)
     return HPX_SUCCESS;
   
-  for (int i = 0; i < HPX_LOG_NUM_CLASSES; i++)
-    log_class_enabled[i] = false;
+  for (int i = 0; i < HPX_INST_NUM_CLASSES; i++)
+    inst_class_enabled[i] = false;
 
   char* max_log_size_text;
-  max_log_size_text = getenv("HPX_LOGGING_MAX_LOG_SIZE");
+  max_log_size_text = getenv("HPX_INST_MAX_LOG_SIZE");
   if (max_log_size_text != NULL && atoll(max_log_size_text) > 0)
-    logging_max_log_size = (size_t)atoll(max_log_size_text);
+    inst_max_log_size = (size_t)atoll(max_log_size_text);
 
   // change to user-specified root directory
-  char* log_rootdir;
-  log_rootdir = getenv("HPX_LOGGING_ROOTDIR");
-  if (log_rootdir != NULL) {
-    success = chdir(log_rootdir);
+  char* inst_rootdir;
+  inst_rootdir = getenv("HPX_INST_ROOTDIR");
+  if (inst_rootdir != NULL) {
+    success = chdir(inst_rootdir);
     if (success != 0) {
-      perror("Specified root directory for logging not found.");
+      perror("Specified root directory for instrumentation not found.");
       return -1;
     }
   }
@@ -102,26 +102,26 @@ int hpx_logging_init() {
   time_t t = time(NULL);
   struct tm lt;
   localtime_r(&t, &lt);  
-  snprintf(log_dir_name, 256, "hpx.%.2d%.2d.%.2d%.2d%.4d.%.3d", 
+  snprintf(inst_dir_name, 256, "hpx.%.2d%.2d.%.2d%.2d%.4d.%.3d", 
            lt.tm_hour, lt.tm_min, lt.tm_mday, lt.tm_mon + 1, 
            lt.tm_year + 1900, 
            hpx_get_my_rank());
-  success = mkdir(log_dir_name, 0777);
+  success = mkdir(inst_dir_name, 0777);
   if (success == 0)
-    success = chdir(log_dir_name);
+    success = chdir(inst_dir_name);
   if (success != 0) {
-    perror("Could not create output directory for logging.");
+    perror("Could not create output directory for instrumentation.");
     return -1;
   }
 
   // create log files
-  bool log_parcels = get_env_var("HPX_LOGGING_PARCELS");
-  if (log_parcels) {
-    log_class_enabled[HPX_LOG_CLASS_PARCEL] = true;
-    int class = HPX_LOG_CLASS_PARCEL;
-    for (int i = 0; i < HPX_LOG_EVENTS_PER_CLASS[class]; i++) {
-      int event = HPX_LOG_FIRST_EVENT_FOR_CLASS[class] + i;
-      int success = log_create(class, event, logging_max_log_size);
+  bool inst_parcels = get_env_var("HPX_INST_PARCELS");
+  if (inst_parcels) {
+    inst_class_enabled[HPX_INST_CLASS_PARCEL] = true;
+    int class = HPX_INST_CLASS_PARCEL;
+    for (int i = 0; i < HPX_INST_EVENTS_PER_CLASS[class]; i++) {
+      int event = HPX_INST_FIRST_EVENT_FOR_CLASS[class] + i;
+      int success = log_create(class, event, inst_max_log_size);
 
       // TODO real error handling
       if (success != HPX_SUCCESS)
@@ -131,15 +131,15 @@ int hpx_logging_init() {
 
   time_start = hpx_time_now();
 
-  logging_active = true;
+  inst_active = true;
 #endif
   return HPX_SUCCESS;
 }
 
-void hpx_logging_fini() {
-#ifdef HPX_LOG_ENABLED
-  logging_active = false;
-  for (int i = 0; i < HPX_LOG_NUM_EVENTS; i++)
+void hpx_inst_fini() {
+#ifdef HPX_INST_ENABLED
+  inst_active = false;
+  for (int i = 0; i < HPX_INST_NUM_EVENTS; i++)
     if (logtables[i].inited == true)
       logtable_fini(&logtables[i]);
 #endif
@@ -147,29 +147,29 @@ void hpx_logging_fini() {
 
 /// Record an event to the log
 /// @param class          Class this event is part of (see 
-///                       hpx_logging_class_type_t)
-/// @param event_type     The type of this event (see hpx_logging_event_type_t)
+///                       hpx_inst_class_type_t)
+/// @param event_type     The type of this event (see hpx_inst_event_type_t)
 /// @param priority       The priority of this event (may be filtered out if
 ///                       below some threshhold)
 /// @param user_data_size The size of the data to record
 /// @param user_data      The data to record (is copied)
-void hpx_logging_log_event(
-                           hpx_logging_class_type_t class,
-                           hpx_logging_event_type_t event_type,
+void hpx_inst_log_event(
+                           hpx_inst_class_type_t class,
+                           hpx_inst_event_type_t event_type,
                            int priority,
                            int user_data_size,
                            void* user_data
                            ) {
-#ifdef HPX_LOG_ENABLED
-  if (!hpx_logging_enabled)
+#ifdef HPX_INST_ENABLED
+  if (!hpx_inst_enabled)
     return;
   
-  if (!logging_active)
+  if (!inst_active)
     return;
 
   logtable_t *lt = &logtables[event_type];
   
-  hpx_logging_event_t* event = logtable_next_and_increment(lt);
+  hpx_inst_event_t* event = logtable_next_and_increment(lt);
   if (event == NULL)
     return;
 
