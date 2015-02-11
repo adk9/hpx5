7e6b43e9c16340da03fb141e28447319df195f41
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-02-10 23:22:47 -0500

    Added hpx_lco_and_set_num to set an AND lco "n" times.
    Former-commit-id: 282d1c4d9018cc70b2ad94631a50e827f26d5ee3

--- tests/unit/test_and_lco.c
+++ tests/unit/test_and_lco.c
@@ -19,10 +19,28 @@ static HPX_ACTION(_and_set, void *UNUSED) {
 }
 
 static HPX_ACTION(test_libhpx_lco_and, void *UNUSED) {
-  // Allocate an and LCO. This is synchronous. An and LCO generates an AND
-  // gate. Inputs should be >=0;
-  hpx_addr_t lco = hpx_lco_and_new(1);
-  hpx_call(HPX_HERE, _and_set, lco, NULL, 0);
+  printf("Test hpx_lco_and\n");
+  hpx_addr_t lco = hpx_lco_and_new(8);
+  for (int i = 0; i < 8; ++i) {
+    hpx_call(HPX_HERE, _and_set, lco, NULL, 0);
+  }
+  hpx_lco_wait(lco);
+  hpx_lco_delete(lco, HPX_NULL);
+  return HPX_SUCCESS;
+}
+
+static HPX_ACTION(_and_set_num, hpx_addr_t *lco) {
+  hpx_lco_and_set_num(*lco, 4, HPX_NULL);
+  return HPX_SUCCESS;
+}
+
+static HPX_ACTION(test_libhpx_lco_and_num, void *UNUSED) {
+  printf("Test hpx_lco_and_set_num\n");
+  hpx_addr_t lco = hpx_lco_and_new(8);
+  for (int i = 0; i < 4; ++i) {
+    hpx_call(HPX_HERE, _and_set, lco, NULL, 0);
+  }
+  hpx_call(HPX_HERE, _and_set_num, HPX_NULL, &lco, sizeof(lco));
   hpx_lco_wait(lco);
   hpx_lco_delete(lco, HPX_NULL);
   return HPX_SUCCESS;
@@ -30,4 +48,5 @@ static HPX_ACTION(test_libhpx_lco_and, void *UNUSED) {
 
 TEST_MAIN({
  ADD_TEST(test_libhpx_lco_and);
+ ADD_TEST(test_libhpx_lco_and_num);
 });
