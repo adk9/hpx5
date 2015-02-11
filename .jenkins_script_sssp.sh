a280af8512f943472c6d183fb5fb68a014a6c9c8
Marcin Zalewski <zalewski@indiana.edu>
2015-02-11 11:33:43 -0500

    Making sure that included jemalloc works.
    Signed-off-by: Marcin Zalewski <zalewski@indiana.edu>
    
    Former-commit-id: 7177ef67993937b82fd65dcef99952bfdc780d92

--- .jenkins_script_sssp.sh
+++ .jenkins_script_sssp.sh
@@ -47,6 +47,9 @@ cd build
 ../configure $CFGFLAGS --enable-jemalloc --enable-apps --enable-testsuite --with-check $HPXDEBUG CFLAGS="-O3"
 make -j 128
 
+# We need this for the included jemalloc. I think that libtool should take care of this, but until it does, we need this line.
+export LD_LIBRARY_PATH=$DIR/contrib/jemalloc/lib
+
 # Run the apps and check their output...
 set -xe
 export PSM_MEMORY=large
