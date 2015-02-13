cdd773f754180cbb81f073534fe83ba4836e9683
Jayashree Candadai <jayaajay@indiana.edu>
2015-02-13 16:04:08 -0500

    photon options changes to script
    Former-commit-id: e23b24159330473226ae5c9b6b404695dc0dfc49

--- .jenkins_script_br2.sh
+++ .jenkins_script_br2.sh
@@ -12,18 +12,14 @@ function add_mpi() {
 
 function add_photon() {
     # This is currently cutter-specific and needs to be generalized.
-    export HPX_USE_IB_DEV=mlx4_0
-    export HPX_USE_IB_PORT=1
-    export HPX_USE_CMA=0
-    export HPX_USE_ETH_DEV=roce0
-    export HPX_USE_BACKEND=ugni
+    export HPX_PHOTON_CARGS="--with-ugni"
 }
 
 set -xe
 export PSM_MEMORY=large
 case "$HPXMODE" in
     photon)
-	CFGFLAGS=" --with-mpi --enable-photon --with-ugni --with-hugetlbfs"
+	CFGFLAGS=" --with-mpi --enable-photon --hpx-photon-backend=ugni --with-hugetlbfs"
 	add_mpi
         add_photon
 	;;
@@ -54,7 +50,7 @@ fi
 mkdir install
 
 echo "Configuring HPX."
-echo ../configure --prefix=$DIR/build/HPX5/ CC=cc $CFGFLAGS --enable-testsuite --with-tests-cmd="aprun -n 2 -N 2" $HPXDEBUG | sh
+./configure --prefix=$DIR/build/HPX5/ CC=cc $CFGFLAGS --enable-testsuite --with-tests-cmd="aprun -n 2 -N 2" $HPXDEBUG
 
 echo "Building HPX."
 make
