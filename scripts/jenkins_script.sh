f77545b4e02be71e0623bf1750cbade12f4351a1
jayaajay <jayaajay@indiana.edu>
2015-03-05 13:37:01 -0500

    fix the unified jenkins script
    Former-commit-id: c333d5a6bc7ce504b7962240c0061414e8335f6f

--- scripts/jenkins_script.sh
+++ scripts/jenkins_script.sh
@@ -184,8 +184,15 @@ if [ "$OP" == "run" ]; then
     make check -C tests
 
     # Check the output of the unit tests:
-    if egrep -q "(FAIL:|ERROR:)\s+[1-9][0-9]*" $DIR/build/tests/unit/test-suite.log
-    then
+    if grep '^# FAIL: *0$' $DIR/build/tests/unit/test-suite.log
+     then
+       echo "FAIL: 0"
+     else
+       cat $DIR/build/tests/unit/test-suite.log
+    fi
+
+    if egrep -q "(ERROR:)\s+[1-9][0-9]*" $DIR/build/tests/unit/test-suite.log
+      then
         cat $DIR/build/tests/unit/test-suite.log
         exit 1
     fi
