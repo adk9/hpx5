935bedea3e34634ac61daa72a505a37abb659276
Luke Dalessandro <ldalessa@indiana.edu>
2015-03-12 08:32:49 -0400

    Add rudimentary hugetlbfs support to the system mmap interface.
    Former-commit-id: 40f1354e207b27d440b2ff7fed0d8f8168537e80

--- libhpx/system/linux/mmap.c
+++ libhpx/system/linux/mmap.c
@@ -19,54 +19,93 @@
 #include <libhpx/debug.h>
 #include <libhpx/system.h>
 
-void *_mmap_aligned(void *addr, size_t n, int prot, int flags, int fd, int off,
-                    size_t align) {
-  // overallocate space so that we have enough to guarantee that an aligned
-  // address exists
-  void *p = mmap(addr, n + align, prot, flags, fd, off);
-  if (!p) {
+/// A simple mmap wrapper that guarantees alignment.
+///
+/// This calls munmap at least once, and will attempt to allocate more data than
+/// is always necessary, so it should only be used on a fallback path. This
+/// implementation simply over allocates space so that we are guaranteed that
+/// there is a properly aligned region inside of the mapping. It then munmaps
+/// the parts of the allocation that aren't part of this region.
+///
+/// @param         addr A "suggested" address. This is most likely ignored.
+/// @param            n The number of bytes to map, must be 2^n.
+/// @param         prot The protection flags.
+/// @param        flags Additional flags for the mapping.
+/// @param           fd A file descriptor to map from.
+/// @param          off The file offset to map at.
+/// @param        align The alignment, must be 2^n.
+///
+/// @returns The properly-aligned mapped region, or NULL if there was an error.
+static void *_mmap_aligned(void *addr, size_t n, int prot, int flags, int fd,
+                           int off, size_t align) {
+  char *buffer = mmap(addr, n + align, prot, flags, fd, off);
+  if (!buffer) {
     dbg_error("could not map %zu bytes with %zu alignment\n", n, align);
   }
 
   // find the range in the allocation that matches what we want
-  const uintptr_t mask = (align - 1);
-  uintptr_t suffix = (uintptr_t)p & mask;
+  uintptr_t   bits = (uintptr_t)buffer;
+  uintptr_t   mask = (align - 1);
+  uintptr_t suffix = bits & mask;
   uintptr_t prefix = align - suffix;
-
-  // the range is now partitioned into three bits,
-  // [0, prefix) [prefix, prefix + n) [prefix + n, prefix + suffix + n)
-  // we'll return [prefix, prefix + n)
-  char *q = (char*)p + prefix;
 
   // return the overallocated pages back to the OS
   if (prefix) {
-    system_munmap(p, prefix);
+    system_munmap(buffer, prefix);
   }
   if (suffix) {
-    system_munmap(q + n, suffix);
+    system_munmap(buffer + prefix + n, suffix);
   }
 
   // and return the correctly aligned range
-  return q;
+  return buffer + prefix;
+}
+
+/// This mmap wrapper tries once to mmap, and then forwards to _mmap_aligned().
+///
+/// @param         addr A "suggested" address. This is most likely ignored.
+/// @param            n The number of bytes to map, must be 2^n.
+/// @param         prot The protection flags.
+/// @param        flags Additional flags for the mapping.
+/// @param           fd A file descriptor to map from.
+/// @param          off The file offset to map at.
+/// @param        align The alignment, must be 2^n.
+///
+/// @returns The properly-aligned mapped region, or NULL if there was an error.
+static void *_mmap_lucky(void *addr, size_t n, int prot, int flags, int fd,
+                         int off, size_t align) {
+  void *buffer = mmap(addr, n, prot, flags, fd, off);
+  if (!buffer) {
+    dbg_error("could not mmap %zu bytes\n", n);
+  }
+
+  uintptr_t   bits = (uintptr_t)buffer;
+  uintptr_t   mask = align - 1;
+  uintptr_t modulo = bits & mask;
+  if (!modulo) {
+    return buffer;
+  }
+
+  system_munmap(buffer, n);
+  return _mmap_aligned(addr, n, prot, flags, fd, off, align);
 }
 
 void *system_mmap(void *addr, size_t n, size_t align) {
-  static const int prot = PROT_READ | PROT_WRITE;
-  const       int flags = MAP_ANONYMOUS | MAP_PRIVATE;
-  const  uintptr_t mask = align - 1;
-
-  void *p = mmap(addr, n, prot, flags, -1, 0);
-  if ((uintptr_t)p & mask) {
-    system_munmap(p, n);
-    return _mmap_aligned(addr, n, prot, flags, -1, 0, align);
-  }
-  else {
-    return p;
-  }
+  static const  int prot = PROT_READ | PROT_WRITE;
+  static const int flags = MAP_ANONYMOUS | MAP_PRIVATE;
+  return _mmap_lucky(addr, n, prot, flags, -1, 0, align);
 }
 
-void *system_mmap_huge_pages(void *addr, size_t size, size_t align) {
-  return NULL;
+void *system_mmap_huge_pages(void *addr, size_t n, size_t align) {
+#ifndef HAVE_HUGETLBFS
+  return system_mmap(addr, n, align);
+#else
+  static const int  prot = PROT_READ | PROTE_WRITE;
+  static const int flags = MAP_PRIVATE;
+  int fd = hugetlbfs_unlinked_fd();
+  dbg_assert_str(fd > 0, "could not get huge tlb file descriptor.");
+  return _mmap_lucky(addr, n, prot, flags, fd, 0, align);
+#endif
 }
 
 void system_munmap(void *addr, size_t size) {
