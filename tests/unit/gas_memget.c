74d3b88cbdb9b5792075acf491bd57fa74db1899
Luke Dalessandro <ldalessa@indiana.edu>
2015-03-14 10:06:41 -0400

    Small style changes to the memget example.
    I switched to using the sync() interface since that's what this unit test is
    doing anyway.
    
    
    Former-commit-id: 39d3d7294de238301f9eeb613c7573ca2abf8213

--- tests/unit/gas_memget.c
+++ tests/unit/gas_memget.c
@@ -11,61 +11,54 @@
 //  Extreme Scale Technologies (CREST).
 // =============================================================================
 
-#include <inttypes.h>
 #include <stdlib.h>
 #include "hpx/hpx.h"
 #include "tests.h"
 
-static uint64_t block[] = {
+static void HPX_NORETURN fail(int i, uint64_t expected, uint64_t actual) {
+  fprintf(stderr, "failed to set element %d correctly, "
+          "expected %lu, got %lu\n", i, expected, actual);
+  exit(EXIT_FAILURE);
+}
+
+static uint64_t block[32] = {
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
   22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 };
 
 
-static HPX_ACTION(_init_array, void* args) {
-  hpx_addr_t target = hpx_thread_current_target();
-  uint64_t *local = NULL;
-  if (!hpx_gas_try_pin(target, (void**)&local))
-    return HPX_RESEND;
-
-  memcpy(local, args, hpx_thread_current_args_size());
-  hpx_gas_unpin(target);
+static HPX_PINNED(_init_array, uint64_t *local, void* args) {
+  size_t n = hpx_thread_current_args_size();
+  memcpy(local, args, n);
   return HPX_SUCCESS;
 }
-
-#define lengthof(a) sizeof(a) / sizeof(a[0])
 
 // Test code -- for memget
 static HPX_ACTION(gas_memget, void *UNUSED) {
   printf("Starting the memget test\n");
   int rank = HPX_LOCALITY_ID;
   int size = HPX_LOCALITIES;
-  int peerid = (rank+1)%size;
+  int peer = (rank+1)%size;
 
   hpx_addr_t data = hpx_gas_global_alloc(size, sizeof(block));
-  hpx_addr_t remote = hpx_addr_add(data, peerid * sizeof(block), sizeof(block));
+  hpx_addr_t remote = hpx_addr_add(data, peer * sizeof(block), sizeof(block));
 
   hpx_addr_t done = hpx_lco_future_new(sizeof(void*));
   hpx_call(remote, _init_array, done, block, sizeof(block));
   hpx_lco_wait(done);
   hpx_lco_delete(done, HPX_NULL);
 
-  uint64_t local[lengthof(block)];
-  memset(&local, 0xFF, sizeof(local));
+  uint64_t local[32];
 
-  // allocate and start a timer
+  // perform the memget, get its time for fun
   hpx_time_t t1 = hpx_time_now();
-  hpx_addr_t completed = hpx_lco_future_new(0);
-  hpx_gas_memget(&local, remote, sizeof(block), completed);
-  hpx_lco_wait(completed);
-  hpx_lco_delete(completed, HPX_NULL);
-  printf(" Elapsed: %g\n", hpx_time_elapsed_ms(t1));
+  hpx_gas_memget_sync(local, remote, sizeof(local));
+  double elapsed = hpx_time_elapsed_ms(t1);
+  printf(" Elapsed: %g\n", elapsed);
 
-  for (int i = 0, e = lengthof(block); i < e; ++i) {
+  for (int i = 0, e = 32; i < e; ++i) {
     if (local[i] != block[i]) {
-      fprintf(stderr, "failed to get element %d correctly, expected %" PRIu64
-              ", got %" PRIu64"\n", i, block[i], local[i]);
-      exit(EXIT_FAILURE);
+      fail(i, block[i], local[i]);
     }
   }
   return HPX_SUCCESS;
