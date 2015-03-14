94f8d6536746b238deac35e2e37e2deb7e22515e
Luke Dalessandro <ldalessa@indiana.edu>
2015-03-14 12:34:45 -0400

    Make HPX_NULL suitable for use as an rvalue in hpx_call*.
    Former-commit-id: 95b7332cc948547a7b37824dc3a4d760943ff81c

--- include/hpx/addr.h
+++ include/hpx/addr.h
@@ -19,15 +19,12 @@
 /// An HPX global address.
 typedef uint64_t hpx_addr_t;
 
-
 /// The equivalent of NULL for global memory
-#define HPX_NULL ((hpx_addr_t)0u)
-
+static const hpx_addr_t HPX_NULL = 0;
 
 /// An address representing this locality in general, that is suitable for use
 /// as a parcel target.
 extern hpx_addr_t HPX_HERE;
-
 
 /// An address representing some other locality, that is suitable for use as a
 /// parcel target.
@@ -36,7 +33,6 @@ extern hpx_addr_t HPX_HERE;
 ///
 /// @returns An address representing that locality.
 hpx_addr_t HPX_THERE(uint32_t i);
-
 
 /// Perform global address displacement arithmetic.
 ///
@@ -53,7 +49,6 @@ hpx_addr_t HPX_THERE(uint32_t i);
 ///
 /// @returns The address of the memory at offset @p bytes from @p addr.
 hpx_addr_t hpx_addr_add(hpx_addr_t addr, int64_t bytes, uint32_t bsize);
-
 
 /// Perform global address distance arithmetic.
 ///
@@ -72,6 +67,5 @@ hpx_addr_t hpx_addr_add(hpx_addr_t addr, int64_t bytes, uint32_t bsize);
 ///          - 0 if @p lhs == @p rhs
 ///          - negative if @p lhs < @p rhs
 int64_t hpx_addr_sub(hpx_addr_t lhs, hpx_addr_t rhs, uint32_t bsize);
-
 
 #endif
