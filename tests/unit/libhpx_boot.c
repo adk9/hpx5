d680c2805a704c82fefc3dcd5a7a30deff4250c9
Luke Dalessandro <ldalessa@indiana.edu>
2015-03-17 09:52:01 -0400

    Fix the test case.
    Include more output and some barrier synchronization to get the output to show
    up at cartain times.
    
    
    Former-commit-id: 3ba8ce9c5841733305d5e2adfcead8b8a2314865

--- tests/unit/libhpx_boot.c
+++ tests/unit/libhpx_boot.c
@@ -26,20 +26,34 @@ static int alltoall_handler(hpx_addr_t done) {
   int src[HPX_LOCALITIES][2];
   int dst[HPX_LOCALITIES][2];
 
+  int base = here->rank * here->ranks;
   for (int i = 0, e = HPX_LOCALITIES; i < e; ++i) {
-    for (int j = 0, e = 2; j < e; ++j) {
-      src[i][j] = here->rank;
-      dst[i][j] = here->rank;
-    }
+    src[i][0] = base + i;
+    src[i][1] = here->rank;
+    dst[i][0] = here->rank;
+    dst[i][1] = here->rank;
   }
 
   boot_t *boot = here->boot;
-  int e = boot_alltoall(boot, dst, src, sizeof(int), 2*sizeof(int));
+  boot_barrier(boot);
+  static tatas_lock_t lock = SYNC_TATAS_LOCK_INIT;
+  sync_tatas_acquire(&lock);
+  {
+    printf("src@%d { ", here->rank);
+    for (int i = 0, e = HPX_LOCALITIES; i < e; ++i) {
+      printf("{%d,%d} ", src[i][0], src[i][1]);
+    }
+    printf("}\n");
+    fflush(stdout);
+  }
+  sync_tatas_release(&lock);
+
+  boot_barrier(boot);
+  int e = boot_alltoall(boot, dst, src, 2*sizeof(int), 2*sizeof(int));
   if (e) {
     FAIL(dst, "boot_alltoall returned failure code\n");
   }
 
-  static tatas_lock_t lock = SYNC_TATAS_LOCK_INIT;
   sync_tatas_acquire(&lock);
   {
     printf("dst@%d { ", here->rank);
@@ -50,13 +64,14 @@ static int alltoall_handler(hpx_addr_t done) {
     fflush(stdout);
   }
   sync_tatas_release(&lock);
+  boot_barrier(boot);
 
   for (int i = 0, e = HPX_LOCALITIES; i < e; ++i) {
-    if (dst[i][0] != i) {
-      FAIL(dst, "dst[%d][0]=%d, expected %d\n", i, dst[i][0], i);
+    if (dst[i][0] != i * here->ranks + here->rank) {
+      FAIL(dst, "%d:dst[%d][0]=%d, expected %d\n", here->rank, i, dst[i][0], i * here->ranks + here->rank);
     }
-    if (dst[i][1] != here->rank) {
-      FAIL(dst, "dst[%d][1]=%d, expected %d\n", i, dst[i][1], here->rank);
+    if (dst[i][1] != i) {
+      FAIL(dst, "%d:dst[%d][1]=%d, expected %d\n", here->rank, i, dst[i][1], i);
     }
   }
 
