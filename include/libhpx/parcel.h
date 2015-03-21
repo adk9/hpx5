1a7d5ea985e8d25f9fa1cdafdc24ca9dec7f2e23
Luke Dalessandro <ldalessa@indiana.edu>
2015-03-21 09:14:14 -0400

    Parcel structure cleanup.
    * Eliminate an unused "sequence" field in the parcel structure---was an older
      instrumentation field replaced by "id".
    * Reorder the action fields---they are uint16_t, this order allows better
      structure packing.
    
    
    Former-commit-id: 8b7a675c578fdb1edc1b1d47002e98d74b7584b9

--- include/libhpx/parcel.h
+++ include/libhpx/parcel.h
@@ -35,12 +35,9 @@ struct hpx_parcel {
   struct hpx_parcel *next;
   int                 src;
   uint32_t           size;
-#if defined(ENABLE_DEBUG) || defined(ENABLE_LOGGING) || defined(ENABLE_INSTRUMENTATION)
-  uint64_t       sequence;
-#endif
   hpx_action_t     action;
-  hpx_addr_t       target;
   hpx_action_t   c_action;
+  hpx_addr_t       target;
   hpx_addr_t     c_target;
   hpx_pid_t           pid;
   uint64_t         credit;
