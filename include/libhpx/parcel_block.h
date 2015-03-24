493b307e99ea438d079a6807e5462fc0142dbb68
Luke Dalessandro <ldalessa@indiana.edu>
2015-03-24 15:23:23 -0400

    Export the parcel block so that it can be accessed from parcel.c.
    Former-commit-id: a50ad960e9fb07374124a7b0fe84ee6c6bf23360

---
+++ include/libhpx/parcel_block.h
@@ -0,0 +1,25 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013-2015, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+#ifndef LIBHPX_PARCEL_BLOCK_H
+#define LIBHPX_PARCEL_BLOCK_H
+
+#include <stddef.h>
+
+typedef struct parcel_block parcel_block_t;
+
+parcel_block_t *parcel_block_new(size_t align, size_t n, size_t *offset);
+void parcel_block_delete(parcel_block_t *block);
+void *parcel_block_at(parcel_block_t *block, size_t offset);
+void parcel_block_deduct(parcel_block_t *block, size_t bytes);
+
+#endif // LIBHPX_PARCEL_BLOCK_H
