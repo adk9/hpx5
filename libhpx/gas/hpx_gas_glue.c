f19ac881fc6f58cab635b0aa1fd8026f03c783d6
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-03-27 21:41:19 -0400

    Rename alignment to boundary, and add a boundary argument to hpx_gas_*_blocked()
    Former-commit-id: 32d322a55d71ea77dcabe2d5065a245a731de3ee

--- libhpx/gas/hpx_gas_glue.c
+++ libhpx/gas/hpx_gas_glue.c
@@ -37,27 +37,29 @@ static hpx_addr_t hpx_gas_calloc_user(size_t n, uint32_t bsize, hpx_gas_dist_t d
   dbg_error("User-defined GAS distribution not supported.\n");
 }
 
-hpx_addr_t hpx_gas_alloc(size_t n, uint32_t bsize, uint32_t alignment,
+hpx_addr_t hpx_gas_alloc(size_t n, uint32_t bsize, uint32_t boundary,
                          hpx_gas_dist_t dist) {
   dbg_assert(dist);
   int type = (int)gas_get_dist_type(dist);
   switch (type) {
    case (HPX_DIST_TYPE_LOCAL): return hpx_gas_alloc_local(n*bsize);
    case (HPX_DIST_TYPE_CYCLIC): return hpx_gas_alloc_cyclic(n, bsize);
-   case (HPX_DIST_TYPE_BLOCKED): return hpx_gas_alloc_blocked(n, bsize);
+   case (HPX_DIST_TYPE_BLOCKED):
+     return hpx_gas_alloc_blocked(n, bsize, boundary);
    case (HPX_DIST_TYPE_USER): return hpx_gas_alloc_user(n, bsize, dist);
    default: dbg_error("Unknown gas distribution type %d.\n", type);
   }
 }
 
-hpx_addr_t hpx_gas_calloc(size_t n, uint32_t bsize, uint32_t alignment,
+hpx_addr_t hpx_gas_calloc(size_t n, uint32_t bsize, uint32_t boundary,
                           hpx_gas_dist_t dist) {
   dbg_assert(dist);
   int type = (int)gas_get_dist_type(dist);
   switch (type) {
    case (HPX_DIST_TYPE_LOCAL): return hpx_gas_calloc_local(n, bsize);
    case (HPX_DIST_TYPE_CYCLIC): return hpx_gas_calloc_cyclic(n, bsize);
-   case (HPX_DIST_TYPE_BLOCKED): return hpx_gas_calloc_blocked(n, bsize);
+   case (HPX_DIST_TYPE_BLOCKED):
+     return hpx_gas_calloc_blocked(n, bsize, boundary);
    case (HPX_DIST_TYPE_USER): return hpx_gas_calloc_user(n, bsize, dist);
    default: dbg_error("Unknown gas distribution type %d.\n", type);
   }
@@ -98,14 +100,14 @@ hpx_addr_t hpx_gas_calloc_cyclic(size_t n, uint32_t bsize) {
   return here->gas->calloc_cyclic(n, bsize);
 }
 
-hpx_addr_t hpx_gas_alloc_blocked(size_t n, uint32_t bsize) {
+hpx_addr_t hpx_gas_alloc_blocked(size_t n, uint32_t bsize, uint32_t boundary) {
   dbg_assert(here && here->gas && here->gas->alloc_blocked);
-  return here->gas->alloc_blocked(n, bsize);
+  return here->gas->alloc_blocked(n, bsize, boundary);
 }
 
-hpx_addr_t hpx_gas_calloc_blocked(size_t n, uint32_t bsize) {
+hpx_addr_t hpx_gas_calloc_blocked(size_t n, uint32_t bsize, uint32_t boundary) {
   dbg_assert(here && here->gas && here->gas->calloc_blocked);
-  return here->gas->calloc_blocked(n, bsize);
+  return here->gas->calloc_blocked(n, bsize, boundary);
 }
 
 hpx_addr_t hpx_gas_alloc_local(uint32_t bsize) {
