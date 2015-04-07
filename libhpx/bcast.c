9c279276079f9469b2c3b04118ca1ed5c5a8c1e6
Luke Dalessandro <ldalessa@indiana.edu>
2015-04-07 15:57:47 -0400

    Implement the three different versions of bcast in a _vabcast handler.
    Former-commit-id: 1c125c710e4b303917537caf6dcfdf687ec6227f

--- libhpx/bcast.c
+++ libhpx/bcast.c
@@ -19,90 +19,87 @@
 // #include "libhpx/libhpx.h"
 #include <libhpx/locality.h>
 
-/// Encapsulates a RPC called on all available localities.
-int _hpx_bcast(hpx_action_t action, hpx_addr_t lsync, hpx_addr_t rsync, int
-               nargs, ...) {
-  hpx_addr_t and = HPX_NULL;
+static int _vabcast(hpx_action_t action, hpx_addr_t lsync, hpx_addr_t rsync,
+                    int nargs, va_list *vargs) {
+  int e = HPX_SUCCESS;
+  hpx_addr_t remote = HPX_NULL;
+  hpx_addr_t local = HPX_NULL;
   if (rsync) {
-    and = hpx_lco_and_new(here->ranks);
-    hpx_call_when_with_continuation(and, rsync, hpx_lco_set_action,
-                                    and, hpx_lco_delete_action, NULL, 0);
+    remote = hpx_lco_and_new(here->ranks);
+    e = hpx_call_when_with_continuation(remote, rsync, hpx_lco_set_action,
+                                        remote, hpx_lco_delete_action, NULL, 0);
+    dbg_check(e, "could not chain LCO\n");
+  }
+
+  if (lsync) {
+    local = hpx_lco_and_new(here->ranks);
+    e = hpx_call_when_with_continuation(local, lsync, hpx_lco_set_action,
+                                        local, hpx_lco_delete_action, NULL, 0);
+    dbg_check(e, "could not chain LCO\n");
   }
 
   for (int i = 0, e = here->ranks; i < e; ++i) {
-    va_list vargs;
-    va_start(vargs, nargs);
-    int e = libhpx_call_action(HPX_THERE(i), action, and, hpx_lco_set_action,
-                               HPX_NULL, HPX_NULL, nargs, &vargs);
-    dbg_check(e, "hpx_bcast returned an error.\n");
-    va_end(vargs);
+    va_list temp;
+    va_copy(temp, *vargs);
+    int e = libhpx_call_action(HPX_THERE(i), action, remote, hpx_lco_set_action,
+                               local, HPX_NULL, nargs, &temp);
+    dbg_check(e, "error generating parcel for bcast.\n");
+    va_end(temp);
   }
   return HPX_SUCCESS;
 }
 
-int _hpx_bcast_lsync(hpx_action_t action, hpx_addr_t rsync, int nargs, ...) {
-  int e;
-  hpx_addr_t lco = hpx_lco_future_new(0);
-  if (lco == HPX_NULL) {
-    e = log_error("could not allocate an LCO.\n");
-    goto unwind0;
-  }
-
-  hpx_addr_t and = hpx_lco_and_new(here->ranks);
-  hpx_call_when_with_continuation(and, lco, hpx_lco_set_action,
-                                  and, hpx_lco_delete_action, NULL, 0);
-
-  for (int i = 0, e = here->ranks; i < e; ++i) {
+int _hpx_bcast(hpx_action_t action, hpx_addr_t lsync, hpx_addr_t rsync,
+               int nargs, ...) {
     va_list vargs;
     va_start(vargs, nargs);
-    int e = libhpx_call_action(HPX_THERE(i), action, and, hpx_lco_set_action,
-                               HPX_NULL, HPX_NULL, nargs, &vargs);
-    dbg_check(e, "hpx_bcast returned an error.\n");
+    int e = _vabcast(action, lsync, rsync, nargs, &vargs);
+    dbg_check(e, "failed broadcast\n");
     va_end(vargs);
-  }
-
-  e = hpx_lco_wait(lco);
-  DEBUG_IF(e != HPX_SUCCESS) {
-    e = log_error("error waiting for bcast and gate");
-    goto unwind1;
-  }
-
- unwind1:
-  hpx_lco_delete(lco, HPX_NULL);
- unwind0:
-  return e;
+    return e;
 }
 
-int _hpx_bcastrlsync(hpx_action_t action, hpx_addr_t rsync, int nargs, ...) {
-  int e;
-  hpx_addr_t lco = hpx_lco_future_new(0);
-  if (lco == HPX_NULL) {
-    e = log_error("could not allocate an LCO.\n");
-    goto unwind0;
+int _hpx_bcast_lsync(hpx_action_t action, hpx_addr_t rsync, int nargs, ...) {
+  hpx_addr_t lsync = hpx_lco_future_new(0);
+  if (lsync == HPX_NULL) {
+    log_error("could not allocate an LCO.\n");
+    return HPX_ENOMEM;
   }
 
-  hpx_addr_t and = hpx_lco_and_new(here->ranks);
-  hpx_call_when_with_continuation(and, lco, hpx_lco_set_action,
-                                  and, hpx_lco_delete_action, NULL, 0);
+  va_list vargs;
+  va_start(vargs, nargs);
+  if (HPX_SUCCESS != _vabcast(action, lsync, rsync, nargs, &vargs)) {
+    dbg_error("failed broadcast\n");
+  }
+  va_end(vargs);
 
-  for (int i = 0, e = here->ranks; i < e; ++i) {
-    va_list vargs;
-    va_start(vargs, nargs);
-    int e = libhpx_call_action(HPX_THERE(i), action, and, hpx_lco_set_action,
-                               HPX_NULL, HPX_NULL, nargs, &vargs);
-    dbg_check(e, "hpx_bcast returned an error.\n");
-    va_end(vargs);
+  if (HPX_SUCCESS != hpx_lco_wait(lsync)) {
+    dbg_error("failed broadcast\n");
   }
 
-  e = hpx_lco_wait(lco);
-  DEBUG_IF(e != HPX_SUCCESS) {
-    e = log_error("error waiting for bcast and gate");
-    goto unwind1;
+  hpx_lco_delete(lsync, HPX_NULL);
+  return HPX_SUCCESS;
+}
+
+int _hpx_bcast_rsync(hpx_action_t action, int nargs, ...) {
+  hpx_addr_t rsync = hpx_lco_future_new(0);
+  if (rsync == HPX_NULL) {
+    log_error("could not allocate an LCO.\n");
+    return HPX_ENOMEM;
   }
 
- unwind1:
-  hpx_lco_delete(lco, HPX_NULL);
- unwind0:
-  return e;
+  va_list vargs;
+  va_start(vargs, nargs);
+  if (HPX_SUCCESS != _vabcast(action, HPX_NULL, rsync, nargs, &vargs)) {
+    dbg_error("failed broadcast\n");
+  }
+  va_end(vargs);
+
+  if (HPX_SUCCESS != hpx_lco_wait(rsync)) {
+    dbg_error("failed broadcast\n");
+  }
+
+  hpx_lco_delete(rsync, HPX_NULL);
+  return HPX_SUCCESS;
 }
 
