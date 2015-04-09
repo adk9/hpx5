990627bf62637d33cf2658c50e4f19a01d87d60e
Luke Dalessandro <ldalessa@indiana.edu>
2015-04-09 15:22:32 -0400

    Add a stub class for agas.
    Former-commit-id: 15c73e60a9f7cb9169d3ea28e1306e571d0938c9

--- libhpx/gas/agas/agas.c
+++ libhpx/gas/agas/agas.c
@@ -14,10 +14,45 @@
 # include "config.h"
 #endif
 
+#include <stdlib.h>
 #include <libhpx/boot.h>
 #include <libhpx/gas.h>
 #include "agas.h"
 
+static void _agas_delete(gas_t *gas) {
+  free(gas);
+}
+
+static gas_t _agas_vtable = {
+  .type           = HPX_GAS_AGAS,
+  .delete         = _agas_delete,
+  .local_size     = NULL,
+  .local_base     = NULL,
+  .sub            = NULL,
+  .add            = NULL,
+  .lva_to_gva     = NULL,
+  .gva_to_lva     = NULL,
+  .there          = NULL,
+  .try_pin        = NULL,
+  .unpin          = NULL,
+  .alloc_cyclic   = NULL,
+  .calloc_cyclic  = NULL,
+  .alloc_blocked  = NULL,
+  .calloc_blocked = NULL,
+  .alloc_local    = NULL,
+  .calloc_local   = NULL,
+  .free           = NULL,
+  .move           = NULL,
+  .memget         = NULL,
+  .memput         = NULL,
+  .memcpy         = NULL,
+  .owner_of       = NULL,
+  .mmap           = NULL,
+  .munmap         = NULL
+};
+
 gas_t *gas_agas_new(const config_t *config, boot_t *boot) {
-  return NULL;
+  gas_t *gas = malloc(sizeof(*gas));
+  *gas = _agas_vtable;
+  return gas;
 }
