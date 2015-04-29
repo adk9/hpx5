57e502bc30fed4b64a85ef4f412f8f795a2813b7
Luke Dalessandro <ldalessa@indiana.edu>
2015-04-29 10:13:15 -0400

    Have the block translation table inherit from our hash table base class.
    Former-commit-id: 668b5e6885d233d6c11780ab8c378c046fcb443b

--- libhpx/gas/agas/btt.cc
+++ libhpx/gas/agas/btt.cc
@@ -15,18 +15,40 @@
 #endif
 
 #include <farmhash.h>
+#include <libcuckoo/cuckoohash_map.hh>
 #include "btt.h"
 
+namespace {
+  class Entry {
+   public:
+    int64_t key;
+    int32_t rank;
+    int32_t count;
+  };
 
+  class Hasher {
+   public:
+    size_t operator()(const hpx_addr_t addr) const {
+      return util::Hash64(reinterpret_cast<const char*>(&addr), sizeof(addr));
+    }
+  };
 
+  typedef cuckoohash_map<hpx_addr_t, Entry, Hasher> BlockHashMap;
+
+  class BlockTranslationTable : BlockHashMap {
+   public:
+    BlockTranslationTable(size_t);
+  };
+}
 
 void *btt_new(size_t size) {
-  return NULL;
+  return new BlockTranslationTable(size);
 }
 
-void btt_delete(void *btt) {
+void btt_delete(void *obj) {
+  BlockTranslationTable *btt = static_cast<BlockTranslationTable*>(obj);
+  delete btt;
 }
 
-entry_t *btt_find(void *btt, hpx_addr_t addr) {
-  return NULL;
+BlockTranslationTable::BlockTranslationTable(size_t size) : BlockHashMap(size) {
 }
