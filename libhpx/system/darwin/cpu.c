074f158338a17c090392318d456f26515eeb8b9e
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-04-29 12:12:50 -0400

    HPX-224 cleanup.
    Former-commit-id: d409d94d0dc20fd9eadb5d288c2b6c44db8b926c

--- libhpx/system/darwin/cpu.c
+++ libhpx/system/darwin/cpu.c
@@ -44,8 +44,7 @@
   } while (0)
 
 
-int system_get_cores(void)
-{
+int system_get_cores(void) {
   int cores;
   size_t length = sizeof(cores);
   sysctlbyname("hw.ncpu", &cores, &length, NULL, 0);
@@ -70,17 +69,19 @@ void system_get_stack(pthread_t thread, void **base, size_t *size) {
 }
 
 int system_get_affinity_group_size(pthread_t thread, int *ncores) {
-  // First get affinity tag and check whether it is null (THREAD_AFFINITY_TAG_NULL)
-  // if it is not THREAD_AFFINITY_TAG_NULL then thread is associated with a L2 cache.
-  // Then find the logical processors associated with L2 cache.
-  // Ref: https://developer.apple.com/library/mac/releasenotes/Performance/RN-AffinityAPI/
+  // First get affinity tag and check whether it is null
+  // (THREAD_AFFINITY_TAG_NULL). If it is not THREAD_AFFINITY_TAG_NULL
+  // then thread is associated with a L2 cache. Then find the logical
+  // processors associated with L2 cache.  Ref:
+  // https://developer.apple.com/library/mac/releasenotes/Performance/RN-AffinityAPI/
   mach_msg_type_number_t count = THREAD_AFFINITY_POLICY_COUNT;
   
   thread_affinity_policy_data_t policy;
   boolean_t b = FALSE;
   
   int kr = thread_policy_get(pthread_mach_thread_np(thread),
-			     THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, &count, &b);
+			     THREAD_AFFINITY_POLICY, (thread_policy_t)&policy,
+                             &count, &b);
 
   if (kr != KERN_SUCCESS) {
     return HPX_ERROR;
@@ -89,23 +90,23 @@ int system_get_affinity_group_size(pthread_t thread, int *ncores) {
   // check whether affinity tag is null. If it is not null then thread
   // is associated with a L2 cache. Then find the number of logical
   // processors associated with L2 cache.
-  if (policy.affinity_tag != THREAD_AFFINITY_TAG_NULL) { // associated with a L2 cache
+  if (policy.affinity_tag != THREAD_AFFINITY_TAG_NULL) { 
     // Trying to get the logical processors associated with a L2 cache
     size_t size;
     if (!sysctlbyname("hw.cacheconfig", NULL, &size, NULL, 0)) {
       unsigned n = size / sizeof(uint32_t);
       uint64_t cacheconfig[n];
 
-      if ((!sysctlbyname("hw.cacheconfig", cacheconfig, &size, NULL, 0))) {
-	if (n >= 2)
+      if (!sysctlbyname("hw.cacheconfig", cacheconfig, &size, NULL, 0)) {
+	if (n >= 2) {
 	  *ncores = (int)cacheconfig[2];
-	else
+        } else {
 	  return HPX_ERROR;
+        }
       }
     }
   } else {  
     *ncores = 1;
   }
-  
   return HPX_SUCCESS;
 }
