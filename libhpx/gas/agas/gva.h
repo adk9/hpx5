efaf6ae9e01d3ce78514cd5bd3dc0b37953b7a4d
Luke Dalessandro <ldalessa@indiana.edu>
2015-04-30 08:46:28 -0400

    Added a union to extract bits from a gva.
    Former-commit-id: e0e781b3770901b14c6cb67b0c83d27f48123040

--- libhpx/gas/agas/gva.h
+++ libhpx/gas/agas/gva.h
@@ -13,14 +13,40 @@
 #ifndef LIBHPX_GAS_AGAS_GVA_H
 #define LIBHPX_GAS_AGAS_GVA_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <hpx/hpx.h>
+
+typedef union {
+  hpx_addr_t addr;
+  struct {
+    uint64_t offset:48;
+    uint64_t large:1;
+    uint64_t size_class:3;
+    uint64_t :12;
+  } bits;
+} gva_t;
+
 
 static inline uint32_t gva_home(hpx_addr_t gva) {
   return 0;
 }
 
+static inline int gva_to_size_class(hpx_addr_t gva) {
+  gva_t addr = {
+    .addr = gva
+  };
+  return  addr.bits.size_class;
+}
+
 static inline uint64_t gva_to_key(hpx_addr_t gva) {
   return gva;
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // LIBHPX_GAS_AGAS_GVA_H
