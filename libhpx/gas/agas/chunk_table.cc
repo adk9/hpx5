d33b02b128364d230d6b385db1b847694e8d759b
jayaajay <jayaajay@indiana.edu>
2015-05-02 03:17:34 -0400

    fixing one of the repeated header
    Former-commit-id: d2a1255e54f138605ba4f43ad00f129684889bb9

--- libhpx/gas/agas/chunk_table.cc
+++ libhpx/gas/agas/chunk_table.cc
@@ -10,18 +10,6 @@
 //  This software was created at the Indiana University Center for Research in
 //  Extreme Scale Technologies (CREST).
 // =============================================================================
-// =============================================================================
-//  High Performance ParalleX Library (libhpx)
-//
-//  Copyright (c) 2013-2015, Trustees of Indiana University,
-//  All rights reserved.
-//
-//  This software may be modified and distributed under the terms of the BSD
-//  license.  See the COPYING file for details.
-//
-//  This software was created at the Indiana University Center for Research in
-//  Extreme Scale Technologies (CREST).
-// =============================================================================
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
