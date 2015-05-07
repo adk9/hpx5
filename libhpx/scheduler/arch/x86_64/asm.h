ec9d531f172a6c0925649698029e59ca6125ff1b
Luke Dalessandro <ldalessa@indiana.edu>
2015-05-06 22:36:26 -0400

    Remove HPX_INTERNAL from the scheduler headers.
    Former-commit-id: 1649276a51bbc2c88b6733dc704fd52fa2513b03

--- libhpx/scheduler/arch/x86_64/asm.h
+++ libhpx/scheduler/arch/x86_64/asm.h
@@ -14,20 +14,14 @@
 #define LIBHPX_ASM_H
 
 #include <stdint.h>
-#include "hpx/attributes.h"
 
 /// This file is the header that declares all of our generic assembly
 /// functions. These are all implemented in an architecture-dependent way. These
 /// are suitable for gcc inline assembly, but are done as asm to support
 /// compilers that do not support inline asm.
 
-void get_mxcsr(uint32_t *out)
-  HPX_INTERNAL;
-
-void get_fpucw(uint16_t *out)
-  HPX_INTERNAL;
-
-void align_stack_trampoline(void)
-  HPX_INTERNAL;
+void get_mxcsr(uint32_t *out);
+void get_fpucw(uint16_t *out);
+void align_stack_trampoline(void);
 
 #endif // LIBHPX_ASM_H
