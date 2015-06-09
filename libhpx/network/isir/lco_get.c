efe2a2dda982b906e6cdf973ebccd4632eeffb30
Luke Dalessandro <ldalessa@indiana.edu>
2015-06-09 11:30:01 -0400

    Add an implementation of remote lco_get for the IS/IR network.
    The Isend/Irecv network uses parcels and the scheduler_wait_launch_when()
    operation to implement an optimized synchronous hpx_lco_get operation for remote
    LCOs. This allows us to bypass the allocation of a local proxy future for the
    operation, along with the memcpy operation to it.
    
    Unfortunately, the structure of the reply handler currently requires us to
    perform an extra copy of the data on the remote side, however parcel
    environments and continuation stacks, or explicit access to the continuation
    parcel, can eliminate that copy in the future.
    
    
    Former-commit-id: 57c466f18a420a0b524b4efd20f94e12aa7e7d38

--- libhpx/network/isir/lco_get.c
+++ libhpx/network/isir/lco_get.c
@@ -14,9 +14,61 @@
 # include "config.h"
 #endif
 
+#include <alloca.h>
+#include <string.h>
+#include <libhpx/action.h>
+#include <libhpx/debug.h>
+#include <libhpx/scheduler.h>
 #include "isir.h"
+
+typedef struct {
+  hpx_parcel_t *p;
+  void *out;
+  char data[];
+} _isir_lco_get_reply_args_t;
+
+static int
+_isir_lco_get_reply_handler(_isir_lco_get_reply_args_t *args, size_t n) {
+  size_t bytes = n - sizeof(*args);
+  if (bytes) {
+    memcpy(args->out, args->data, bytes);
+  }
+  scheduler_spawn(args->p);
+  return HPX_SUCCESS;
+}
+static HPX_ACTION(HPX_INTERRUPT, HPX_MARSHALLED, _isir_lco_get_reply,
+                  _isir_lco_get_reply_handler, HPX_POINTER, HPX_SIZE_T);
+
+static int
+_isir_lco_get_handler(hpx_parcel_t *p, size_t n, void *out) {
+  hpx_addr_t lco = hpx_thread_current_target();
+
+  dbg_assert_str(n < here->config->stacksize,
+                 "remote lco get could overflow stack\n");
+  _isir_lco_get_reply_args_t *args = alloca(sizeof(*args) + n);
+  dbg_assert(args);
+  args->p = p;
+  args->out = out;
+
+  int e;
+  // convert to wait if there's no buffer
+  if (n == 0) {
+    e = hpx_lco_wait(lco);
+  }
+  else {
+    e = hpx_lco_get(lco, n, args->data);
+  }
+  dbg_check(e, "Failure in remote get operation\n");
+
+  hpx_thread_continue(args, sizeof(*args) + n);
+}
+static HPX_ACTION(HPX_DEFAULT, 0, _isir_lco_get, _isir_lco_get_handler,
+                  HPX_POINTER, HPX_SIZE_T, HPX_POINTER);
 
 int
 isir_lco_get(void *obj, hpx_addr_t lco, size_t n, void *out) {
-  return HPX_ERROR;
+  hpx_parcel_t *current = scheduler_current_parcel();
+  hpx_parcel_t *p = parcel_create(lco, _isir_lco_get, HPX_HERE,
+                                  _isir_lco_get_reply, 3, &current, &n, &out);
+  return scheduler_wait_launch_through(p, lco);
 }
