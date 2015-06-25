cc6b385026bf328f39433ca6ea83bd5c8dbfe1ad
Jayashree Candadai <jayaajay@indiana.edu>
2015-06-25 15:14:24 -0400

    removing the extra header in intrumentation_events.c
    Former-commit-id: 86ed6acb911a43d0f73748697e852bc1c5e50f03

--- libhpx/instrumentation/instrumentation_events.c
+++ libhpx/instrumentation/instrumentation_events.c
@@ -13,21 +13,6 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
-// =============================================================================
-//  High Performance ParalleX Library (libhpx)
-//
-//  Copyright (c) 2013-2015, Trustees of Indiana University,
-//  All rights reserved.
-//
-//  This software may be modified and distributed under the terms of the BSD
-//  license.  See the COPYING file for details.
-//
-//  This software was created at the Indiana University Center for Research in
-//  Extreme Scale Technologies (CREST).
-// =============================================================================
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#endif
 
 #include <limits.h>
 #include <stdint.h>
