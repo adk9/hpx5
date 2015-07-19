2484a5c05468db3edd6aadae022c20f25b408e1e
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-07-19 12:00:25 -0400

    Remove obsolete scheduler statistics.
    Former-commit-id: 534bdddf6a9c4c7eda97429e4e736312a7f93811

--- libhpx/scheduler/stats.c
+++ libhpx/scheduler/stats.c
@@ -14,14 +14,10 @@
 #include "config.h"
 #endif
 
-/// ----------------------------------------------------------------------------
-/// @file libhpx/scheduler/statistics.c
-/// ----------------------------------------------------------------------------
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "hpx/hpx.h"
-
 #include "libsync/sync.h"
 #include "libsync/locks.h"
 #include "libhpx/scheduler.h"
@@ -29,34 +25,21 @@
 #include "libhpx/stats.h"
 
 void scheduler_stats_init(struct scheduler_stats *stats) {
-  stats->spins      = 0;
   stats->spawns     = 0;
   stats->steals     = 0;
   stats->mail       = 0;
-  stats->started    = 0;
-  stats->finished   = 0;
-  stats->progress   = 0;
-  stats->backoffs   = 0;
-  stats->max_stacks = 0;
   stats->stacks     = 0;
   stats->yields     = 0;
-  stats->backoff  = 0.0;
 }
 
 struct scheduler_stats *scheduler_stats_accum(struct scheduler_stats *lhs,
                                               const struct scheduler_stats *rhs)
 {
-  lhs->spins    += rhs->spins;
   lhs->spawns   += rhs->spawns;
   lhs->steals   += rhs->steals;
   lhs->stacks   += rhs->stacks;
   lhs->mail     += rhs->mail;
-  lhs->started  += rhs->started;
-  lhs->finished += rhs->finished;
-  lhs->progress += rhs->progress;
-  lhs->backoffs += rhs->backoffs;
   lhs->yields   += rhs->yields;
-  lhs->backoff  += rhs->backoff;
 
   return lhs;
 }
@@ -71,15 +54,10 @@ void scheduler_stats_print(const char *id, const struct scheduler_stats *counts)
   printf("node %d, ", here->rank);
   printf("thread %s, ", id);
   printf("yields: %lu, ", counts->yields);
-  printf("spins: %lu, ", counts->spins);
   printf("spawns: %lu, ", counts->spawns);
   printf("steals: %lu, ", counts->steals);
   printf("stacks: %lu, ", counts->stacks);
   printf("mail: %lu, ", counts->mail);
-  printf("started: %lu, ", counts->started);
-  printf("finished: %lu, ", counts->finished);
-  printf("progress: %lu, ", counts->progress);
-  printf("backoffs: %lu (%.1fms)", counts->backoffs, counts->backoff);
   printf("\n");
   fflush(stdout);
 #endif
