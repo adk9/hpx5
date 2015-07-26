d837711a3210d7968161639584bf3dcd5f6f2df4
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-07-26 01:18:40 -0400

    Add dlmalloc for GAS allocation.
    Former-commit-id: 590b4b9df173b911b90cf78319d2be8bf266c52f

---
+++ libhpx/network/pwc/dlmalloc_registered.c
@@ -0,0 +1,36 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013-2015, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <string.h>
+#include <libhpx/debug.h>
+#include <libhpx/memory.h>
+#include <libhpx/system.h>
+#include <malloc-2.8.6.h>
+#include "registered.h"
+#include "xport.h"
+
+void
+registered_allocator_init(pwc_xport_t *xport) {
+  void *base = system_mmap_huge_pages(NULL, NULL, bytes, 0);
+  if (!base) {
+    dbg_error("failed to mmap %zu bytes anywhere in memory\n", bytes);
+  }
+
+  memset(base, 0, bytes);
+  _xport->pin(base, bytes, NULL);
+
+  mspaces[AS_REGISTERED] = create_mspace_with_base(base, bytes, 0);
+}
