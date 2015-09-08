b1ef0dab333fda5402086705ba8f07c49b124390
Jayashree Candadai <jayaajay@indiana.edu>
2015-09-08 19:32:24 -0400

    fixing the build broken due to hpx_shutdown change to hpx_exit

--- tests/perf/collbench.c
+++ tests/perf/collbench.c
@@ -121,7 +121,7 @@ static int _main_action(int iters, size_t size) {
 
   free(sbuf);
   free(rbuf);
-  hpx_shutdown(HPX_SUCCESS);
+  hpx_exit(HPX_SUCCESS);
 }  
 static HPX_ACTION(HPX_DEFAULT, 0, _main, _main_action, HPX_INT, HPX_SIZE_T);
 
