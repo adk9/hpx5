adk9/hpx5

commit b240f267dbf5cc97710cf9786b2ddc371a3e0b68

Replace --hpx-dbg-waitonsegv with --hpx-dbg-waitonsig.
This update allows users to wait on any set of "program error signals" as
defined by glibc
(http://www.gnu.org/software/libc/manual/html_node/Program-Error-Signals.html). It
also cleans up the bitset option for log-level so that its default when
specified without any arguments is "all," while it's default when unspecified is
"none." These same rules apply for waitonsig.


Former-commit-id: 62c58fb4d7386ce9ace4d9319e2f3baad505daf4

include/libhpx/config.h
@@ -112,7 +112,6 @@ static const char* const HPX_BOOT_TO_STRING[] = {
 #define HPX_LOG_MEMORY  1024            //!< Log memory (coarse grained)
 
 static const char *const HPX_LOG_LEVEL_TO_STRING[] = {
-  "default",
   "boot",
   "sched",
   "gas",
@@ -178,6 +177,27 @@ typedef enum {
 static const char* const HPX_PROF_EVENT_TO_STRING[] = {
   "HPX_PROF_EVENT_NONE",
   "INVALID"
+};
+
+#define HPX_WAITON_NONE     (UINT64_C(0))
+#define HPX_WAITON_SIGSEGV  (UINT64_C(1) << 0)
+#define HPX_WAITON_SIGABRT  (UINT64_C(1) << 1)
+#define HPX_WAITON_SIGFPE   (UINT64_C(1) << 2)
+#define HPX_WAITON_SIGILL   (UINT64_C(1) << 3)
+#define HPX_WAITON_SIGBUS   (UINT64_C(1) << 4)
+#define HPX_WAITON_SIGIOT   (UINT64_C(1) << 5)
+#define HPX_WAITON_SIGSYS   (UINT64_C(1) << 6)
+#define HPX_WAITON_SIGTRAP  (UINT64_C(1) << 7)
+
+static const char* const HPX_WAITON_TO_STRING[] = {
+  "SIGSEGV",
+  "SIGABRT",
+  "SIGFPE",
+  "SIGILL",
+  "SIGBUS",
+  "SIGIOT",
+  "SIGSYS",
+  "SIGTRAP"
 };
 
 /// The HPX configuration type.

libhpx/util/options.ggo
@@ -73,8 +73,9 @@ typestr="[localities]"
 int optional multiple
 
 option "hpx-log-level" - "set the logging level"
-typestr="level"
-values="default","boot","sched","gas","lco","net","trans","parcel","action","config","memory","all"
+typestr="level,..."
+values="boot","sched","gas","lco","net","trans","parcel","action","config","memory","all"
+default="all"
 enum optional argoptional multiple
 
 section "HPX Debug Options"
@@ -83,11 +84,14 @@ option "hpx-dbg-waitat" - "wait for debugger at specific locality"
 typestr="[locality]"
 int optional multiple
 
-option "hpx-dbg-waitonabort" - "call hpx_wait() inside of hpx_abort() for debugging"
+option "hpx-dbg-waitonabort" - "wait inside of hpx_abort()"
 flag off
 
-option "hpx-dbg-waitonsegv" - "call hpx_wait() for SIGSEGV for debugging (unreliable)"
-flag off
+option "hpx-dbg-waitonsig" - "wait on program error signals"
+typestr="sig,..."
+values="segv","abrt","fpe","ill","bus","iot","sys","trap","all"
+default="all"
+enum optional argoptional multiple
 
 option "hpx-dbg-mprotectstacks" - "use mprotect() to bracket stacks to look for stack overflows"
 flag off

libhpx/util/parser.h
@@ -1,6 +1,6 @@
 /** @file parser.h
  *  @brief The header file for the command line option parser
- *  generated by GNU Gengetopt version 2.22.6
+ *  generated by GNU Gengetopt version 2.22
  *  http://www.gnu.org/software/gengetopt.
  *  DO NOT modify this file, since it can be overwritten
  *  @author GNU Gengetopt by Lorenzo Bettini */
@@ -20,17 +20,8 @@ extern "C" {
 #endif /* __cplusplus */
 
 #ifndef HPX_OPTION_PARSER_PACKAGE
-/** @brief the program name (used for printing errors) */
+/** @brief the program name */
 #define HPX_OPTION_PARSER_PACKAGE PACKAGE
-#endif
-
-#ifndef HPX_OPTION_PARSER_PACKAGE_NAME
-/** @brief the complete program name (used for help and version) */
-#ifdef PACKAGE_NAME
-#define HPX_OPTION_PARSER_PACKAGE_NAME PACKAGE_NAME
-#else
-#define HPX_OPTION_PARSER_PACKAGE_NAME PACKAGE
-#endif
 #endif
 
 #ifndef HPX_OPTION_PARSER_VERSION
@@ -38,15 +29,16 @@ extern "C" {
 #define HPX_OPTION_PARSER_VERSION VERSION
 #endif
 
-enum enum_hpx_gas { hpx_gas__NULL = -1, hpx_gas_arg_default = 0, hpx_gas_arg_smp, hpx_gas_arg_pgas, hpx_gas_arg_agas };
-enum enum_hpx_boot { hpx_boot__NULL = -1, hpx_boot_arg_default = 0, hpx_boot_arg_smp, hpx_boot_arg_mpi, hpx_boot_arg_pmi };
-enum enum_hpx_transport { hpx_transport__NULL = -1, hpx_transport_arg_default = 0, hpx_transport_arg_mpi, hpx_transport_arg_photon };
-enum enum_hpx_network { hpx_network__NULL = -1, hpx_network_arg_default = 0, hpx_network_arg_smp, hpx_network_arg_pwc, hpx_network_arg_isir };
-enum enum_hpx_log_level { hpx_log_level__NULL = -1, hpx_log_level_arg_default = 0, hpx_log_level_arg_boot, hpx_log_level_arg_sched, hpx_log_level_arg_gas, hpx_log_level_arg_lco, hpx_log_level_arg_net, hpx_log_level_arg_trans, hpx_log_level_arg_parcel, hpx_log_level_arg_action, hpx_log_level_arg_config, hpx_log_level_arg_memory, hpx_log_level_arg_all };
-enum enum_hpx_trace_classes { hpx_trace_classes__NULL = -1, hpx_trace_classes_arg_parcel = 0, hpx_trace_classes_arg_pwc, hpx_trace_classes_arg_sched, hpx_trace_classes_arg_lco, hpx_trace_classes_arg_process, hpx_trace_classes_arg_memory, hpx_trace_classes_arg_schedtimes, hpx_trace_classes_arg_all };
-enum enum_hpx_prof_counters { hpx_prof_counters__NULL = -1, hpx_prof_counters_arg_HPX_L1_TCM = 0, hpx_prof_counters_arg_HPX_L2_TCM, hpx_prof_counters_arg_HPX_L3_TCM, hpx_prof_counters_arg_HPX_TLB_TL, hpx_prof_counters_arg_HPX_TOT_INS, hpx_prof_counters_arg_HPX_INT_INS, hpx_prof_counters_arg_HPX_FP_INS, hpx_prof_counters_arg_HPX_LD_INS, hpx_prof_counters_arg_HPX_SR_INS, hpx_prof_counters_arg_HPX_BR_INS, hpx_prof_counters_arg_HPX_TOT_CYC, hpx_prof_counters_arg_all };
-enum enum_hpx_prof_event { hpx_prof_event__NULL = -1, hpx_prof_event_arg_none = 0 };
-enum enum_hpx_photon_backend { hpx_photon_backend__NULL = -1, hpx_photon_backend_arg_default = 0, hpx_photon_backend_arg_verbs, hpx_photon_backend_arg_ugni, hpx_photon_backend_arg_fi };
+enum enum_hpx_gas { hpx_gas_arg_default = 0 , hpx_gas_arg_smp, hpx_gas_arg_pgas, hpx_gas_arg_agas };
+enum enum_hpx_boot { hpx_boot_arg_default = 0 , hpx_boot_arg_smp, hpx_boot_arg_mpi, hpx_boot_arg_pmi };
+enum enum_hpx_transport { hpx_transport_arg_default = 0 , hpx_transport_arg_mpi, hpx_transport_arg_photon };
+enum enum_hpx_network { hpx_network_arg_default = 0 , hpx_network_arg_smp, hpx_network_arg_pwc, hpx_network_arg_isir };
+enum enum_hpx_log_level { hpx_log_level_arg_boot = 0 , hpx_log_level_arg_sched, hpx_log_level_arg_gas, hpx_log_level_arg_lco, hpx_log_level_arg_net, hpx_log_level_arg_trans, hpx_log_level_arg_parcel, hpx_log_level_arg_action, hpx_log_level_arg_config, hpx_log_level_arg_memory, hpx_log_level_arg_all };
+enum enum_hpx_dbg_waitonsig { hpx_dbg_waitonsig_arg_segv = 0 , hpx_dbg_waitonsig_arg_abrt, hpx_dbg_waitonsig_arg_fpe, hpx_dbg_waitonsig_arg_ill, hpx_dbg_waitonsig_arg_bus, hpx_dbg_waitonsig_arg_iot, hpx_dbg_waitonsig_arg_sys, hpx_dbg_waitonsig_arg_trap, hpx_dbg_waitonsig_arg_all };
+enum enum_hpx_trace_classes { hpx_trace_classes_arg_parcel = 0 , hpx_trace_classes_arg_pwc, hpx_trace_classes_arg_sched, hpx_trace_classes_arg_lco, hpx_trace_classes_arg_process, hpx_trace_classes_arg_memory, hpx_trace_classes_arg_schedtimes, hpx_trace_classes_arg_all };
+enum enum_hpx_prof_counters { hpx_prof_counters_arg_HPX_L1_TCM = 0 , hpx_prof_counters_arg_HPX_L2_TCM, hpx_prof_counters_arg_HPX_L3_TCM, hpx_prof_counters_arg_HPX_TLB_TL, hpx_prof_counters_arg_HPX_TOT_INS, hpx_prof_counters_arg_HPX_INT_INS, hpx_prof_counters_arg_HPX_FP_INS, hpx_prof_counters_arg_HPX_LD_INS, hpx_prof_counters_arg_HPX_SR_INS, hpx_prof_counters_arg_HPX_BR_INS, hpx_prof_counters_arg_HPX_TOT_CYC, hpx_prof_counters_arg_all };
+enum enum_hpx_prof_event { hpx_prof_event_arg_none = 0  };
+enum enum_hpx_photon_backend { hpx_photon_backend_arg_default = 0 , hpx_photon_backend_arg_verbs, hpx_photon_backend_arg_ugni, hpx_photon_backend_arg_fi };
 
 /** @brief Where the command line options are stored */
 struct hpx_options_t
@@ -92,23 +84,26 @@ struct hpx_options_t
   const char *hpx_sched_stackcachelimit_help; /**< @brief bound on the number of stacks to cache help description.  */
   int* hpx_log_at_arg;	/**< @brief selectively output log information.  */
   char ** hpx_log_at_orig;	/**< @brief selectively output log information original value given at command line.  */
-  unsigned int hpx_log_at_min; /**< @brief selectively output log information's minimum occurreces */
-  unsigned int hpx_log_at_max; /**< @brief selectively output log information's maximum occurreces */
+  int hpx_log_at_min; /**< @brief selectively output log information's minimum occurreces */
+  int hpx_log_at_max; /**< @brief selectively output log information's maximum occurreces */
   const char *hpx_log_at_help; /**< @brief selectively output log information help description.  */
-  enum enum_hpx_log_level *hpx_log_level_arg;	/**< @brief set the logging level.  */
+  enum enum_hpx_log_level *hpx_log_level_arg;	/**< @brief set the logging level (default='all').  */
   char ** hpx_log_level_orig;	/**< @brief set the logging level original value given at command line.  */
-  unsigned int hpx_log_level_min; /**< @brief set the logging level's minimum occurreces */
-  unsigned int hpx_log_level_max; /**< @brief set the logging level's maximum occurreces */
+  int hpx_log_level_min; /**< @brief set the logging level's minimum occurreces */
+  int hpx_log_level_max; /**< @brief set the logging level's maximum occurreces */
   const char *hpx_log_level_help; /**< @brief set the logging level help description.  */
   int* hpx_dbg_waitat_arg;	/**< @brief wait for debugger at specific locality.  */
   char ** hpx_dbg_waitat_orig;	/**< @brief wait for debugger at specific locality original value given at command line.  */
-  unsigned int hpx_dbg_waitat_min; /**< @brief wait for debugger at specific locality's minimum occurreces */
-  unsigned int hpx_dbg_waitat_max; /**< @brief wait for debugger at specific locality's maximum occurreces */
+  int hpx_dbg_waitat_min; /**< @brief wait for debugger at specific locality's minimum occurreces */
+  int hpx_dbg_waitat_max; /**< @brief wait for debugger at specific locality's maximum occurreces */
   const char *hpx_dbg_waitat_help; /**< @brief wait for debugger at specific locality help description.  */
-  int hpx_dbg_waitonabort_flag;	/**< @brief call hpx_wait() inside of hpx_abort() for debugging (default=off).  */
-  const char *hpx_dbg_waitonabort_help; /**< @brief call hpx_wait() inside of hpx_abort() for debugging help description.  */
-  int hpx_dbg_waitonsegv_flag;	/**< @brief call hpx_wait() for SIGSEGV for debugging (unreliable) (default=off).  */
-  const char *hpx_dbg_waitonsegv_help; /**< @brief call hpx_wait() for SIGSEGV for debugging (unreliable) help description.  */
+  int hpx_dbg_waitonabort_flag;	/**< @brief wait inside of hpx_abort() (default=off).  */
+  const char *hpx_dbg_waitonabort_help; /**< @brief wait inside of hpx_abort() help description.  */
+  enum enum_hpx_dbg_waitonsig *hpx_dbg_waitonsig_arg;	/**< @brief wait on program error signals (default='all').  */
+  char ** hpx_dbg_waitonsig_orig;	/**< @brief wait on program error signals original value given at command line.  */
+  int hpx_dbg_waitonsig_min; /**< @brief wait on program error signals's minimum occurreces */
+  int hpx_dbg_waitonsig_max; /**< @brief wait on program error signals's maximum occurreces */
+  const char *hpx_dbg_waitonsig_help; /**< @brief wait on program error signals help description.  */
   int hpx_dbg_mprotectstacks_flag;	/**< @brief use mprotect() to bracket stacks to look for stack overflows (default=off).  */
   const char *hpx_dbg_mprotectstacks_help; /**< @brief use mprotect() to bracket stacks to look for stack overflows help description.  */
   int hpx_dbg_syncfree_flag;	/**< @brief use synchronous GAS free operations (default=off).  */
@@ -118,21 +113,21 @@ struct hpx_options_t
   const char *hpx_inst_dir_help; /**< @brief directory to output instrumentation files help description.  */
   int* hpx_inst_at_arg;	/**< @brief set the localities to activate instrumentation at.  */
   char ** hpx_inst_at_orig;	/**< @brief set the localities to activate instrumentation at original value given at command line.  */
-  unsigned int hpx_inst_at_min; /**< @brief set the localities to activate instrumentation at's minimum occurreces */
-  unsigned int hpx_inst_at_max; /**< @brief set the localities to activate instrumentation at's maximum occurreces */
+  int hpx_inst_at_min; /**< @brief set the localities to activate instrumentation at's minimum occurreces */
+  int hpx_inst_at_max; /**< @brief set the localities to activate instrumentation at's maximum occurreces */
   const char *hpx_inst_at_help; /**< @brief set the localities to activate instrumentation at help description.  */
   enum enum_hpx_trace_classes *hpx_trace_classes_arg;	/**< @brief set the event classes to trace.  */
   char ** hpx_trace_classes_orig;	/**< @brief set the event classes to trace original value given at command line.  */
-  unsigned int hpx_trace_classes_min; /**< @brief set the event classes to trace's minimum occurreces */
-  unsigned int hpx_trace_classes_max; /**< @brief set the event classes to trace's maximum occurreces */
+  int hpx_trace_classes_min; /**< @brief set the event classes to trace's minimum occurreces */
+  int hpx_trace_classes_max; /**< @brief set the event classes to trace's maximum occurreces */
   const char *hpx_trace_classes_help; /**< @brief set the event classes to trace help description.  */
   long hpx_trace_filesize_arg;	/**< @brief set the size of each trace file.  */
   char * hpx_trace_filesize_orig;	/**< @brief set the size of each trace file original value given at command line.  */
   const char *hpx_trace_filesize_help; /**< @brief set the size of each trace file help description.  */
   enum enum_hpx_prof_counters *hpx_prof_counters_arg;	/**< @brief set which PAPI counters to use for profiling.  */
   char ** hpx_prof_counters_orig;	/**< @brief set which PAPI counters to use for profiling original value given at command line.  */
-  unsigned int hpx_prof_counters_min; /**< @brief set which PAPI counters to use for profiling's minimum occurreces */
-  unsigned int hpx_prof_counters_max; /**< @brief set which PAPI counters to use for profiling's maximum occurreces */
+  int hpx_prof_counters_min; /**< @brief set which PAPI counters to use for profiling's minimum occurreces */
+  int hpx_prof_counters_max; /**< @brief set which PAPI counters to use for profiling's maximum occurreces */
   const char *hpx_prof_counters_help; /**< @brief set which PAPI counters to use for profiling help description.  */
   enum enum_hpx_prof_event hpx_prof_event_arg;	/**< @brief set which event to profile.  */
   char * hpx_prof_event_orig;	/**< @brief set which event to profile original value given at command line.  */
@@ -218,7 +213,7 @@ struct hpx_options_t
   unsigned int hpx_log_level_given ;	/**< @brief Whether hpx-log-level was given.  */
   unsigned int hpx_dbg_waitat_given ;	/**< @brief Whether hpx-dbg-waitat was given.  */
   unsigned int hpx_dbg_waitonabort_given ;	/**< @brief Whether hpx-dbg-waitonabort was given.  */
-  unsigned int hpx_dbg_waitonsegv_given ;	/**< @brief Whether hpx-dbg-waitonsegv was given.  */
+  unsigned int hpx_dbg_waitonsig_given ;	/**< @brief Whether hpx-dbg-waitonsig was given.  */
   unsigned int hpx_dbg_mprotectstacks_given ;	/**< @brief Whether hpx-dbg-mprotectstacks was given.  */
   unsigned int hpx_dbg_syncfree_given ;	/**< @brief Whether hpx-dbg-syncfree was given.  */
   unsigned int hpx_inst_dir_given ;	/**< @brief Whether hpx-inst-dir was given.  */
@@ -265,8 +260,6 @@ struct hpx_option_parser_params
 extern const char *hpx_options_t_purpose;
 /** @brief the usage string of the program */
 extern const char *hpx_options_t_usage;
-/** @brief the description string of the program */
-extern const char *hpx_options_t_description;
 /** @brief all the lines making the help output */
 extern const char *hpx_options_t_help[];
 
@@ -277,7 +270,7 @@ extern const char *hpx_options_t_help[];
  * @param args_info the structure where option information will be stored
  * @return 0 if everything went fine, NON 0 if an error took place
  */
-int hpx_option_parser (int argc, char **argv,
+int hpx_option_parser (int argc, char * const *argv,
   struct hpx_options_t *args_info);
 
 /**
@@ -291,7 +284,7 @@ int hpx_option_parser (int argc, char **argv,
  * @return 0 if everything went fine, NON 0 if an error took place
  * @deprecated use hpx_option_parser_ext() instead
  */
-int hpx_option_parser2 (int argc, char **argv,
+int hpx_option_parser2 (int argc, char * const *argv,
   struct hpx_options_t *args_info,
   int override, int initialize, int check_required);
 
@@ -303,7 +296,7 @@ int hpx_option_parser2 (int argc, char **argv,
  * @param params additional parameters for the parser
  * @return 0 if everything went fine, NON 0 if an error took place
  */
-int hpx_option_parser_ext (int argc, char **argv,
+int hpx_option_parser_ext (int argc, char * const *argv,
   struct hpx_options_t *args_info,
   struct hpx_option_parser_params *params);
 
@@ -372,7 +365,7 @@ void hpx_option_parser_free (struct hpx_options_t *args_info);
  * @return 0 if everything went fine, NON 0 if an error took place
  * @deprecated use hpx_option_parser_config_file() instead
  */
-int hpx_option_parser_configfile (const char *filename,
+int hpx_option_parser_configfile (char * const filename,
   struct hpx_options_t *args_info,
   int override, int initialize, int check_required);
 
@@ -383,7 +376,7 @@ int hpx_option_parser_configfile (const char *filename,
  * @param params additional parameters for the parser
  * @return 0 if everything went fine, NON 0 if an error took place
  */
-int hpx_option_parser_config_file (const char *filename,
+int hpx_option_parser_config_file (char * const filename,
   struct hpx_options_t *args_info,
   struct hpx_option_parser_params *params);
 
@@ -435,15 +428,16 @@ int hpx_option_parser_string_ext (const char *cmdline, struct hpx_options_t *arg
 int hpx_option_parser_required (struct hpx_options_t *args_info,
   const char *prog_name);
 
-extern const char *hpx_option_parser_hpx_gas_values[];  /**< @brief Possible values for hpx-gas. */
-extern const char *hpx_option_parser_hpx_boot_values[];  /**< @brief Possible values for hpx-boot. */
-extern const char *hpx_option_parser_hpx_transport_values[];  /**< @brief Possible values for hpx-transport. */
-extern const char *hpx_option_parser_hpx_network_values[];  /**< @brief Possible values for hpx-network. */
-extern const char *hpx_option_parser_hpx_log_level_values[];  /**< @brief Possible values for hpx-log-level. */
-extern const char *hpx_option_parser_hpx_trace_classes_values[];  /**< @brief Possible values for hpx-trace-classes. */
-extern const char *hpx_option_parser_hpx_prof_counters_values[];  /**< @brief Possible values for hpx-prof-counters. */
-extern const char *hpx_option_parser_hpx_prof_event_values[];  /**< @brief Possible values for hpx-prof-event. */
-extern const char *hpx_option_parser_hpx_photon_backend_values[];  /**< @brief Possible values for hpx-photon-backend. */
+extern char *hpx_option_parser_hpx_gas_values[] ;	/**< @brief Possible values for hpx-gas.  */
+extern char *hpx_option_parser_hpx_boot_values[] ;	/**< @brief Possible values for hpx-boot.  */
+extern char *hpx_option_parser_hpx_transport_values[] ;	/**< @brief Possible values for hpx-transport.  */
+extern char *hpx_option_parser_hpx_network_values[] ;	/**< @brief Possible values for hpx-network.  */
+extern char *hpx_option_parser_hpx_log_level_values[] ;	/**< @brief Possible values for hpx-log-level.  */
+extern char *hpx_option_parser_hpx_dbg_waitonsig_values[] ;	/**< @brief Possible values for hpx-dbg-waitonsig.  */
+extern char *hpx_option_parser_hpx_trace_classes_values[] ;	/**< @brief Possible values for hpx-trace-classes.  */
+extern char *hpx_option_parser_hpx_prof_counters_values[] ;	/**< @brief Possible values for hpx-prof-counters.  */
+extern char *hpx_option_parser_hpx_prof_event_values[] ;	/**< @brief Possible values for hpx-prof-event.  */
+extern char *hpx_option_parser_hpx_photon_backend_values[] ;	/**< @brief Possible values for hpx-photon-backend.  */
 
 
 #ifdef __cplusplus

include/libhpx/options.def
@@ -124,7 +124,7 @@ LIBHPX_OPT_INTSET(log_, at,  HPX_LOCALITY_ALL, HPX_LOCALITY_NONE, HPX_LOCALITY_A
 LIBHPX_OPT_FLAG(dbg_, mprotectstacks, 0)
 LIBHPX_OPT_FLAG(dbg_, syncfree, 0)
 LIBHPX_OPT_FLAG(dbg_, waitonabort, 0)
-LIBHPX_OPT_FLAG(dbg_, waitonsegv, 0)
+LIBHPX_OPT_BITSET(dbg_, waitonsig, LIBHPX_OPT_BITSET_NONE)
 LIBHPX_OPT_INTSET(dbg_, waitat, HPX_LOCALITY_NONE, HPX_LOCALITY_NONE, HPX_LOCALITY_ALL)
 /// @}
 

libhpx/debug.c
@@ -113,17 +113,52 @@ static int _register_wait_on_sig(int signum) {
   return LIBHPX_OK;
 }
 
-int
-dbg_init(config_t *config) {
+int dbg_init(config_t *config) {
   int e = LIBHPX_OK;
-  if (config->dbg_waitonsegv) {
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGSEGV)) {
     e = _register_wait_on_sig(SIGSEGV);
     if (e != LIBHPX_OK) {
       return e;
     }
   }
-  if (config->dbg_waitonabort) {
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGABRT)) {
     e = _register_wait_on_sig(SIGABRT);
+    if (e != LIBHPX_OK) {
+      return e;
+    }
+  }
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGFPE)) {
+    e = _register_wait_on_sig(SIGFPE);
+    if (e != LIBHPX_OK) {
+      return e;
+    }
+  }
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGILL)) {
+    e = _register_wait_on_sig(SIGILL);
+    if (e != LIBHPX_OK) {
+      return e;
+    }
+  }
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGBUS)) {
+    e = _register_wait_on_sig(SIGBUS);
+    if (e != LIBHPX_OK) {
+      return e;
+    }
+  }
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGIOT)) {
+    e = _register_wait_on_sig(SIGIOT);
+    if (e != LIBHPX_OK) {
+      return e;
+    }
+  }
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGSYS)) {
+    e = _register_wait_on_sig(SIGSYS);
+    if (e != LIBHPX_OK) {
+      return e;
+    }
+  }
+  if (config_dbg_waitonsig_isset(config, HPX_WAITON_SIGTRAP)) {
+    e = _register_wait_on_sig(SIGTRAP);
     if (e != LIBHPX_OK) {
       return e;
     }

libhpx/util/options.c
@@ -400,7 +400,15 @@ config_print(config_t *cfg, FILE *f) {
   fprintf(f, "\nDebugging\n");
   fprintf(f, "  mprotectstacks\t%d\n", cfg->dbg_mprotectstacks);
   fprintf(f, "  waitonabort\t\t%d\n", cfg->dbg_waitonabort);
-  fprintf(f, "  waitonsegv\t\t%d\n", cfg->dbg_waitonsegv);
+  fprintf(f, "  waitonsig\t\t");
+  for (int i = 0, e = sizeof(HPX_WAITON_TO_STRING) /
+               sizeof(HPX_WAITON_TO_STRING[0]); i < e; ++i) {
+    uint64_t signal = 1lu << i;
+    if (cfg->dbg_waitonsig & signal) {
+      fprintf(f, "\"%s\", ", HPX_WAITON_TO_STRING[i]);
+    }
+  }
+  fprintf(f, "\n");
   fprintf(f, "  waitat\t\t");
   if (!cfg->dbg_waitat) {
     fprintf(f, "all");
@@ -463,7 +471,7 @@ config_print(config_t *cfg, FILE *f) {
 #ifdef HAVE_PHOTON
   fprintf(f, "\nPhoton\n");
   fprintf(f, "  backend\t\t%s\n",
-	  PHOTON_BACKEND_TO_STRING[cfg->photon_backend]);
+      PHOTON_BACKEND_TO_STRING[cfg->photon_backend]);
   fprintf(f, "  ibport\t\t%d\n", cfg->photon_ibport);
   fprintf(f, "  ledgersize\t\t%d\n", cfg->photon_ledgersize);
   fprintf(f, "  eagerbufsize\t\t%d\n", cfg->photon_eagerbufsize);

commit 212ffd3de21995bd3ccd7ad3dacd19544f0e79e2

Fix the configuration log-level regression introduced in b240f267.

include/libhpx/config.h
@@ -112,6 +112,7 @@ static const char* const HPX_BOOT_TO_STRING[] = {
 #define HPX_LOG_MEMORY  1024            //!< Log memory (coarse grained)
 
 static const char *const HPX_LOG_LEVEL_TO_STRING[] = {
+  "default",
   "boot",
   "sched",
   "gas",

libhpx/util/options.ggo
@@ -74,7 +74,7 @@ int optional multiple
 
 option "hpx-log-level" - "set the logging level"
 typestr="level,..."
-values="boot","sched","gas","lco","net","trans","parcel","action","config","memory","all"
+values="default","boot","sched","gas","lco","net","trans","parcel","action","config","memory","all"
 default="all"
 enum optional argoptional multiple
 

libhpx/util/parser.c
@@ -47,7 +47,7 @@ const char *hpx_options_t_help[] = {
   "      --hpx-sched-stackcachelimit=stacks\n                                bound on the number of stacks to cache",
   "\nLog options:",
   "      --hpx-log-at=[localities] selectively output log information",
-  "      --hpx-log-level[=level,...]\n                                set the logging level  (possible \n                                  values=\"boot\", \"sched\", \"gas\", \"lco\", \n                                  \"net\", \"trans\", \"parcel\", \"action\", \n                                  \"config\", \"memory\", \"all\" \n                                  default=`all')",
+  "      --hpx-log-level[=level,...]\n                                set the logging level  (possible \n                                  values=\"default\", \"boot\", \"sched\", \n                                  \"gas\", \"lco\", \"net\", \"trans\", \n                                  \"parcel\", \"action\", \"config\", \n                                  \"memory\", \"all\" default=`all')",
   "\nHPX Debug Options:",
   "      --hpx-dbg-waitat=[locality]\n                                wait for debugger at specific locality",
   "      --hpx-dbg-waitonabort     wait inside of hpx_abort()  (default=off)",
@@ -139,7 +139,7 @@ char *hpx_option_parser_hpx_gas_values[] = {"default", "smp", "pgas", "agas", 0}
 char *hpx_option_parser_hpx_boot_values[] = {"default", "smp", "mpi", "pmi", 0} ;	/* Possible values for hpx-boot.  */
 char *hpx_option_parser_hpx_transport_values[] = {"default", "mpi", "photon", 0} ;	/* Possible values for hpx-transport.  */
 char *hpx_option_parser_hpx_network_values[] = {"default", "smp", "pwc", "isir", 0} ;	/* Possible values for hpx-network.  */
-char *hpx_option_parser_hpx_log_level_values[] = {"boot", "sched", "gas", "lco", "net", "trans", "parcel", "action", "config", "memory", "all", 0} ;	/* Possible values for hpx-log-level.  */
+char *hpx_option_parser_hpx_log_level_values[] = {"default", "boot", "sched", "gas", "lco", "net", "trans", "parcel", "action", "config", "memory", "all", 0} ;	/* Possible values for hpx-log-level.  */
 char *hpx_option_parser_hpx_dbg_waitonsig_values[] = {"segv", "abrt", "fpe", "ill", "bus", "iot", "sys", "trap", "all", 0} ;	/* Possible values for hpx-dbg-waitonsig.  */
 char *hpx_option_parser_hpx_trace_classes_values[] = {"parcel", "pwc", "sched", "lco", "process", "memory", "schedtimes", "all", 0} ;	/* Possible values for hpx-trace-classes.  */
 char *hpx_option_parser_hpx_prof_counters_values[] = {"HPX_L1_TCM", "HPX_L2_TCM", "HPX_L3_TCM", "HPX_TLB_TL", "HPX_TOT_INS", "HPX_INT_INS", "HPX_FP_INS", "HPX_LD_INS", "HPX_SR_INS", "HPX_BR_INS", "HPX_TOT_CYC", "all", 0} ;	/* Possible values for hpx-prof-counters.  */

libhpx/util/parser.h
@@ -33,7 +33,7 @@ enum enum_hpx_gas { hpx_gas_arg_default = 0 , hpx_gas_arg_smp, hpx_gas_arg_pgas,
 enum enum_hpx_boot { hpx_boot_arg_default = 0 , hpx_boot_arg_smp, hpx_boot_arg_mpi, hpx_boot_arg_pmi };
 enum enum_hpx_transport { hpx_transport_arg_default = 0 , hpx_transport_arg_mpi, hpx_transport_arg_photon };
 enum enum_hpx_network { hpx_network_arg_default = 0 , hpx_network_arg_smp, hpx_network_arg_pwc, hpx_network_arg_isir };
-enum enum_hpx_log_level { hpx_log_level_arg_boot = 0 , hpx_log_level_arg_sched, hpx_log_level_arg_gas, hpx_log_level_arg_lco, hpx_log_level_arg_net, hpx_log_level_arg_trans, hpx_log_level_arg_parcel, hpx_log_level_arg_action, hpx_log_level_arg_config, hpx_log_level_arg_memory, hpx_log_level_arg_all };
+enum enum_hpx_log_level { hpx_log_level_arg_default = 0 , hpx_log_level_arg_boot, hpx_log_level_arg_sched, hpx_log_level_arg_gas, hpx_log_level_arg_lco, hpx_log_level_arg_net, hpx_log_level_arg_trans, hpx_log_level_arg_parcel, hpx_log_level_arg_action, hpx_log_level_arg_config, hpx_log_level_arg_memory, hpx_log_level_arg_all };
 enum enum_hpx_dbg_waitonsig { hpx_dbg_waitonsig_arg_segv = 0 , hpx_dbg_waitonsig_arg_abrt, hpx_dbg_waitonsig_arg_fpe, hpx_dbg_waitonsig_arg_ill, hpx_dbg_waitonsig_arg_bus, hpx_dbg_waitonsig_arg_iot, hpx_dbg_waitonsig_arg_sys, hpx_dbg_waitonsig_arg_trap, hpx_dbg_waitonsig_arg_all };
 enum enum_hpx_trace_classes { hpx_trace_classes_arg_parcel = 0 , hpx_trace_classes_arg_pwc, hpx_trace_classes_arg_sched, hpx_trace_classes_arg_lco, hpx_trace_classes_arg_process, hpx_trace_classes_arg_memory, hpx_trace_classes_arg_schedtimes, hpx_trace_classes_arg_all };
 enum enum_hpx_prof_counters { hpx_prof_counters_arg_HPX_L1_TCM = 0 , hpx_prof_counters_arg_HPX_L2_TCM, hpx_prof_counters_arg_HPX_L3_TCM, hpx_prof_counters_arg_HPX_TLB_TL, hpx_prof_counters_arg_HPX_TOT_INS, hpx_prof_counters_arg_HPX_INT_INS, hpx_prof_counters_arg_HPX_FP_INS, hpx_prof_counters_arg_HPX_LD_INS, hpx_prof_counters_arg_HPX_SR_INS, hpx_prof_counters_arg_HPX_BR_INS, hpx_prof_counters_arg_HPX_TOT_CYC, hpx_prof_counters_arg_all };