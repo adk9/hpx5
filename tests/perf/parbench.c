974d1137992dc0a9ae72a3cac8430692cc754d00
Abhishek Kulkarni <adkulkar@indiana.edu>
2015-09-11 18:55:08 -0400

    parbench: print nthreads correctly.

--- tests/perf/parbench.c
+++ tests/perf/parbench.c
@@ -86,13 +86,13 @@ static void _usage(FILE *f, int error) {
 
 static HPX_ACTION_DECL(_main);
 static int _main_action(int iters, int work, int ntasks) {
-  printf("parbench(iters=%d, work=%d, ntasks=%d)\n", iters, work, ntasks);
-  printf("time resolution: milliseconds\n");
-  fflush(stdout);
-
   if (ntasks == 0) {
     ntasks = HPX_THREADS;
   }
+
+  printf("parbench(iters=%d, work=%d, ntasks=%d)\n", iters, work, ntasks);
+  printf("time resolution: milliseconds\n");
+  fflush(stdout);
 
   hpx_time_t start = hpx_time_now();
   for (int i = 0; i < ntasks; ++i) {
