da2555423632994c345a67cb6ad6840b0836f297
Jayashree Candadai <jayaajay@indiana.edu>
2015-09-21 11:26:30 -0400

    updating the version of HPX

--- tests/unit/allreduce.c
+++ tests/unit/allreduce.c
@@ -10,6 +10,7 @@
 //  This software was created at the Indiana University Center for Research in
 //  Extreme Scale Technologies (CREST).
 // =============================================================================
+
 #include <stdint.h>
 #include <hpx/hpx.h>
 #include "tests.h"
