f77fcee0cf5e830946303e638dd0fdb8b19eac7a
Luke Dalessandro <ldalessa@indiana.edu>
2015-09-22 22:28:13 -0400

    Fix our dlmalloc allocation.
    This deals with the out-of-registered-memory issues that we have been
    having. Basically, it uses system malloc to deal with all malloc/free calls (so
    LD_PRELOAD something nice), and then uses an "anonymous" mspace to service
    registered allocations. It sets dlmalloc's MMAP and MUNMAP redirects to a custom
    mmap/munmap that we provide that forwards to the system's versions.
    
    This allows the registered memory to grow as necessary. It has to set some other
    dlmalloc variables to prevent issues, those can be seen in Makefile.am
    CPPFLAGS. It can't detect out-of-memory for global memory---this will just
    mmap() more space---but it will crash when we try and get offsets for that
    memory.

--- libhpx/network/pwc/dlmalloc_registered.c
+++ libhpx/network/pwc/dlmalloc_registered.c
@@ -19,18 +19,32 @@
 #include <libhpx/debug.h>
 #include <libhpx/memory.h>
 #include <libhpx/system.h>
-#include <malloc-2.8.6.h>
 #include "registered.h"
 #include "xport.h"
 
+static pwc_xport_t *_xport = NULL;
+
+void *dl_mmap_wrapper(size_t length) {
+  void *base = system_mmap_huge_pages(NULL, NULL, length, 1);
+  if (!base) {
+    dbg_error("failed to mmap %zu bytes anywhere in memory\n", length);
+  }
+  _xport->pin(base, length, NULL);
+  log_mem("mapped %zu registered bytes at %p\n", length, base);
+  return base;
+}
+
+void dl_munmap_wrapper(void *ptr, size_t length) {
+  if (!length) {
+    return;
+  }
+  _xport->unpin(ptr, length);
+  system_munmap_huge_pages(NULL, ptr, length);
+}
+
 void
 registered_allocator_init(pwc_xport_t *xport) {
-  size_t bytes = 256 * 1024 * 1024; // arbitrary, for now.
-  void *base = system_mmap_huge_pages(NULL, NULL, bytes, bytes);
-  if (!base) {
-    dbg_error("failed to mmap %zu bytes anywhere in memory\n", bytes);
-  }
-
-  xport->pin(base, bytes, NULL);
-  mspaces[AS_REGISTERED] = create_mspace_with_base(base, bytes, 1);
+  dbg_assert_str(!_xport, "registered allocator already initialized\n");
+  _xport = xport;
+  mspaces[AS_REGISTERED] = create_mspace(0, 1);
 }
