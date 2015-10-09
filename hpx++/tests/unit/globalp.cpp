b6465758cc35d6c44bbae83962ef6821bc5777ba
Nilesh Mahajan <nnmahaja@indiana.edu>
2015-10-09 01:01:09 -0400

    start some unit tests

---
+++ hpx++/tests/unit/globalp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <hpx++/hpx++.h>
+
+using namespace std;
+
+hpx_status_t test_addr_arith() {
+  int n1 = 10, n2 = 20;
+  hpx::global_ptr<uint64_t, sizeof(uint64_t)> ptr1 = hpx::gas::alloc_cyclic<uint64_t, sizeof(uint64_t)>(n1);
+  
+  // TODO populate ptr1 and dereference at some index and check the value
+  
+  auto ptr2 = hpx::gas::alloc_cyclic<uint64_t>(n2);
+  
+  auto dist = ptr2 - ptr1;
+  // is dist guaranteed to be > 0?
+  cout << "dist: " << dist << endl;
+  
+  return HPX_SUCCESS;
+}
+
+int main(int argc, char* argv[]) {
+  
+  int e = hpx::init(&argc, &argv);
+  if (e) {
+    cerr << "HPX: failed to initialize." << endl;
+    return e;
+  }
+  
+  // TODO use hpx test framework
+  test_addr_arith();
+  
+  hpx::finalize();
+  return 0;
+}
