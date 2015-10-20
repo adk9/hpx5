086bd0d253f1733ee48bee555d8861842b289daf
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-10-20 15:53:50 -0400

    Add a test case for hpx_map.

---
+++ tests/unit/map.c
@@ -0,0 +1,93 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013-2015, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+
+#include "hpx/hpx.h"
+#include "tests.h"
+
+static int _initialize_handler(float *element, float initializer) {
+  *element = initializer;
+  hpx_thread_continue(element, sizeof(*element));
+  return HPX_SUCCESS;
+}
+static HPX_ACTION(HPX_DEFAULT, HPX_PINNED, _initialize, _initialize_handler,
+                  HPX_POINTER, HPX_FLOAT);
+
+static int _multiply_handler(float *multiplicand, float multiplier) {
+  *multiplicand *= multiplier;
+  hpx_thread_continue(multiplicand, sizeof(*multiplicand));
+  return HPX_SUCCESS;
+}
+static HPX_ACTION(HPX_DEFAULT, HPX_PINNED, _multiply, _multiply_handler,
+                  HPX_POINTER, HPX_FLOAT);
+
+static int _verify_handler(float *element, float expected) {
+  return !(*element == expected);
+}
+static HPX_ACTION(HPX_DEFAULT, HPX_PINNED, _verify, _verify_handler,
+                  HPX_POINTER, HPX_FLOAT);
+
+static int map_handler(void) {
+  uint64_t bsize = 64*sizeof(float);
+  int blocks = HPX_LOCALITIES;
+  hpx_addr_t array = hpx_gas_alloc_cyclic(blocks, bsize, 0);
+  test_assert(array != HPX_NULL);
+
+  hpx_addr_t out_array = hpx_gas_alloc_cyclic(blocks, bsize, 0);
+  test_assert(out_array != HPX_NULL);
+
+  uint64_t nelts = (bsize * blocks)/sizeof(float);
+  float initializer = 4.0;
+
+  printf("Testing hpx_map...\n");
+  
+  hpx_addr_t lco = hpx_lco_future_new(0);
+  int e = hpx_map(_initialize, nelts, array, sizeof(float),
+                  out_array, sizeof(float),
+                  bsize, lco, &initializer);
+
+  test_assert(e == HPX_SUCCESS);
+  e = hpx_lco_wait(lco);
+  test_assert(e == HPX_SUCCESS);
+  hpx_lco_delete(lco, HPX_NULL);
+
+  printf("Testing hpx_map_sync...\n");
+
+  float multiplier = 5.0;
+  e = hpx_map_sync(_multiply, nelts, array, sizeof(float),
+                   out_array, sizeof(float),
+                   bsize, &multiplier);
+  test_assert(e == HPX_SUCCESS);
+
+  printf("Verifying results...\n");
+  hpx_addr_t and = hpx_lco_and_new(nelts);
+  // We should use a process map-reduce here eventually..
+  hpx_addr_t base = out_array;
+  float expected = initializer * multiplier;
+  for (int i = 0; i < nelts; ++i) {
+    e = hpx_call(base, _verify, and, &expected);
+    test_assert(e == HPX_SUCCESS);
+    base = hpx_addr_add(base, sizeof(float), bsize);
+  }
+
+  e = hpx_lco_wait(and);
+  test_assert(e == HPX_SUCCESS);
+  hpx_lco_delete(and, HPX_NULL);
+
+  hpx_gas_free(array, HPX_NULL);
+  return HPX_SUCCESS;
+}
+static HPX_ACTION(HPX_DEFAULT, 0, map, map_handler);
+
+TEST_MAIN({
+    ADD_TEST(map, 0);
+});
