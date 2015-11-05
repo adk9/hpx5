cf5536299802fec523ef36d6622b5b70bc55e4dc
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-11-04 22:27:01 -0500

    Add build support for percolation.

---
+++ libhpx/percolation/percolation.c
@@ -0,0 +1,30 @@
+// =============================================================================
+//  High Performance ParalleX Library (libhpx)
+//
+//  Copyright (c) 2013-2015, Trustees of Indiana University,
+//  All rights reserved.
+//
+//  This software may be modified and distributed under the terms of the BSD
+//  license.  See the COPYING file for details.
+//
+//  This software was created at the Indiana University Center for Research in
+//  Extreme Scale Technologies (CREST).
+// =============================================================================
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <hpx/hpx.h>
+#include <libhpx/config.h>
+#include <libhpx/debug.h>
+#include <libhpx/libhpx.h>
+#include <libhpx/locality.h>
+#include <libhpx/profiling.h>
+#include <libsync/sync.h>
+
