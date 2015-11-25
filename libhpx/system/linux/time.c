6c9c98fabacb74947e4543bb26ede447f405758c
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2015-11-25 17:45:39 -0500

    Restructure/reorganize the time code.
    Pull out common time related code into a top-level platform-independent
    file (time.c). This deduplicates code and keeps the platform-specific
    time subset small.

--- libhpx/system/linux/time.c
+++ libhpx/system/linux/time.c
@@ -15,7 +15,7 @@
 # include "config.h"
 #endif
 
-/// @file libhpx/platform/linux/time.c
+/// @file libhpx/system/linux/time.c
 /// @brief Implements HPX's time interface on linux.
 #include <assert.h>
 #include <stdio.h>
@@ -23,8 +23,6 @@
 #include <stdlib.h>
 #include <hpx/hpx.h>
 #include <libhpx/debug.h>
-
-static hpx_time_t _beginning_of_time;
 
 hpx_time_t hpx_time_now(void) {
   hpx_time_t time;
@@ -34,16 +32,8 @@ hpx_time_t hpx_time_now(void) {
   return time;
 }
 
-static double _diff_ns(hpx_time_t from, hpx_time_t to) {
-  return (double)(((to.tv_sec - from.tv_sec) * 1e9) + (to.tv_nsec - from.tv_nsec));
-}
-
-double hpx_time_diff_us(hpx_time_t from, hpx_time_t to) {
-  return _diff_ns(from, to)/1e3;
-}
-
-double hpx_time_diff_ms(hpx_time_t from, hpx_time_t to) {
-  return _diff_ns(from, to)/1e6;
+double hpx_time_ns(hpx_time_t time) {
+  return (time.tv_sec * 1e9) + time.tv_nsec;
 }
 
 void hpx_time_diff(hpx_time_t start, hpx_time_t end, hpx_time_t *diff) {
@@ -54,43 +44,6 @@ void hpx_time_diff(hpx_time_t start, hpx_time_t end, hpx_time_t *diff) {
     diff->tv_sec = end.tv_sec - start.tv_sec;
     diff->tv_nsec = end.tv_nsec - start.tv_nsec;
   }
-}
-
-double hpx_time_elapsed_us(hpx_time_t from) {
-  return hpx_time_diff_us(from, hpx_time_now());
-}
-
-double hpx_time_elapsed_ms(hpx_time_t from) {
-  return hpx_time_diff_ms(from, hpx_time_now());
-}
-
-void hpx_time_elapsed(hpx_time_t start, hpx_time_t *diff) {
-  hpx_time_diff(start, hpx_time_now(), diff);
-}
-
-static double _ns(hpx_time_t time) {
-  return (time.tv_sec * 1e9) + time.tv_nsec;
-}
-
-double hpx_time_us(hpx_time_t time) {
-  return _ns(time)/1e3;
-}
-
-double hpx_time_ms(hpx_time_t time) {
-  return _ns(time)/1e6;
-}
-
-int64_t hpx_time_diff_ns(hpx_time_t from, hpx_time_t to) {
-  return (int64_t)(((to.tv_sec - from.tv_sec) * 1e9) + (to.tv_nsec - from.tv_nsec));
-}
-
-uint64_t hpx_time_elapsed_ns(hpx_time_t from) {
-  return (uint64_t)hpx_time_diff_ns(from, hpx_time_now());
-}
-
-
-uint64_t hpx_time_to_ns(hpx_time_t t) {
-  return (uint64_t)hpx_time_diff_ns(_beginning_of_time, t);
 }
 
 hpx_time_t hpx_time_construct(unsigned long s, unsigned long ns) {
@@ -115,10 +68,3 @@ hpx_time_t hpx_time_point(hpx_time_t time, hpx_time_t duration) {
   return t;
 }
 
-void libhpx_time_start() {
-  _beginning_of_time = hpx_time_now();
-}
-
-hpx_time_t libhpx_beginning_of_time() {
-  return _beginning_of_time;
-}
