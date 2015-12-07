408d7a9fe7b594d5021f0518542c801cf5a2db4d
Nilesh Mahajan <nnmahaja@indiana.edu>
2015-12-06 21:38:23 -0500

    HPXPP_REGISTER_MACRO

--- include/hpx++/action.h
+++ include/hpx++/action.h
@@ -15,27 +15,147 @@
 #define ACTION_H
 
 extern "C" {
+  #include "hpx/addr.h"
   #include "hpx/types.h"
   #include "hpx/action.h"
+  #include "hpx/rpc.h"
 }
 
+#include <hpx++/lco.h>
+
 #include <cstdlib>
+#include <tuple>
+#include <type_traits>
 
 namespace hpx {
   
-  namespace action {
+  namespace detail {
+    // reference: https://functionalcpp.wordpress.com/2013/08/05/function-traits/
+    template<class F>
+    struct function_traits;
     
-    template <typename F>
+    // function pointer
+    template<class R, class... Args>
+    struct function_traits<R(*)(Args...)> : public function_traits<R(Args...)>
+    {};
+
+    template<class R, class... Args>
+    struct function_traits<R(&)(Args...)> : public function_traits<R(Args...)>
+    {};
+
+    template<class R, class... Args>
+    struct function_traits<R(Args...)> {
+	using return_type = R;
+	static constexpr std::size_t arity = sizeof...(Args);
+	static constexpr auto arg_types_tpl = std::tuple<Args...>();
+    };
+    
+    template <::std::size_t I, typename FT, typename... Args>
+    struct matching_helper {
+      using f_type = typename ::std::tuple_element< I, typename FT::arg_types >::type;
+      using s_type = typename ::std::tuple_element< I, std::tuple<Args...> >::type;
+      static constexpr bool value = ::std::is_same< f_type, s_type >::value;
+    };
+
+    template <typename FT, typename... Args>
+    struct is_matching {
+      static constexpr bool value = (FT::arity == sizeof...(Args)) && matching_helper<0, FT, Args...>::value;
+    };
+    
+    // reference: http://stackoverflow.com/questions/17424477/implementation-c14-make-integer-sequence
+    // using aliases for cleaner syntax
+    template<class T> using Invoke = typename T::type;
+
+    template<unsigned...> struct seq{ using type = seq; };
+
+    template<class S1, class S2> struct concat;
+
+    template<unsigned... I1, unsigned... I2>
+    struct concat<seq<I1...>, seq<I2...>>
+      : seq<I1..., (sizeof...(I1)+I2)...>{};
+
+    template<class S1, class S2>
+    using Concat = Invoke<concat<S1, S2>>;
+
+    template<unsigned N> struct gen_seq;
+    template<unsigned N> using GenSeq = Invoke<gen_seq<N>>;
+
+    template<unsigned N>
+    struct gen_seq : Concat< GenSeq<N/2>, GenSeq<N - N/2> >{};
+
+    template<> struct gen_seq<0> : seq<>{};
+    template<> struct gen_seq<1> : seq<0>{};
+    
+    template <typename T>
     inline
-    int register_action(F func) {
-      
-      // TODO analyze F and call HPX_REGISTER_ACTION
-      
-      return HPX_SUCCESS;
+    ::std::tuple<decltype(HPX_POINTER), decltype(HPX_SIZE_T)>
+    xform(T&& t) {
+      return ::std::make_tuple(HPX_POINTER, HPX_SIZE_T);
     }
     
+    struct action_struct {
+      /*
+      * 
+      /// @param        type The type of the action (THREAD, TASK, INTERRUPT, ...).
+      /// @param        attr The attribute of the action (PINNED, PACKED, ...).
+      /// @param     handler The action handler (the function).
+      /// @param          id The action id (the hpx_action_t address).
+      /// @param __VA_ARGS__ The parameter types (HPX_INT, ...).
+      #define HPX_REGISTER_ACTION(type, attr, id, handler, ...)
+      */
+      // for 0 args, HPX_REGISTER_ACTION is not HPX_MARSHALLED
+      template <typename F, typename Tpl>
+      static int _register_helper(hpx_action_t id, F f, Tpl&& t, seq<>&& s) {
+	return HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_ATTR_NONE, id, f);
+      }
+      template <typename F, typename Tpl, unsigned... Is>
+      static int _register_helper(hpx_action_t id, F f, Tpl&& t, seq<Is...>&& s) {
+	return HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_MARSHALLED, id, f, ::std::get<Is>(t)...);
+      }
+      
+      template <typename F, typename Tpl, unsigned... Is>
+      static int _register(hpx_action_t id, F f, Tpl&& arg_tpl, seq<Is...>&& s) {
+	auto tpl = ::std::tuple_cat(xform(::std::get<Is>(arg_tpl))...);
+	return _register_helper(id, f, tpl, gen_seq<::std::tuple_size<decltype(tpl)>::value>());
+      }
+      
+    };
     
+    template <typename T, typename F>
+    inline
+    int _register_action(F f) {
+      return T::_register(T::id, f, T::traits::arg_types_tpl,
+		  hpx::detail::gen_seq<::std::tuple_size<decltype(T::traits::arg_types_tpl)>::value>());
+    }
+  }
+  
+  /*
+   * int    _hpx_call(hpx_addr_t addr, hpx_action_t action, hpx_addr_t result,
+                 int nargs, ...) HPX_PUBLIC;
+     #define hpx_call(addr, action, result, ...) \
+	_hpx_call(addr, action, result, __HPX_NARGS(__VA_ARGS__) , ##__VA_ARGS__)
+   */
+  template <typename F, typename RType, typename... Args>
+  int call_sync(hpx_addr_t addr, F && f, RType& result, Args... args) {
+    static_assert(hpx::detail::is_matching<hpx::detail::function_traits<decltype(f)>, Args...>::value, 
+		  "action and argument types do not match");
+    // TODO make lco out of result and pass that here
+    return hpx_call_sync(addr, f, HPX_NULL, args...);
   }
 }
+
+// 
+#define HPXPP_REGISTER_ACTION(f)						\
+struct f##_action_struct : public hpx::detail::action_struct {			\
+  static hpx_action_t id;							\
+  using traits = hpx::detail::function_traits<decltype(f)>;			\
+  \
+  template <typename... Args>							\
+  int operator()(hpx_addr_t addr, traits::return_type& result, Args... args) {	\
+    return 0;									\
+  }										\
+};										\
+hpx_action_t f##_action_struct::id = 0;						\
+int f##_action_struct_dummy = hpx::detail::_register_action<f##_action_struct>(f);
 
 #endif
