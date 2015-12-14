b109029084defef2e0f2617860b69a6174705996
Nilesh Mahajan <nnmahaja@indiana.edu>
2015-12-14 00:30:23 -0500

    arg passing not working

--- hpx++/tests/unit/action_tests.cpp
+++ hpx++/tests/unit/action_tests.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "hpx++/hpx++.h"
 
+using namespace std;
+
 int hello(int a) {
-  std::cout << "Hello World from " << hpx_get_my_rank() << "." << std::endl;
+  std::cout << "Rank#" << hpx_get_my_rank() << " received " << a << "." << std::endl;
   hpx::exit(HPX_SUCCESS);
 }
 
 HPXPP_REGISTER_ACTION(hello);
 
-hpx_status_t test1() {
+hpx_status_t test1(int arg) {
   
   hello_action_struct obj;
   int r;
@@ -17,23 +19,27 @@ hpx_status_t test1() {
 //   obj(HPX_HERE, r);
 //   obj(HPX_HERE, r, "");
   
-  int a = 1;
-  obj(HPX_HERE, r, a);
-//   hpx::run(&hello_action_struct::id, &a, sizeof(int));
+  obj.call_sync(HPX_HERE, r, arg);
   
   return HPX_SUCCESS;
 }
 
-int main_act() {
-  hello_action_struct obj;
-  int r, a = 1;
-  obj(HPX_HERE, r, a);
-//   hpx::run(&hello_action_struct::id, &a, sizeof(int));
+int main_act(int* arg, size_t sz) {
+  std::cout << "main action begin..." << std::endl;
+  
+//   hello_action_struct obj;
+//   int r;
+//   obj.call_sync(HPX_HERE, r, arg);
+  
+  cout << "main " << arg << endl;
+  
+  test1(arg);
+  
   hpx::exit(HPX_SUCCESS);
 }
 
-// HPXPP_REGISTER_ACTION(main_act);
-static hpx_action_t m_a_id = 0;
+HPXPP_REGISTER_ACTION(main_act);
+// static hpx_action_t m_a_id = 0;
 
 int main(int argc, char* argv[]) {
   
@@ -42,10 +48,12 @@ int main(int argc, char* argv[]) {
     std::cerr << "HPX: failed to initialize." << std::endl;
     return e;
   }
+  int a = hpx_get_my_rank();
+  main_act_action_struct obj;
+  obj(a);
   
-//   hpx::run(&(main_act_action_struct::id));
-  HPX_REGISTER_ACTION(HPX_DEFAULT, 0, m_a_id, main_act);
-  hpx::run(&m_a_id);
+//   HPX_REGISTER_ACTION(HPX_DEFAULT, 0, m_a_id, main_act);
+//   hpx::run(&m_a_id);
   hpx::finalize();
   return 0;
 }
