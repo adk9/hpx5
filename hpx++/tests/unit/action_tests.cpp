17fc075d3e1d92666082fbfec6dbd749cbe34ca1
Nilesh Mahajan <nnmahaja@indiana.edu>
2015-12-14 01:25:42 -0500

    actions with no params crash, typed stuff working

--- hpx++/tests/unit/action_tests.cpp
+++ hpx++/tests/unit/action_tests.cpp
@@ -3,11 +3,37 @@
 
 using namespace std;
 
+// static int _my_interrupt_handler(void) {
+//   printf("Hi, I am an interrupt!\n");
+//   return HPX_SUCCESS;
+// }
+// static HPX_ACTION(HPX_INTERRUPT, 0, _my_interrupt, _my_interrupt_handler);
+// 
+// static int _my_task_handler(void) {
+//   printf("Hi, I am a task!\n");
+//   hpx_call_cc(HPX_HERE, _my_interrupt, NULL, NULL);
+//   return HPX_SUCCESS;
+// }
+// static HPX_ACTION(HPX_TASK, 0, _my_task, _my_task_handler);
+// 
+// static int _my_action_handler(void) {
+//   printf("Hi, I am an action!\n");
+//   hpx_call_cc(HPX_HERE, _my_task, NULL, NULL);
+//   return HPX_SUCCESS;
+// }
+// HPXPP_REGISTER_ACTION(_my_action_handler);
+
+static int _my_typed_handler(int i, float f, char c) {
+  printf("Hi, I am a typed action with args: %d %f %c!\n", i, f, c);
+//   hpx_call_cc(HPX_HERE, _my_action_handler_action_struct::id, NULL, NULL);
+  return HPX_SUCCESS;
+}
+HPXPP_REGISTER_ACTION(_my_typed_handler);
+
 int hello(int a) {
   std::cout << "Rank#" << hpx_get_my_rank() << " received " << a << "." << std::endl;
-  hpx::exit(HPX_SUCCESS);
+  return HPX_SUCCESS;
 }
-
 HPXPP_REGISTER_ACTION(hello);
 
 hpx_status_t test1(int arg) {
@@ -24,22 +50,21 @@ hpx_status_t test1(int arg) {
   return HPX_SUCCESS;
 }
 
-int main_act(int* arg, size_t sz) {
+
+
+int main_act(int arg) {
   std::cout << "main action begin..." << std::endl;
   
-//   hello_action_struct obj;
-//   int r;
-//   obj.call_sync(HPX_HERE, r, arg);
-  
-  cout << "main " << arg << endl;
-  
   test1(arg);
+  
+  _my_typed_handler_action_struct typed_o;
+  int r, i = 1; float f = 3.0; char c = 'b';
+  typed_o.call_sync(HPX_HERE, r, i, f, c);
   
   hpx::exit(HPX_SUCCESS);
 }
 
 HPXPP_REGISTER_ACTION(main_act);
-// static hpx_action_t m_a_id = 0;
 
 int main(int argc, char* argv[]) {
   
@@ -48,12 +73,10 @@ int main(int argc, char* argv[]) {
     std::cerr << "HPX: failed to initialize." << std::endl;
     return e;
   }
-  int a = hpx_get_my_rank();
+  int a = hpx_get_my_rank() + 1;
   main_act_action_struct obj;
   obj(a);
   
-//   HPX_REGISTER_ACTION(HPX_DEFAULT, 0, m_a_id, main_act);
-//   hpx::run(&m_a_id);
   hpx::finalize();
   return 0;
 }
