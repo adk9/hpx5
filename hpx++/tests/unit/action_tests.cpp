447163b9f2cad9897d9757e547eac8efcc41f1ea
Nilesh Mahajan <nnmahaja@indiana.edu>
2015-12-13 21:14:29 -0500

    problem with no args funcs

--- hpx++/tests/unit/action_tests.cpp
+++ hpx++/tests/unit/action_tests.cpp
@@ -3,7 +3,7 @@
 
 int hello(int a) {
   std::cout << "Hello World from " << hpx_get_my_rank() << "." << std::endl;
-  return 0;
+  hpx::exit(HPX_SUCCESS);
 }
 
 HPXPP_REGISTER_ACTION(hello);
@@ -12,9 +12,28 @@ hpx_status_t test1() {
   
   hello_action_struct obj;
   int r;
-  obj(HPX_HERE, r, 1);
+  
+  // commented code should fail to compile
+//   obj(HPX_HERE, r);
+//   obj(HPX_HERE, r, "");
+  
+  int a = 1;
+  obj(HPX_HERE, r, a);
+//   hpx::run(&hello_action_struct::id, &a, sizeof(int));
+  
   return HPX_SUCCESS;
 }
+
+int main_act() {
+  hello_action_struct obj;
+  int r, a = 1;
+  obj(HPX_HERE, r, a);
+//   hpx::run(&hello_action_struct::id, &a, sizeof(int));
+  hpx::exit(HPX_SUCCESS);
+}
+
+// HPXPP_REGISTER_ACTION(main_act);
+static hpx_action_t m_a_id = 0;
 
 int main(int argc, char* argv[]) {
   
@@ -24,7 +43,9 @@ int main(int argc, char* argv[]) {
     return e;
   }
   
-  test1();
+//   hpx::run(&(main_act_action_struct::id));
+  HPX_REGISTER_ACTION(HPX_DEFAULT, 0, m_a_id, main_act);
+  hpx::run(&m_a_id);
   hpx::finalize();
   return 0;
 }
