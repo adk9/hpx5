665f88d6097eeeb27d83787e15479bb700080b90
Luke Dalessandro <ldalessa@indiana.edu>
2016-01-08 17:00:10 -0500

    Use uintptr_t for 32-bit agnosticism.

--- libhpx/gas/smp/smp.c
+++ libhpx/gas/smp/smp.c
@@ -55,11 +55,7 @@ _smp_add(const void *gas, hpx_addr_t gva, int64_t bytes, uint32_t bsize) {
 /// Compute the global address for a local address.
 static hpx_addr_t
 _smp_lva_to_gva(const void *lva) {
-#ifdef __LP64__
-  return (hpx_addr_t)lva;
-#else
-  return (hpx_addr_t)(uint32_t)lva;
-#endif
+  return (hpx_addr_t)(uintptr_t)lva;
 }
 
 /// Perform address translation and pin the global address.
