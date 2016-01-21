37a6747f46d68363c5d85e6dd296815ed77e1fdf
Luke Dalessandro <ldalessa@indiana.edu>
2016-01-21 15:12:37 -0500

    Update the global pointer implementation to support generic pointers and perform normal pointer comparisons.

--- include/hpx/cxx/global_ptr.h
+++ include/hpx/cxx/global_ptr.h
@@ -35,14 +35,16 @@ template <typename T>
 class gp_subscr;
 
 /// An HPX global address ptr.
-/// Basically wraps hpx_addr_t and provides address arithmetic and pin/unpin methods
+///
+/// Basically wraps hpx_addr_t and provides address arithmetic and pin/unpin
+/// methods.
 template <typename T>
 class global_ptr {
 
  public:
   typedef T value_type;
 
-  global_ptr() : _gbl_ptr(HPX_NULL), _elems_per_blk(1) {}
+  global_ptr() : _gbl_ptr(HPX_NULL), _elems_per_blk(0) {}
   global_ptr(hpx_addr_t addr, uint32_t b_s=1) : _gbl_ptr(addr), _elems_per_blk(b_s) {}
 
   /// return raw hpx_addr_t
@@ -81,10 +83,114 @@ class global_ptr {
 
   inline
   gp_subscr<T> operator[](size_t index) const;
+
+  global_ptr<T> operator+(ptrdiff_t n) const {
+    ptrdiff_t bytes = n * sizeof(T);
+    hpx_addr_t addr = hpx_addr_add(_gbl_ptr, bytes, bsize());
+    return global_ptr<T>(addr, _elems_per_blk);
+  }
+
+  ptrdiff_t operator-(const global_ptr<T>& rhs) const {
+    assert(_elems_per_blk == rhs.get_block_size());
+    return hpx_addr_sub(_gbl_ptr, rhs.ptr(), bsize());
+  }
+
+ protected:
+  size_t bsize() const {
+    return _elems_per_blk * sizeof(T);
+  }
+
  private:
   hpx_addr_t _gbl_ptr;
   size_t _elems_per_blk;
 }; // template class global_ptr
+
+/// Special case global pointers to void with a template specialization.
+///
+/// These serve the same roll as void* pointers do. Other pointers can be cast
+/// to global_ptr<void> and global_ptr<void> will be cast to other pointers,
+/// however we can't pin, unpin, or perform address arithmetic on them.
+template <>
+class global_ptr<void> {
+ public:
+  /// Default constructor.
+  ///
+  /// This constructs a global_ptr<void> to NULL.
+  global_ptr<void>() : _impl(0), _bsize(0) {
+  }
+
+  /// Construct a generic global pointer from a generic HPX address and block
+  /// size.
+  global_ptr<void>(hpx_addr_t addr, size_t bsize) : _impl(addr), _bsize(bsize) {
+  }
+
+  /// Construct a generic global pointer from a pointer to any other type---this
+  /// serves to handle pointer casts as well.
+  template <typename U>
+  global_ptr<void>(const global_ptr<U>& ptr)
+  : _impl(ptr.ptr()),
+    _bsize(ptr.get_block_size()) {
+  }
+
+  /// Implicitly cast back to a typed smart pointer.
+  template <typename U>
+  operator global_ptr<U>() {
+    return global_ptr<U>(_impl, _bsize);
+  }
+
+  /// Support any user that wants to get the underlying HPX address.
+  hpx_addr_t ptr() const {
+    return _impl;
+  }
+
+  /// Support any user that wants to get the underlying HPX block size.
+  size_t get_block_size() const {
+    return _bsize;
+  }
+
+ private:
+  hpx_addr_t _impl;
+  size_t _bsize;
+};
+
+template <typename T, typename U>
+bool operator==(const global_ptr<T>& lhs, const global_ptr<U> &rhs) {
+  return ((lhs - rhs) == 0);
+}
+
+template <typename T, typename U>
+bool operator!=(const global_ptr<T>& lhs, const global_ptr<U> &rhs) {
+  return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator<(const global_ptr<T>& lhs, const global_ptr<U> &rhs) {
+  assert(lhs.get_block_size() == rhs.get_block_size());
+  return ((lhs - rhs) < 0);
+}
+
+template <typename T, typename U>
+bool operator<=(const global_ptr<T>& lhs, const global_ptr<U> &rhs) {
+  assert(lhs.get_block_size() == rhs.get_block_size());
+  return ((lhs - rhs) <= 0);
+}
+
+template <typename T, typename U>
+bool operator>(const global_ptr<T>& lhs, const global_ptr<U> &rhs) {
+  assert(lhs.get_block_size() == rhs.get_block_size());
+  return ((lhs - rhs) > 0);
+}
+
+template <typename T, typename U>
+bool operator>=(const global_ptr<T>& lhs, const global_ptr<U> &rhs) {
+  assert(lhs.get_block_size() == rhs.get_block_size());
+  return ((lhs - rhs) >= 0);
+}
+
+/// The NULL pointer can be represented as the default global pointer.
+/// @{
+const global_ptr<void> null_ptr;
+/// @}
 
 template <typename T>
 class gp_subscr {
@@ -127,24 +233,13 @@ global_ptr<T> alloc_blocked(size_t total_elems, uint32_t elems_per_block=1, uint
 } // namespace gas
 } // namespace hpx
 
-template <typename T>
-inline
-hpx::global_ptr<T> operator+(const hpx::global_ptr<T>& lhs, size_t n) {
-  return hpx::global_ptr<T>(hpx_addr_add(lhs.ptr(), n * sizeof(T), lhs.get_block_size() * sizeof(T)), lhs.get_block_size());
-}
-
-template <typename T>
-inline
-int64_t operator-(const hpx::global_ptr<T>& lhs, const hpx::global_ptr<T>& rhs) {
-  return hpx_addr_sub(lhs.ptr(), rhs.ptr(), lhs.get_block_size() * sizeof(T));
-}
-
 namespace hpx {
 template <typename T>
 inline
 global_ptr<T> gp_subscr<T>::operator&() const {
   return _gp + _offset;
 }
+
 } // namespace hpx
 
 #endif // HPX_CXX_GLOBAL_PTR_H
