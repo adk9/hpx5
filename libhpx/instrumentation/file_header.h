62e7f2450163b14fa9283e265ce0c711947e8859
Abhishek Kulkarni <adkulkar@indiana.edu>
2016-02-10 22:35:59 -0500

    inst: more (minor) cleanup to the instrumentation headers.

--- libhpx/instrumentation/file_header.h
+++ libhpx/instrumentation/file_header.h
@@ -14,8 +14,6 @@
 #ifndef LIBHPX_INSTRUMENTATION_FILE_HEADER_H
 #define LIBHPX_INSTRUMENTATION_FILE_HEADER_H
 
-#include <hpx/hpx.h>
-
 /// The header needed for our data file format
 typedef struct {
   const char magic_number[8];
@@ -24,13 +22,13 @@ typedef struct {
   char header_data[];
 } logtable_header_t;
 
-#define _LOGTABLE_HEADER                                        \
-  {                                                             \
-    .magic_number = {'h', 'p', 'x', ' ', 'l', 'o', 'g', '\0'},  \
-      .order = 0xFF00AA55                                       \
-         }
+#define _LOGTABLE_HEADER                                       \
+  {                                                            \
+    .magic_number = {'h', 'p', 'x', ' ', 'l', 'o', 'g', '\0'}, \
+    .order = 0xFF00AA55                                        \
+  }
 
-extern logtable_header_t LOGTABLE_HEADER; // == _LOGTABLE_HEADER
+extern logtable_header_t LOGTABLE_HEADER;
 
 size_t write_trace_header(void* base, int class, int id);
 
