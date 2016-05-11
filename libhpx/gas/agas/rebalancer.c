aa2b4e5026887561fe2bae6d27e81ef73765a28c
Abhishek Kulkarni <adkulkar@indiana.edu>
2016-05-11 16:23:01 -0400

    rebalancer: Set soft affinity for the thread that aggregates the lcoal BSTs.

--- libhpx/gas/agas/rebalancer.c
+++ libhpx/gas/agas/rebalancer.c
@@ -118,6 +118,7 @@ void rebalancer_finalize(void) {
 // This function takes the thread-local BST and merges it with the
 // per-node global BST.
 int _local_to_global_bst(int id, void *UNUSED) {
+  hpx_thread_set_affinity(id);
   worker_t *w = scheduler_get_worker(here->sched, id);
   dbg_assert(w);
 
