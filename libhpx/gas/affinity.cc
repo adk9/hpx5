6064df8342bce8b11a0e373dbb5babdaa4924162
Luke Dalessandro <ldalessa@indiana.edu>
2016-06-09 12:03:34 -0400

    Add an initial implementation of hpx_gas_{set,clear}_affinity.
    This uses a static libcuckoo hash table to store affinities. It promotes the
    libcuckoo contrib to required, which transitively makes C++11 support
    required. In the future it might make sense to add a configure-time option that
    can enable and disable gas affinity in order to remediate this increased
    system depenendecy. At the same time, all of the systems that we work with
    _have_ C++11 support, so it's not a huge concern.

--- libhpx/gas/affinity.cc
+++ libhpx/gas/affinity.cc
@@ -15,10 +15,40 @@
 # include "config.h"
 #endif
 
+#include <cuckoohash_map.hh>
+#include <city_hasher.hh>
 #include <hpx/hpx.h>
+extern "C" {
+#include <libhpx/debug.h>
+#include <libhpx/gas.h>
+#include <libhpx/locality.h>
+#include <libhpx/scheduler.h>
+}
+
+namespace {
+typedef cuckoohash_map<hpx_addr_t, int, CityHasher<hpx_addr_t>> AffinityMap;
+AffinityMap _map;
+}
 
 void hpx_gas_set_affinity(hpx_addr_t addr, int worker) {
+  DEBUG_IF(gas_owner_of(here->gas, addr) != here->rank) {
+    dbg_error("Attempt to set affinity of %zu at %d (owned by %d)\n",
+              addr, here->rank, gas_owner_of(here->gas, addr));
+  }
+  DEBUG_IF(worker < 0 || here->sched->n_workers <= worker) {
+    dbg_error("Attempt to set affinity of %zu to %d is outside range [0, %d)\n",
+              addr, worker, here->sched->n_workers);
+  }
+  // @todo: Should we be pinning addr? The interface doesn't require it, but it
+  //        could prevent usage errors in AGAS? On the other hand, it could
+  //        result in debugging issues with pin reference counting.
+  _map.insert(addr, worker);
 }
 
 void hpx_gas_clear_affinity(hpx_addr_t addr) {
+  DEBUG_IF(gas_owner_of(here->gas, addr) != here->rank) {
+    dbg_error("Attempt to clear affinity of %zu at %d (owned by %d)\n",
+              addr, here->rank, gas_owner_of(here->gas, addr));
+  }
+  _map.erase(addr);
 }
