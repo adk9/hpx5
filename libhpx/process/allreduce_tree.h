2af455f55294239d19b9868b8bee95e5773cc451
Udayanga Wickramasinghe <uswickra@cutter.crest.iu.edu>
2016-06-10 03:53:01 -0400

    adding definitions for binomial tree algorithm for allreduce

--- libhpx/process/allreduce_tree.h
+++ libhpx/process/allreduce_tree.h
@@ -13,6 +13,8 @@ void allreduce_tree_register_leaves(allreduce_t *r, hpx_addr_t leaf_addr);
 void allreduce_tree_algo_nary(allreduce_t *r, hpx_addr_t *locals,
 	       	int32_t num_locals, int32_t arity);
 
+void allreduce_tree_algo_binomial(allreduce_t *r, hpx_addr_t *locals, int32_t num_locals);
+
 int32_t allreduce_tree_setup_parent(allreduce_t *r, hpx_action_t op, hpx_addr_t child) ;
 
 void allreduce_tree_setup_child(allreduce_t *r, hpx_addr_t parent) ;
@@ -24,6 +26,7 @@ extern HPX_ACTION_DECL(allreduce_tree_fini_async);
 extern HPX_ACTION_DECL(allreduce_tree_add_async);
 extern HPX_ACTION_DECL(allreduce_tree_register_leaves_async);
 extern HPX_ACTION_DECL(allreduce_tree_algo_nary_async);
+extern HPX_ACTION_DECL(allreduce_tree_algo_binomial_async);
 extern HPX_ACTION_DECL(allreduce_tree_setup_parent_async);
 extern HPX_ACTION_DECL(allreduce_tree_setup_child_async);
 
