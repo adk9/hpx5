37db161778f52190727e27cca11cfc997219f51d
Abhishek Kulkarni <adkulkar@indiana.edu>
2016-06-20 13:14:40 -0400

    AGAS refactoring. Split out cyclic allocation operations into a separate file.

--- libhpx/gas/agas/agas.c
+++ libhpx/gas/agas/agas.c
@@ -165,107 +165,6 @@ void _agas_set_attr(void *gas, hpx_addr_t addr, uint32_t attr) {
   btt_set_attr(agas->btt, gva, attr);
 }
 
-static int
-_locality_alloc_cyclic_handler(uint64_t blocks, uint32_t align, uint64_t offset,
-                               void *lva, uint32_t attr, int zero) {
-  agas_t *agas = (agas_t*)here->gas;
-  size_t bsize = 1u << align;
-  if (here->rank != 0) {
-    size_t boundary = (bsize < 8) ? 8 : bsize;
-    lva = NULL;
-    int e = posix_memalign(&lva, boundary, blocks * bsize);
-    dbg_check(e, "Failed memalign\n");
-    (void)e;
-  }
-
-  if (zero) {
-    lva = memset(lva, 0, blocks * bsize);
-  }
-
-  // and insert entries into our block translation table
-  gva_t gva = {
-    .bits = {
-      .offset = offset,
-      .cyclic = 1,
-      .size = align,
-      .home = here->rank
-    }
-  };
-
-  for (int i = 0; i < blocks; i++) {
-    btt_insert(agas->btt, gva, here->rank, lva, blocks, attr);
-    lva += bsize;
-    gva.bits.offset += bsize;
-  }
-  return HPX_SUCCESS;
-}
-static LIBHPX_ACTION(HPX_DEFAULT, 0, _locality_alloc_cyclic,
-                     _locality_alloc_cyclic_handler, HPX_UINT64, HPX_UINT32,
-                     HPX_UINT64, HPX_POINTER, HPX_UINT32, HPX_INT);
-
-hpx_addr_t agas_alloc_cyclic_sync(size_t n, size_t bbsize, uint32_t attr,
-                                  int zero) {
-  dbg_assert(bbsize <= _agas_max_block_size);
-  uint32_t bsize = bbsize;
-
-  agas_t *agas = (agas_t*)here->gas;
-  dbg_assert(here->rank == 0);
-
-  // Figure out how many blocks per node we need.
-  uint64_t blocks = ceil_div_64(n, here->ranks);
-  uint32_t  align = ceil_log2_32(bsize);
-  dbg_assert(align <= 32);
-  size_t padded = 1u << align;
-
-  agas_alloc_bsize = padded;
-  // Allocate the blocks as a contiguous, aligned array from cyclic memory.
-  void *lva = cyclic_memalign(padded, blocks * padded);
-  if (!lva) {
-    dbg_error("failed cyclic allocation\n");
-  }
-
-  gva_t gva = agas_lva_to_gva(agas, lva, padded);
-  gva.bits.cyclic = 1;
-  uint64_t offset = gva.bits.offset;
-  int e = hpx_bcast_rsync(_locality_alloc_cyclic, &blocks, &align, &offset,
-                          &lva, &attr, &zero);
-  dbg_check(e, "failed to insert btt entries.\n");
-
-  // and return the address
-  return gva.addr;
-}
-LIBHPX_ACTION(HPX_DEFAULT, 0, agas_alloc_cyclic, agas_alloc_cyclic_sync,
-              HPX_SIZE_T, HPX_SIZE_T, HPX_UINT32, HPX_INT);
-
-static hpx_addr_t
-_agas_alloc_cyclic(size_t n, size_t bbsize, uint32_t boundary, uint32_t attr) {
-  dbg_assert(bbsize <= _agas_max_block_size);
-  uint32_t bsize = bbsize;
-
-  int zero = 0;
-  hpx_addr_t addr;
-  int e = hpx_call_sync(HPX_THERE(0), agas_alloc_cyclic, &addr, sizeof(addr),
-                        &n, &bsize, &attr, &zero);
-  dbg_check(e, "Failed to call agas_alloc_cyclic.\n");
-  dbg_assert_str(addr != HPX_NULL, "HPX_NULL is not a valid allocation\n");
-  return addr;
-}
-
-static hpx_addr_t
-_agas_calloc_cyclic(size_t n, size_t bbsize, uint32_t boundary,
-                    uint32_t attr) {
-  dbg_assert(bbsize <= _agas_max_block_size);
-  uint32_t bsize = bbsize;
-
-  int zero = 1;
-  hpx_addr_t addr;
-  int e = hpx_call_sync(HPX_THERE(0), agas_alloc_cyclic, &addr, sizeof(addr),
-                        &n, &bsize, &attr, &zero);
-  dbg_check(e, "Failed to call agas_calloc_cyclic.\n");
-  dbg_assert_str(addr != HPX_NULL, "HPX_NULL is not a valid allocation\n");
-  return addr;
-}
-
 static hpx_addr_t _agas_alloc_blocked(size_t n, size_t bsize,
                                       uint32_t boundary, uint32_t attr) {
   dbg_error("Blocked GAS distributions are not supported.\n");
@@ -309,8 +208,8 @@ static gas_t _agas = {
   .there          = _agas_there,
   .try_pin        = _agas_try_pin,
   .unpin          = _agas_unpin,
-  .alloc_cyclic   = _agas_alloc_cyclic,
-  .calloc_cyclic  = _agas_calloc_cyclic,
+  .alloc_cyclic   = agas_alloc_cyclic,
+  .calloc_cyclic  = agas_calloc_cyclic,
   .alloc_blocked  = _agas_alloc_blocked,
   .calloc_blocked = _agas_calloc_blocked,
   .alloc_user     = _agas_alloc_user,
--- libhpx/gas/agas/agas.h
+++ libhpx/gas/agas/agas.h
@@ -49,41 +49,40 @@ void agas_chunk_dalloc(agas_t *agas, void *bitmap, void *addr, size_t n);
 
 void agas_move(void *gas, hpx_addr_t src, hpx_addr_t dst, hpx_addr_t sync);
 
-int agas_memput(void *gas, hpx_addr_t to, const void *from, size_t n,
-                hpx_addr_t lsync, hpx_addr_t rsync);
-
-int agas_memput_lsync(void *gas, hpx_addr_t to, const void *from, size_t n,
-                      hpx_addr_t rsync);
-
-int agas_memput_rsync(void *gas, hpx_addr_t to, const void *from, size_t n);
-
-int agas_memget(void *gas, void *to, hpx_addr_t from, size_t n,
-                hpx_addr_t lsync, hpx_addr_t rsync);
-
-int agas_memget_rsync(void *gas, void *to, hpx_addr_t from, size_t n,
-                      hpx_addr_t lsync);
-
-int agas_memget_lsync(void *gas, void *to, hpx_addr_t from, size_t n);
-
-int agas_memcpy(void *gas, hpx_addr_t to, hpx_addr_t from, size_t size,
-                hpx_addr_t sync);
-
-int agas_memcpy_sync(void *gas, hpx_addr_t to, hpx_addr_t from, size_t size);
-
 gva_t agas_lva_to_gva(agas_t *gas, void *lva, size_t bsize);
 
+void agas_free(void *gas, hpx_addr_t addr, hpx_addr_t rsync);
+
+/// Local allocation operations.
 hpx_addr_t agas_alloc_local(size_t n, size_t bsize, uint32_t boundary,
                             uint32_t attr);
-
 hpx_addr_t agas_calloc_local(size_t n, size_t bsize, uint32_t boundary,
                              uint32_t attr);
-
 int64_t agas_sub_local(const agas_t *agas, gva_t lhs, gva_t rhs, size_t bsize);
-
 hpx_addr_t agas_add_local(const agas_t *agas, gva_t gva, int64_t bytes,
                           size_t bsize);
 
-void agas_free(void *gas, hpx_addr_t addr, hpx_addr_t rsync);
+/// Cyclic allocation operations.
+hpx_addr_t agas_alloc_cyclic(size_t n, size_t bbsize, uint32_t boundary,
+                             uint32_t attr);
+hpx_addr_t agas_calloc_cyclic(size_t n, size_t bbsize, uint32_t boundary,
+                              uint32_t attr);
+
+/// String operations.
+int agas_memput(void *gas, hpx_addr_t to, const void *from, size_t n,
+                hpx_addr_t lsync, hpx_addr_t rsync);
+int agas_memput_lsync(void *gas, hpx_addr_t to, const void *from, size_t n,
+                      hpx_addr_t rsync);
+int agas_memput_rsync(void *gas, hpx_addr_t to, const void *from, size_t n);
+int agas_memget(void *gas, void *to, hpx_addr_t from, size_t n,
+                hpx_addr_t lsync, hpx_addr_t rsync);
+int agas_memget_rsync(void *gas, void *to, hpx_addr_t from, size_t n,
+                      hpx_addr_t lsync);
+int agas_memget_lsync(void *gas, void *to, hpx_addr_t from, size_t n);
+int agas_memcpy(void *gas, hpx_addr_t to, hpx_addr_t from, size_t size,
+                hpx_addr_t sync);
+int agas_memcpy_sync(void *gas, hpx_addr_t to, hpx_addr_t from, size_t size);
+
 
 #ifdef __cplusplus
 }
