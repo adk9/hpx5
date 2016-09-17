cc049d7241fc5ced482068e1ea37eadb10e9877e
Luke Dalessandro <ldalessa@indiana.edu>
2016-09-17 10:18:50 -0400

    Update the apex test to work with 3.2.0.

--- tests/unit/apex.c
+++ tests/unit/apex.c
@@ -24,9 +24,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include "hpx/hpx.h"
-#ifdef HAVE_APEX
 #include "apex.h"
-#endif
 
 // using this to validate the APEX result.
 volatile int function_count = 0;
@@ -39,11 +37,9 @@ static void _usage(FILE *f, int error) {
   exit(error);
 }
 
-static hpx_action_t _fib      = 0;
-static hpx_action_t _fib_main = 0;
-
-static int _fib_action(int *args, size_t size) {
-  int n = *args;
+static HPX_ACTION_DECL(_fib);
+static int _fib_handler(int n)
+{
   __sync_fetch_and_add(&function_count, 1); // increment the function count
 
   if (n < 2) {
@@ -80,8 +76,8 @@ static int _fib_action(int *args, size_t size) {
     sizeof(int)
   };
 
-  hpx_call(peers[0], _fib, futures[0], &ns[0], sizeof(int));
-  hpx_call(peers[1], _fib, futures[1], &ns[1], sizeof(int));
+  hpx_call(peers[0], _fib, futures[0], &ns[0]);
+  hpx_call(peers[1], _fib, futures[1], &ns[1]);
   hpx_lco_get_all(2, futures, sizes, addrs, NULL);
   hpx_lco_delete(futures[0], HPX_NULL);
   hpx_lco_delete(futures[1], HPX_NULL);
@@ -89,15 +85,16 @@ static int _fib_action(int *args, size_t size) {
   int fn = fns[0] + fns[1];
   return HPX_THREAD_CONTINUE(fn);
 }
+static HPX_ACTION(HPX_DEFAULT, 0, _fib, _fib_handler, HPX_INT);
 
-static int _fib_main_action(int *args, size_t size) {
-  int n = *args;
+static int _fib_main_handler(int n)
+{
   int fn = 0;                                   // fib result
   printf("fib(%d)=", n); fflush(stdout);
   hpx_time_t now = hpx_time_now();
   apex_profiler_handle p = apex_start(APEX_NAME_STRING, "FIB main");
 
-  hpx_call_sync(HPX_HERE, _fib, &fn, sizeof(fn), &n, sizeof(n));
+  hpx_call_sync(HPX_HERE, _fib, &fn, sizeof(fn), &n);
   apex_stop(p);
   double elapsed = hpx_time_elapsed_ms(now)/1e3;
 
@@ -108,14 +105,9 @@ static int _fib_main_action(int *args, size_t size) {
 
   hpx_exit(0, NULL);
 }
+static HPX_ACTION(HPX_DEFAULT, 0, _fib_main, _fib_main_handler, HPX_INT);
 
 int main(int argc, char *argv[]) {
-  // register the fib action
-  HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_MARSHALLED, _fib, _fib_action,
-                      HPX_POINTER, HPX_SIZE_T);
-  HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_MARSHALLED, _fib_main, _fib_main_action,
-                      HPX_POINTER, HPX_SIZE_T);
-
   int e = hpx_init(&argc, &argv);
   if (e) {
     fprintf(stderr, "HPX: failed to initialize.\n");
@@ -148,13 +140,12 @@ int main(int argc, char *argv[]) {
   }
 
   // run the main action
-  e = hpx_run(&_fib_main, &n, NULL, sizeof(n), NULL);
+  e = hpx_run(&_fib_main, NULL, &n);
   hpx_finalize();
 
-#ifdef HAVE_APEX
   // validate the APEX counts
   printf("_fib_action Function calls : %d\n", function_count);
-  apex_profile * profile = apex_get_profile(APEX_FUNCTION_ADDRESS, (void*)(&_fib_action));
+  apex_profile * profile = apex_get_profile(APEX_FUNCTION_ADDRESS, (void*)(&_fib_handler));
   if (profile) {
       printf("Number of calls Reported by APEX : %d, total time in action: %f seconds (or cycles, if rdtsc was used).\n", (int)profile->calls, profile->accumulated);
       if (function_count == profile->calls) {
@@ -164,7 +155,6 @@ int main(int argc, char *argv[]) {
           e = 1;
       }
   }
-#endif
   return e;
 }
 
