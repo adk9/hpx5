adk9/hpx5

commit 3a0931b3be25df591ccb0be77a60c116ed58abf1

Fix the compilation error on jetson

libhpx/gas/pgas/HeapSegment.cpp
@@ -167,7 +167,7 @@ bool
 HeapSegment::chunksAreUsed(uint64_t offset, size_t n) const
 {
   uint32_t from = offset / bytesPerChunk_;
-  uint32_t to = ceil_div(offset + n, bytesPerChunk_);
+  uint32_t to = ceil_div_32(offset + n, bytesPerChunk_);
   return chunks_.isSet(from, to - from);
 }
 

libhpx/gas/pgas/PGAS.cpp
@@ -157,7 +157,7 @@ void
 PGAS::zeroBlocks(uint64_t offset, size_t n, size_t bsize)
 {
   dbg_assert(isCyclicOffset(offset));
-  uint64_t blocks = ceil_div(n, uint64_t(here->ranks));
+  uint64_t blocks = ceil_div_64(n, uint64_t(here->ranks));
   uint32_t align = ceil_log2(bsize);
   uint64_t padded = uint64_t(1) << align;
   std::memset(offsetToLVA(offset), 0, blocks * padded);
@@ -168,7 +168,7 @@ PGAS::allocateCyclicBlocks(size_t n, size_t bsize, uint32_t boundary,
                            uint32_t attr, int zero)
 {
   dbg_assert(here->rank == 0);
-  size_t blocks = ceil_div(n, uint64_t(here->ranks));
+  size_t blocks = ceil_div_64(n, uint64_t(here->ranks));
   uint64_t offset = allocateBlocks(blocks, bsize, boundary, attr, zero, true);
   uint64_t csbrk = getCsbrk();
   hpx_bcast_rsync(SetCsbrk, &csbrk);

commit 2d63c5100fc7949f6a88ce5769cff649e92f2857

Clean up regressions introduced in 3a0931b3.
3a0931b3 was an attempt to deal with some ILP32 type size issues, but it
introduces some regressions as it does not correctly compute block offsets and
such. This commit addresses #67 and attempts to solve the ILP32 issue while
avoiding the regressions.

libhpx/gas/pgas/HeapSegment.cpp
@@ -167,7 +167,9 @@ bool
 HeapSegment::chunksAreUsed(uint64_t offset, size_t n) const
 {
   uint32_t from = offset / bytesPerChunk_;
-  uint32_t to = ceil_div_32(offset + n, bytesPerChunk_);
+  auto t = ceil_div(offset + n, uint64_t(bytesPerChunk_)); // ILP32
+  dbg_assert(t < UINT32_MAX);
+  uint32_t to = (uint32_t)t;
   return chunks_.isSet(from, to - from);
 }
 

libhpx/gas/pgas/PGAS.cpp
@@ -157,7 +157,7 @@ void
 PGAS::zeroBlocks(uint64_t offset, size_t n, size_t bsize)
 {
   dbg_assert(isCyclicOffset(offset));
-  uint64_t blocks = ceil_div_64(n, uint64_t(here->ranks));
+  uint64_t blocks = ceil_div(uint64_t(n), uint64_t(here->ranks));
   uint32_t align = ceil_log2(bsize);
   uint64_t padded = uint64_t(1) << align;
   std::memset(offsetToLVA(offset), 0, blocks * padded);
@@ -168,7 +168,7 @@ PGAS::allocateCyclicBlocks(size_t n, size_t bsize, uint32_t boundary,
                            uint32_t attr, int zero)
 {
   dbg_assert(here->rank == 0);
-  size_t blocks = ceil_div_64(n, uint64_t(here->ranks));
+  uint64_t blocks = ceil_div(uint64_t(n), uint64_t(here->ranks));
   uint64_t offset = allocateBlocks(blocks, bsize, boundary, attr, zero, true);
   uint64_t csbrk = getCsbrk();
   hpx_bcast_rsync(SetCsbrk, &csbrk);