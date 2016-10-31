adk9/hpx5

commit 543cc65ca47f8d9060bd58526d94c742fa6392a1

Fix a bug related to pwc eager buffer reference counting during shutdown.
The pwc eager buffers are reference counted. When the number of bytes remaining
in the buffer reaches 0 it is automatically freed. It is normally the case that
the "reload" mechanism has replaced the current Peer-referenced buffer before
the reference count reaches 0 because the local scheduler hasn't yet processed
all of the parcels that were sent. At shutdown the Peer deletes the current
buffer.

It turns out that there is an extreme corner case race where the current buffer
has had its last byte reclaimed (and thus has been deleted), but no reload
command has yet run. In this context the current recv_ pointer in the Peer is
pointing at a buffer that was already deleted, so deleting it at shutdown is a
double-delete and triggers allocator issues.

The fix for this is to check and see if the current recv_ buffer has already
been reclaimed at shutdown before trying to delete it.

libhpx/network/pwc/ParcelBlock.cpp
@@ -107,7 +107,7 @@ InplaceBlock::InplaceBlock(size_t n)
 InplaceBlock::~InplaceBlock()
 {
   if (remaining_.load()) {
-    log_parcel("block freed with %zu bytes remaining\n", remaining_.load());
+    log_parcel("block freed with %zu bytes remaining\n", this, remaining_.load());
   }
   log_parcel("deleting parcel block at %p\n", (void*)this);
 }
@@ -142,6 +142,14 @@ InplaceBlock::deallocate(size_t bytes)
              (void*)this, remaining);
   if (!remaining) {
     delete this;
+  }
+}
+
+void
+InplaceBlock::finalize()
+{
+  if (remaining_) {
+    deallocate(remaining_);
   }
 }
 

libhpx/network/pwc/ParcelBlock.h
@@ -123,6 +123,13 @@ class EagerBlock {
 /// EagerBlock metadata for an InplaceBlock back to the sender, such that the
 /// sender can manage allocation.
 class InplaceBlock final : public EagerBlock {
+  /// Destruct the eager block.
+  ///
+  /// This is private to prevent accidental double frees. All block deletion
+  /// occurs through the reference-counted deallocate() or shutdown-time
+  /// finalize() routines.
+  ~InplaceBlock();
+
  public:
   /// Construct a recv block with the given size.
   ///
@@ -131,9 +138,6 @@ class InplaceBlock final : public EagerBlock {
   ///
   /// @param       size The total size of the Recv block.
   InplaceBlock(size_t size);
-
-  /// Destruct the eager block.
-  ~InplaceBlock();
 
   /// Allocate registered memory for a new RecvBlock.
   ///
@@ -157,6 +161,15 @@ class InplaceBlock final : public EagerBlock {
   ///
   /// @param          n The number of bytes to return.
   void deallocate(size_t n);
+
+  /// Clean up a block at shutdown.
+  ///
+  /// This is called by a Peer to finalize its current inplace block at
+  /// shutdown. If there are bytes remaining then it should deallocate them,
+  /// otherwise if there are no bytes remaining then it has no impact (because
+  /// the fact that there are 0 remaining bytes implies delete was already
+  /// called).
+  void finalize();
 
   /// A utility to help delete a block-allocated parcel.
   ///

libhpx/network/pwc/Peer.cpp
@@ -43,9 +43,9 @@ Peer::Peer()
 
 Peer::~Peer()
 {
-  delete recv_;
+  if (recv_)
+    recv_->finalize();
 }
-
 
 void*
 Peer::operator new[](size_t bytes)

commit a71ce433caa1ef0a265c1e8066b0113d0773fc34

Fix printf format regression introduced in 543cc65.

libhpx/network/pwc/ParcelBlock.cpp
@@ -107,7 +107,8 @@ InplaceBlock::InplaceBlock(size_t n)
 InplaceBlock::~InplaceBlock()
 {
   if (remaining_.load()) {
-    log_parcel("block freed with %zu bytes remaining\n", this, remaining_.load());
+    log_parcel("block %p freed with %zu bytes remaining\n", this,
+               remaining_.load());
   }
   log_parcel("deleting parcel block at %p\n", (void*)this);
 }