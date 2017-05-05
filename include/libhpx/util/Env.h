6d908bd9d4c2f9de8dcf60e80be7ebdf4437e609
Luke Dalessandro <ldalessa@indiana.edu>
2017-05-05 11:18:42 -0400

    Eliminate deprecated use of throw() specifier.

--- include/libhpx/util/Env.h
+++ include/libhpx/util/Env.h
@@ -27,7 +27,7 @@ class NotFound : public std::exception {
 /// Get a value of type T from the environment associated with a
 /// case-insensitive key @p key.
 template <typename T>
-inline T getEnv(const char * const key) throw(NotFound) {
+inline T getEnv(const char * const key) {
   char* env = std::getenv(key);
   if (!env) {
     std::string upper(key);
